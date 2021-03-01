7ebde2507883af798fc74a3607b7a8599d520464
Abhisek Kumar Gupta <65363296+akgmage@users.noreply.github.com>
2021-03-01 22:39:41 +0530

    Add files via upload

---
+++ CCTI/arrays_strings_check_permutations.cpp
@@ -0,0 +1,46 @@
+// Implement an algorithm to determine if a string is permutation of other.
+// Program Author : Abhisek Kumar Gupta
+#include<bits/stdc++.h>
+using namespace std;
+bool check_permutation(string s, string t){
+    // Time complexity O(n log (n)) n = len of string
+    // but its clean simple and easy to understand
+    if(s.length() != t.length())
+        return false;
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if(s.compare(t) == 0)
+        return true;
+    return false;    
+}
+bool check_permutation_fast(string s, string t){
+    // TC O(n)
+    if(s.length() != t.length())
+        return false;
+    map<char, int> letters;
+    for(char c : s){
+        letters[c]++;
+    }
+    for(char c : t){
+       letters[c]--;
+       if(letters[c] < 0){
+        return false;
+       } 
+    }
+    return true;    
+}
+void print_ans(bool ans){
+    if(ans){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+}
+int main(){
+    check_permutation("ABDE", "DEBA") == true ? print_ans(true) : print_ans(false);
+    check_permutation("ABCD", "DEFG") == true ? print_ans(true) : print_ans(false);
+    check_permutation_fast("AA", "DEBA") == true ? print_ans(true) : print_ans(false);
+    check_permutation_fast("ADEB", "ABED") == true ? print_ans(true) : print_ans(false);
+    return 0;
+}
---
+++ CCTI/arrays_strings_one_away.cpp
@@ -0,0 +1,59 @@
+// Given two strings write a function to check if they are one edit(or zero edits) away
+// Program Author : Abhisek Kumar Gupta
+#include<bits/stdc++.h>
+using namespace std;
+bool one_edit_replace(string a, string b){
+    bool found_difference = false;
+    for(int i = 0; i < a.length(); i++){
+        if(a[i] != b[i]){
+            if(found_difference){
+                return false;
+            }
+            found_difference = true;
+        }
+    }
+    return true;
+}
+bool one_edit_insert(string a, string b){
+    int i1 = 0, i2 = 0;
+    while(i1 < a.length() && i2 < b.length()){
+        if(a[i1] != b[i2]){
+            if(i1 != i2){
+                return false;
+            }
+            i2++;
+        }
+        else{
+            i1++;
+            i2++;
+        }
+    }
+    return true;
+}
+bool is_one_edit_away(string a, string b){
+    if(a.length() == b.length()){
+        return one_edit_replace(a, b);
+    }
+    else if(a.length() + 1 == b.length()){
+        return one_edit_insert(a, b);
+    }
+    else if(a.length() - 1 == b.length()){
+        return one_edit_insert(b, a);
+    }
+    return false;
+}
+void print_ans(bool ans){
+    if(ans){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+}
+int main(){
+    is_one_edit_away("Pale", "Ple") == true ? print_ans(true) : print_ans(false);
+    is_one_edit_away("Pales", "Pale") == true ? print_ans(true) : print_ans(false);
+    is_one_edit_away("Pale", "bale") == true ? print_ans(true) : print_ans(false);
+    is_one_edit_away("Pale", "bae") == true ? print_ans(true) : print_ans(false);   
+    return 0;
+}
