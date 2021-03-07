bc2d6359fdf7ba79e499e52fc579fd8deff830a8
Abhisek Kumar Gupta <65363296+akgmage@users.noreply.github.com>
2021-03-07 17:57:13 +0530

    Add files via upload

---
+++ Bit_Manipulation/setbits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.H>
+using namespace std;
+int find_set_bits(int n){
+    int set_bits = 0;
+    while(n){
+        set_bits += (n & 1);
+        n >>= 1;
+    }
+    return set_bits;
+}
+int main(){
+    cout << find_set_bits(3) << endl;
+    cout << find_set_bits(4) << endl;
+    cout << find_set_bits(5) << endl;
+    cout << find_set_bits(6) << endl;
+    cout << find_set_bits(7) << endl;
+    cout << find_set_bits(15) << endl;
+    return 0;
+}
