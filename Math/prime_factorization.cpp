36737cf35d6a0f04124abdef773d581bb98df86d
Abhisek Kumar Gupta <65363296+akgmage@users.noreply.github.com>
2021-06-23 13:07:07 +0530

    Add files via upload

--- Math/prime_factorization.cpp
+++ Math/prime_factorization.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 int f[100], expo[100], len = -1;
-
-int main(){
-	int n;
-    cin >> n;
+void prime_factor(int n){
     int d = 2;
-    while(n > 1 && d * d <= n){
+    if(n == 1){
+        len++;
+        f[len] = 2;
+        expo[len] = 0;
+        return;
+    }
+    while(n > 1 && 1ll * d * d <= n){
         int k = 0;
         while(n % d == 0){
             n = n / d;
@@ -25,6 +28,11 @@ int main(){
         f[len] = n;
         expo[len] = 1;
     }
+}
+int main(){
+	int n;
+    cin >> n;
+    prime_factor(n);
     for(int i = 0; i <= len; i++){
         cout << f[i] << "->" << expo[i] << " Times" << endl;
     }
