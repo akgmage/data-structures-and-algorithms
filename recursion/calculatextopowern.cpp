71127335331f7ff46cd0ff28d4c74ee131029ba4
Abhisek Kumar Gupta <65363296+akgmage@users.noreply.github.com>
2021-06-27 16:48:50 +0530

    Add files via upload

---
+++ recursion/calculatextopowern.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Method 1 Time complexity O(n)
+int power(int x, int n){
+	if(n == 0)
+		return 1;
+	return x * power(x, n-1);
+}
+*/
+
+// Method 2 : Time complexity O(log n)
+int power(int x, int n){
+	if(n == 0)
+		return 1;
+	else if(n % 2 == 0){
+		int y = power(x, n / 2);
+		return y * y;	
+	}
+	return x * power(x, n - 1);
+}
+
+int main(){
+	int x, n;
+	cout << "Give me x annd n";
+	cin >> x >> n;
+	int result = power(x,n);
+	cout << result << endl;
+	return 0;
+}
