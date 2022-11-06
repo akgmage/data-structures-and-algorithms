d1298468ca2cc54b624bf4c13442c1cebc2a3a42
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-06 21:09:55 +0530

    add comment

--- CCTI/tempCodeRunnerFile.go
+++ CCTI/tempCodeRunnerFile.go
@@ -1,8 +1,1 @@
-
-
-func main() {
-	s := "Mr John Smith               ";
-	nRunes := utf8.RuneCountInString(s)
-		runes := make([]rune, nRunes)
-		for i, r := range s {
-			runes[i] = r
+length
--- CCTI/urlify.go
+++ CCTI/urlify.go
@@ -21,13 +21,14 @@ func Urlify(s string, length int) string {
 	// triple space count and find out how many extra characters will be there in final string 
 	index := length + (spaceCount * 3) // actual length of final string
 	for i := length - 1; i >= 0; i-- {
-		// if u encounter space then add %20
+		// If u encounter space then replace with  %20
 		if r[i] == ' ' {
 			r[index - 1] = '0'
 			r[index - 2] = '2'
 			r[index - 3] = '%'
 			index -= 3
 		} else {
+			// If there is no space then copy the original character
 			r[index - 1] = r[i]
 			index--
 		}
@@ -37,7 +38,7 @@ func Urlify(s string, length int) string {
 
 
 func main() {
-	s := "Mr John Smith               ";
+	s := "Mr John Smith          ";
 	length := 13
 	msg := Urlify(s, length);
 	fmt.Println(msg)
