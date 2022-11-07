ec1dc6728841a20d91b71ebdfc1083dd82b31bc9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-07 22:45:03 +0530

    add explanation

--- sorting/selectionsort.cpp
+++ sorting/selectionsort.cpp
@@ -1,3 +1,13 @@
+// Implementation of selection sort.
+// Selection sort is an in-place comparison sorting algorithm.
+// It has an O(n2) time complexity which makes it inefficient on large lists,
+// and generally performs worse than the similar insertion sort.
+// Selection sort is noted for its simplicity and has performance advantages
+// over more complicated algorithms in certain situations,
+// particularly where auxiliary memory is limited. (Source wiki) https://en.wikipedia.org/wiki/Selection_sort
+
+// Sample Input : [2, 1, 9, 3, 5, 4, 0]
+// Output : [0 1 2 3 4 5 9]
 #include<bits/stdc++.h>
 using namespace std;
 
