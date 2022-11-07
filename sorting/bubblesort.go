36735e3ed7cf81ed2cc10164347b69c868b00f21
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-07 22:50:00 +0530

    modify time complexity

--- sorting/bubblesort.go
+++ sorting/bubblesort.go
@@ -5,7 +5,8 @@
 //  These passes through the list are repeated until no swaps had to be performed during a pass,
 // meaning that the list has become fully sorted.
 
-// Time Complexity O(n2)
+// Time Complexity worst-case and average complexity O(n^{2})
+// Bubble sort is O(n) on a list that is already sorted i.e. Best case
 
 // Sample Input : [2, 1, 9, 3, 5, 4, 0]
 // Output : [0 1 2 3 4 5 9]
