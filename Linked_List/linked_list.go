313d48bc35381714cb9b424be32ecf88ee5c62c3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-10 22:25:39 +0530

    add pushback func

--- Linked_List/linked_list.go
+++ Linked_List/linked_list.go
@@ -10,3 +10,24 @@ type linkedlist struct {
 	head   *node
 	tail   *node
 }
+
+func (ll *linkedlist) PushBack(n *node) {
+	if ll.head == nil {
+		ll.head = n
+		ll.tail = n
+		ll.length++
+	} else {
+		ll.tail.next = n
+		ll.tail = n
+		ll.length++
+	}
+}
+func main() {
+	list := linkedlist{}
+	node1 := &node{data: 10}
+	node2 := &node{data: 20}
+	node3 := &node{data: 30}
+	list.PushBack(node1)
+	list.PushBack(node2)
+	list.PushBack(node3)
+}
