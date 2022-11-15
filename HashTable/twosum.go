2dcdb8354941293b3992e52a88017ba161f7d3ec
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-15 22:24:01 +0530

    add description

--- HashTable/twosum.go
+++ HashTable/twosum.go
@@ -1,3 +1,10 @@
+/*
+  Write a function that takes in a non-empty array of distinct integers and an
+  integer representing a target sum. If any two numbers in the input array sum
+  up to the target sum, the function should return them in an array, in any
+  order. If no two numbers sum up to the target sum, the function should return
+  an empty array.
+*/
 package main
 
 import "fmt"
