80b7c8c7a39396f33b43b515dd7cfd9bc6db2101
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-16 23:37:14 +0530

    add sample input output

--- Arrays/sorted_square_array.go
+++ Arrays/sorted_square_array.go
@@ -2,6 +2,9 @@
   Write a function that takes in a non-empty array of integers that are sorted
   in ascending order and returns a new array of the same length with the squares
   of the original integers also sorted in ascending order.
+
+  Sample Input: [-6, 1, 2, 3, 4]
+  Output: [1, 4, 6, 16, 36]
 */
 package main
 
