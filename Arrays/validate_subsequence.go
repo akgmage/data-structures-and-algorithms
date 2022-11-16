f6240c624b38fae7649d9fb5c457d99963082869
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-16 22:26:31 +0530

    add validate subsequence in go

---
+++ Arrays/validate_subsequence.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+
+func IsValidSubsequence(array []int, sequence []int) bool {
+	// Write your code here.
+    index := 0
+	for i := 0; i < len(array) && index < len(sequence); i++ {
+		if array[i] == sequence[index] {
+			index++
+		}
+	}
+	return index == len(sequence)
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6}
+	seq := []int{2, 4, 6}
+	msg := IsValidSubsequence(arr, seq)
+	fmt.Println(msg)
+}
