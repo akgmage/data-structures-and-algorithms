d3fe02275b8988cd0a938f41b0e5239ea9ad7743
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-16 23:36:24 +0530

    add description

--- Arrays/sorted_square_array.go
+++ Arrays/sorted_square_array.go
@@ -1,3 +1,8 @@
+/*
+  Write a function that takes in a non-empty array of integers that are sorted
+  in ascending order and returns a new array of the same length with the squares
+  of the original integers also sorted in ascending order.
+*/
 package main
 
 import "fmt"
