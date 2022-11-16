5dd5e1b473b5a748e2eb18094421fa0b44744c5d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-16 22:27:49 +0530

    add description

--- Arrays/validate_subsequence.go
+++ Arrays/validate_subsequence.go
@@ -1,6 +1,10 @@
 /*
   Given two non-empty arrays of integers, write a function that determines
   whether the second array is a subsequence of the first one.
+
+  A subsequence of an array is a set of numbers that aren't necessarily adjacent
+  in the array but that are in the same order as they appear in the array. For
+  instance, the numbers
 */
 package main
 
