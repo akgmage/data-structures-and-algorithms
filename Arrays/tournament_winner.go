39a30e602c7022af7f277d09b4ec728799bf2e4b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-18 22:20:37 +0530

    add description

--- Arrays/tournament_winner.go
+++ Arrays/tournament_winner.go
@@ -11,6 +11,9 @@
   Given an array of pairs representing the teams that have competed against each
   other and an array containing the results of each competition, write a
   function that returns the winner of the tournament. The input arrays are named
+  competitions and results, respectively. The competitions  array has elements in the
+  form of [homeTeam, awayTeam], where each team is a string of at most 30
+  characters representing the name of the team.
 
 
 */
