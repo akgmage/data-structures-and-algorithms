0ae5f434f8c4da4348d2022c58823be4a7001dca
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-18 22:16:57 +0530

    add description

--- Arrays/tournament_winner.go
+++ Arrays/tournament_winner.go
@@ -1,3 +1,13 @@
+/*
+  There's an algorithms tournament taking place in which teams of programmers
+  compete against each other to solve algorithmic problems as fast as possible.
+  Teams compete in a round robin, where each team faces off against all other
+  teams. Only two teams compete against each other at a time, and for each
+  competition, one team is designated the home team, while the other team is the
+  away team. In each competition there's always one winner and one loser; there
+  are no ties. A team receives 3 points if it wins and 0 points if it loses. The
+  winner of the tournament is the team that receives the most amount of points.
+*/
 package main
 
 import "fmt"
