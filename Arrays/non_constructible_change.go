1569c368664eff5dd2922a3ad4ffcd405c31cd5e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-22 22:12:52 +0530

    add description

--- Arrays/non_constructible_change.go
+++ Arrays/non_constructible_change.go
@@ -1,3 +1,12 @@
+/*
+  Given an array of positive integers representing the values of coins in your
+  possession, write a function that returns the minimum amount of change (the
+  minimum sum of money) that you
+
+  For example, if you're given coins = [1, 2, 5] , the minimum
+  amount of change that you can't create is 4, . If you're given no
+  coins, the minimum amount of change that you can't create is 1
+*/
 package main
 
 import (
