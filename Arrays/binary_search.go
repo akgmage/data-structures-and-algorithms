ed078019f1b55fcf89f2af9654be39c7d9b2fc5a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-23 22:42:42 +0530

    add description

--- Arrays/binary_search.go
+++ Arrays/binary_search.go
@@ -1,3 +1,9 @@
+/*
+  Write a function that takes in a sorted array of integers as well as a target
+  integer. The function should use the Binary Search algorithm to determine if
+  the target integer is contained in the array and should return its index if it
+  is, otherwise
+*/
 package main
 
 import "fmt"
