43810731bc0bf6c3a71ad6fe35e8be90e6ddadb2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-23 22:14:18 +0530

    add description

--- Arrays/is_pallindrome.go
+++ Arrays/is_pallindrome.go
@@ -1,3 +1,11 @@
+/*
+  Write a function that takes in a non-empty string and that returns a boolean
+  representing whether the string is a palindrome.
+  Sample Input: abba
+  Output: True
+  Sample Input: aberba
+  Output: False
+*/
 package main
 
 import "fmt"
