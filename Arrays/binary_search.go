a7865fedb855a1d24053f08682257f75ee189ccf
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-23 22:43:54 +0530

    add sample input output

--- Arrays/binary_search.go
+++ Arrays/binary_search.go
@@ -2,7 +2,11 @@
   Write a function that takes in a sorted array of integers as well as a target
   integer. The function should use the Binary Search algorithm to determine if
   the target integer is contained in the array and should return its index if it
-  is, otherwise
+  is, otherwise -1
+  Sample Input : [0, 1, 44, 66, 77] target = 66
+  Output : 3
+  Sample Input : [0, 1, 44, 66, 77] target = 101
+  Output : -1
 */
 package main
 
