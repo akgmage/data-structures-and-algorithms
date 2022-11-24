c33ae06ca0e7e6b66d45019ae5525066371d0305
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-24 22:49:06 +0530

    add comments

--- Arrays/ceaser_cipher.go
+++ Arrays/ceaser_cipher.go
@@ -1,8 +1,9 @@
 package main
 
 func CaesarCipherEncryptor(str string, key int) string {
-	key = key % 26
+	key = key % 26 // mod with 26 just in case the key is not bigger
 	result := ""
+	// find out ascii value if its greater than ascii value of 'z' then reduce 26 from value
 	for i := 0; i < len(str); i++ {
 		asciiValue := int(str[i]) + key
 		if asciiValue > 122 {
