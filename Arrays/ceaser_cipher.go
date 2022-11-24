cab90ae3dfbf0939fb17f4530fae9793314a8ae9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-24 22:52:35 +0530

    add description

--- Arrays/ceaser_cipher.go
+++ Arrays/ceaser_cipher.go
@@ -1,3 +1,9 @@
+/*
+  Given a non-empty string of lowercase letters and a non-negative integer
+  representing a key, write a function that returns a new string obtained by
+  shifting every letter in the input string by k positions in the alphabet,
+  where k is the key.
+*/
 package main
 
 import "fmt"
