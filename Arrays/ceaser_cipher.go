4e306a6898fd074647f4334bee245ecc82a66dc2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-24 22:53:23 +0530

    add description

--- Arrays/ceaser_cipher.go
+++ Arrays/ceaser_cipher.go
@@ -3,6 +3,9 @@
   representing a key, write a function that returns a new string obtained by
   shifting every letter in the input string by k positions in the alphabet,
   where k is the key.
+
+  Note that letters should "wrap" around the alphabet; in other words, the
+  letter z shifted by one returns the letter a
 */
 package main
 
