00b7ddfb3416038d2f6c2f37af04e15d37ab079f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-24 23:26:11 +0530

    add comment

--- Binary_Trees/dfs.go
+++ Binary_Trees/dfs.go
@@ -7,6 +7,7 @@ type Node struct {
 
 func (n *Node) DepthFirstSearch(array []int) []int {
 	array = append(array, n.Value)
+	// call dfs on children of each node
 	for _, child := range n.Children {
 		array = child.DepthFirstSearch(array)
 	}
