8609f5524f7867f1c158903c268c422f9f077990
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-26 11:19:12 +0530

    modify tree for more complex dom

--- Tree/tree.go
+++ Tree/tree.go
@@ -4,21 +4,50 @@ package main
 
 /*
 	<html>
-	<h1>This is h1</h1>
-	<h2>This is h2</h2>
-	<p>This is para.</p>
+		<body>
+			<h1>This is a H1</h1>
+			<h2>This is a H1</h2>
+			<p>
+			This is paragraph.
+			<img src="https://image.com/image.png" alt="some logo"/>
+			</p>
+			<div class='someClass'>
+			This is the footer of the page.
+			<span id='spanId'>2022 &copy; AKGMAGE</span>
+			</div>
+		</body>
 	</html>
 */
 type Node struct {
 	tag string
 	text string
+	src string
+	alt string
+	class string
+	id string
 	children []*Node
 }
 
 func main() {
+	span := Node {
+		tag: "span",
+		id: "spanId",
+		text: "2022 &copy; AKGMAGE",
+	}
+	div := Node {
+		tag: "div",
+		class: "someClass",
+		children: []*Node{&span},
+	}
+	image := Node{
+		tag: "image",
+		src: "https://image.com/image.png",
+		alt: "some logo",
+	}
 	p := Node {
 		tag: "p",
 		text: "This is para.",
+		children: []*Node{&image},
 	}
 	h1:= Node {
 		tag: "h1",
@@ -28,8 +57,12 @@ func main() {
 		tag: "h2",
 		text: "This is h2",
 	}
+	body := Node {
+		tag: "body",
+		children: []*Node{&div, &p, &h2, &h1},
+	}
 	html := Node {
 		tag: "html",
-		children: []*Node{&p, &h2, &h1},
+		children: []*Node{&body},
 	}
 }
