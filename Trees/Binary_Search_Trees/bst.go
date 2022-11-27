024cc52bcdf34baf8d91ea7f1bd011064fb8fb28
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-27 14:14:37 +0530

    add pre-order traversal

--- Trees/Binary_Search_Trees/bst.go
+++ Trees/Binary_Search_Trees/bst.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
 type BinaryTreeNode struct {
-	left *BinaryTreeNode
-	data int
+	left  *BinaryTreeNode
+	data  int
 	right *BinaryTreeNode
+}
+
+// Pre-order traversal
+
+func PreOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+	fmt.Printf("%d ", root.data)
+	PreOrder(root.left)
+	PreOrder(root.right)
 }
