8d73eae880fe2f0eab82ffd0e826becd6d189eca
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-27 14:33:33 +0530

    add main func

--- Trees/Binary_Trees/bst.go
+++ Trees/Binary_Trees/bst.go
@@ -24,20 +24,29 @@ func PreOrder(root *BinaryTreeNode) {
 
 func NewBinaryTree(n, k int) *BinaryTreeNode {
 	var root * BinaryTreeNode
-
 	for _, v := range rand.Perm(n) {
 		root = insert(root, (1 + v) * k)
 	}
+	return root
 }
 
 func insert(root * BinaryTreeNode, v int) *BinaryTreeNode {
 	if root == nil {
+		fmt.Printf("%d root", v)
 		return &BinaryTreeNode{nil, v, nil}
 	}
 	if v < root.data {
+		fmt.Printf("%d left\n", v)
 		root.left = insert(root.left, v)
 		return root
 	}
+	fmt.Printf("%d right\n", v)
 	root.right = insert(root.right, v)
 	return root
+}
+
+func main() {
+	t1 := NewBinaryTree(10, 1)
+	PreOrder(t1)
+	fmt.Println()
 }
