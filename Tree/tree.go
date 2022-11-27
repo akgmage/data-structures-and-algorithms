a4ed9b846a0f9189a646e24bf1cc07591b9cdae9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-27 13:05:48 +0530

    add comments

--- Tree/tree.go
+++ Tree/tree.go
@@ -84,15 +84,19 @@ func (n *Node) hasClass(className string) bool {
 }
 
 func findAllByClassName(root *Node, className string) []*Node {
+	// make result of type Node which will contain all the resulting className nodes 
 	result := make([]*Node, 0)
+	// make queue of type Node which will contain all the nodes 
 	queue := make([]*Node, 0)
 	queue = append(queue, root)
 	for len(queue) > 0 {
-		curr := queue[0]
-		queue = queue[1:]
+		curr := queue[0] // set curr as first node
+		queue = queue[1:] // remove first node from queue
+		// if match is found append it to result's slice
 		if curr.hasClass(className) {
 			result = append(result, curr)
 		}
+		// keep exploring children if it has any
 		if len(curr.children) > 0 {
 			for _, child := range curr.children {
 				queue = append(queue, child)
