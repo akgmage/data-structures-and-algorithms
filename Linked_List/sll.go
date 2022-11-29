74fa33f0bec97478c9975102bba777ec0cf6c7dc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-29 22:30:17 +0530

    add description

--- Linked_List/sll.go
+++ Linked_List/sll.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-type ListNode struct {
-	data interface{}
-	next *ListNode
+// Linked list node contains a pointer to the next node as well as data.
+// The generic behavior is achieved by marking the data field as type interface
+type ListNode struct { // define a ListNode in a sll
+	data interface{} // datum
+	next *ListNode // pointer to next ListNode
 }
 
 type LinkedList struct {
