78540956b2ff8ce1344bb13b4bda54069f3131fc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-02 22:33:55 +0530

    recursively check if its sorted

--- Recursion/is_array_sorted.go
+++ Recursion/is_array_sorted.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
-
+func isSorted(A []int) bool {
+	n := len(A)
+	if n == 1 {
+		return true
+	}
+	if A[n - 1] < A[n - 2] {
+		return false
+	}
+	return isSorted(A[:n-1])
+}
 
 func main() {
 	A := []int{10, 20, 23, 23, 45, 78, 88}
