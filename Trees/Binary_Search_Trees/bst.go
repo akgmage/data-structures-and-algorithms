04afea49831ea334bf2fd73c3f53d6e0f147459c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-04 12:54:24 +0530

    add Insert func

--- Trees/Binary_Search_Trees/bst.go
+++ Trees/Binary_Search_Trees/bst.go
@@ -10,12 +10,26 @@ type BSTNode struct {
 	data int
 	right *BSTNode
 }
+
 func ConstructBST(n, k int) *BSTNode {
 	var root *BSTNode
 	for _, v := range rand.Perm(n) {
 		root = Insert(root, (1 + v) * k)
 	}
-} 
+	return root
+}
+
+func Insert(root *BSTNode, data int) *BSTNode {
+	if root == nil {
+		return &BSTNode{nil, data, nil}
+	}
+	if data < root.data {
+		root.left = Insert(root.left, data)
+		return root
+	}
+	root.right = Insert(root.right, data)
+	return root
+}
 
 
 func main() {
