970286285b0bc721cd0a737f5bbae9d2a5687b05
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-12 22:33:32 +0530

    add comments

--- Heaps/heap.go
+++ Heaps/heap.go
@@ -4,32 +4,36 @@ import (
 	"fmt"
 	"math"
 )
-
+// Item: Defines the interface for an element to be held by a Heap instance
 type Item interface {
 	Less(item Item) bool
 }
-
+// Heap: binary heap with support for min heap operations
 type Heap struct {
 	size int
 	data []Item
 }
 
+// New: returns a pointer to an empty min-heap
 func New() *Heap {
 	return &Heap{}
 }
 
+// Parent: For a node at ith location its parent is ar (i - 1) / 2 location
 func Parent(i int) int {
 	return int(math.Floor(float64(i - 1) / 2.0))
 }
 
+// LeftChild: For a node at ith location its left children is at 2 * i + 1 location
 func LeftChild(parent int) int {
 	return (2 * parent) + 1
 }
-
+// RightChild: For a node at ith location its right children is at 2 * i + 2 th locations
 func RightChild(parent int) int {
 	return (2 * parent) + 2
 }
 
+// GetMinimum: Minimum element is always at root
 func GetMinimum(h *Heap) (Item, error) {
 	if h.size == 0 {
 		return nil, fmt.Errorf("Unable to get element from empty heap")
