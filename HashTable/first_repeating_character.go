3d11f438e94d44fc466c77d26067751754c96646
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-14 22:45:30 +0530

    add description

--- HashTable/first_repeating_character.go
+++ HashTable/first_repeating_character.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
-
+// FirstRepeatedCharacter: returns the first repeating character
+// Approach: we know extended ascii has 256 values, so create an array with length 256
+// initialize with all zeroes.
+// Each of input character in word go to the corresponding position and increment its count
+// while scanning the array if we see some value is already 1 then return the value
 func FirstRepeatedCharacter(word string) byte {
 	lengthOfString := len(word)
+	// The maximum number of characters that can be represented in extended ASCII is 256
 	asciiArr := [256]int{}
 	for i := 0; i < 256; i++ {
 		asciiArr[i] = 0
