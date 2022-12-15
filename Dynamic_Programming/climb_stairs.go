0f5212b2abb5aea7c2c8dff0398b2a3f7fbfa3a7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-15 22:36:41 +0530

    add comment

--- Dynamic_Programming/climb_stairs.go
+++ Dynamic_Programming/climb_stairs.go
@@ -8,12 +8,15 @@ import "fmt"
 // Approach: Number of ways to reach kth stair = Number of ways to reach k − 1th stair + Number of ways to reach k − 2th stair
 // ClimbStairs(k) = ClimbStairs(k-1) + ClimbStairs(k-2)
 func ClimbStairs(n int) int {
+	// base case
 	if n < 3 {
 		return n
 	}
 	cache := make([]int, n)
+	// initialize initial 2 values
 	cache[0], cache[1] = 1, 2
 	for i := 2; i < n; i++ {
+		// add previous 2 values
 		cache[i] = cache[i-1] + cache[i-2]
 	}
 	return cache[n-1]
