a00585453ab5044cbca7714fc9f2c7e0fc2ad115
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-16 19:53:37 +0530

    add logic for merge

--- sorting/mergesort.go
+++ sorting/mergesort.go
@@ -23,4 +23,22 @@ func MergeSort(Arr []int) []int {
 
 func Merge(left, right []int) []int {
 	result := make([]int, len(left) + len(right))
+	for i:=0; len(left) > 0 || len(right) > - 0; i++ {
+		if len(left) > 0 && len(right) > 0 {
+			if left[0] < right[0] {
+				result[i] = left[0]
+				left = left[1:]
+			} else {
+				result[i] = right[0]
+				right = right[1:]
+			}
+		} else if len(left) > 0 {
+			result[i] = left[0]
+			left = left[1:]
+		} else if len(right) > 0 {
+			result[i] = right[0]
+			right = right[1:]
+		}
+	}
+	return result
 }
