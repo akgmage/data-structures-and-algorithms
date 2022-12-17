cece8dd278f0fc190226a298deb8736b63863d92
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-17 22:41:27 +0530

    add comments

--- Stacks/stack_array_based.go
+++ Stacks/stack_array_based.go
@@ -6,6 +6,7 @@ type Stack struct {
 	array    []interface{}
 }
 
+// Init: Returns an Initialized Stack
 func (stack *Stack) Init(capacity uint) *Stack {
 	stack.top = -1
 	stack.capacity = capacity
@@ -13,18 +14,22 @@ func (stack *Stack) Init(capacity uint) *Stack {
 	return stack
 }
 
+// NewStack: Returns a new Stack
 func NewStack(capacity uint) *Stack {
 	return new(Stack).Init(capacity)
 }
 
+// IsFull: Returns true if Stack is full oe else false
 func (stack *Stack) IsFull() bool {
 	return stack.top == int(stack.capacity)-1
 }
 
+// IsEmpty: Returns true if Stack is empty oe else false
 func (stack *Stack) IsEmpty() bool {
 	return stack.top == -1
 }
 
+// Size: Returns the size of Stack
 func (stack *Stack) Size() uint {
 	return uint(stack.top + 1)
 }
