924888c5343f08ad76e04f67175b9625a65734dc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-18 13:50:17 +0530

    add Init and NewStack method

---
+++ Stacks/stack_dynamic_array.go
@@ -0,0 +1,20 @@
+package main
+
+type Stack struct {
+	top      int
+	capacity uint
+	array    []interface{}
+}
+
+// Init: Returns an Initialized Stack
+func (stack *Stack) Init(capacity uint) *Stack {
+	stack.top = -1
+	stack.capacity = capacity
+	stack.array = make([]interface{}, capacity)
+	return stack
+}
+
+// NewStack: Returns a new Stack
+func NewStack(capacity uint) *Stack {
+	return new(Stack).Init(capacity)
+}
