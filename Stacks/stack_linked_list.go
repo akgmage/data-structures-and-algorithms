8698febeceb70f6dbc646bd5e089d1637fb1fd0b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-19 22:32:50 +0530

    add comments

--- Stacks/stack_linked_list.go
+++ Stacks/stack_linked_list.go
@@ -10,22 +10,27 @@ type ListNode struct {
 	next *ListNode
 }
 
+// Size: Returns the size of Stack
 func (s *Stack) length() int {
 	return s.size
 }
 
+// Push: Pushes new [data] into Stack
 func (s *Stack) push(data interface{}) {
 	s.top = &ListNode{data, s.top}
 }
 
+// IsEmpty: Returns true if Stack is empty or else false
 func (s *Stack) IsEmpty() bool {
 	return s.size == 0
 }
 
+// IsFull: Returns false since its LL based implementation
 func (s *Stack) IsFull() bool {
 	return false
 }
 
+// Pop: Pops top most data from Stack
 func (s *Stack) Pop() (data interface{}) {
 	if s.size > 0 {
 		data, s.top = s.top.data, s.top.next
@@ -35,6 +40,7 @@ func (s *Stack) Pop() (data interface{}) {
 	return nil
 }
 
+// Peek: Returns top most element from Stack
 func (s *Stack) Peek() (data interface{}) {
 	if s.size > 0 {
 		data = s.top.data
