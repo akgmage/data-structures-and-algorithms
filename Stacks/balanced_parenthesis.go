0b6eb118792115712f4da04ab9848906b574d2e5
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-20 22:13:15 +0530

    add Pair struct and pairs var

---
+++ Stacks/balanced_parenthesis.go
@@ -0,0 +1,131 @@
+// Checking balancing of symbols/parentheses.
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stack struct {
+	top      int
+	capacity uint
+	array    []interface{}
+}
+
+// Init: Returns an Initialized Stack
+func (stack *Stack) Init(capacity uint) *Stack {
+	stack.top = -1
+	stack.capacity = capacity
+	stack.array = make([]interface{}, capacity)
+	return stack
+}
+
+// NewStack: Returns a new Stack
+func NewStack(capacity uint) *Stack {
+	return new(Stack).Init(capacity)
+}
+
+// Size: Returns the size of Stack
+// Time complexity O(1)
+func (stack *Stack) Size() uint {
+	return uint(stack.top + 1)
+}
+
+// IsFull: Returns true if Stack is full or else false
+// Time complexity O(1)
+func (stack *Stack) IsFull() bool {
+	// Stack is full when top is equal to the last index
+	return stack.top == int(stack.capacity) - 1
+}
+
+// IsEmpty: Returns true if Stack is empty or else false
+// Time complexity O(1)
+func (stack *Stack) IsEmpty() bool {
+	// Stack is empty when top is equal to -1
+	return stack.top == -1
+}
+
+// Resize: If the array is full, creates a new array of
+// twice the size, and copy the items
+func (stack *Stack) Resize() {
+	fmt.Println("Resizing")
+	if stack.IsFull() {
+		stack.capacity *= 2
+	} else {
+		stack.capacity /= 2
+	}
+	target := make([]interface{}, stack.capacity)
+	copy(target, stack.array[:stack.top+1])
+	stack.array = target
+}
+
+// Push: Pushes new [data] into Stack, resizes to double if stack is full
+// Time complexity O(1)
+// Space complexity for n push operations O(n)
+func (stack *Stack) Push(data interface{}) error {
+	if stack.IsFull() {
+		stack.Resize()
+	}
+	stack.top++
+	stack.array[stack.top] = data
+	fmt.Printf("\n%v Pushed to stack", data)
+	return nil
+}
+
+// Pop: Pops top most data from Stack, resizes to hald if sizeof stack is less than half the capacity
+// Time complexity O(1)
+func (stack *Stack) Pop() (interface{}, error) {
+	if stack.IsEmpty() {
+		return nil, errors.New("Stack is empty")
+	}
+	temp := stack.array[stack.top]
+	fmt.Printf("\n%v Popped from stack", temp)
+	stack.top--
+	if stack.Size() < stack.capacity / 2 {
+		stack.Resize()
+	}
+	return temp, nil
+}
+
+// Peek: Returns top most element from Stack
+// Time complexity O(1)
+func (stack *Stack) Peek() (interface{}, error) {
+	if stack.IsEmpty() {
+		return nil, errors.New("Stack is empty")
+	}
+	temp := stack.array[stack.top]
+	fmt.Printf("\n%v is the topmost element in stack", temp)
+	return temp, nil
+}
+
+// Drain: Removes all elements that are currently in Stack
+// Time complexity O(1)
+func (stack *Stack) Drain() {
+	stack.array = nil
+	stack.top = -1
+}
+
+type Pair struct {
+	open rune
+	close rune
+}
+
+var pairs = []Pair {
+	{'(', ')'},
+	{'[', ']'},
+	{'{', '}'},
+}
+
+func main() {
+	stack := NewStack(1)
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	fmt.Println(stack.Size())
+	stack.Pop()
+	stack.Pop()
+	stack.Pop()
+	stack.Drain()
+	stack.Peek()
+}
