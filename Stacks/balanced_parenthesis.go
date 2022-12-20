0363d6557e4def84f18892e95c7b80fbb1dcfdc4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-20 22:27:40 +0530

    add time and space complexity

--- Stacks/balanced_parenthesis.go
+++ Stacks/balanced_parenthesis.go
@@ -122,6 +122,7 @@ var pairs = []Pair {
 // If it is a closing symbol like ),],}, then if the stack is empty report an error. Otherwise pop the stack.
 // If the symbol popped is not the corresponding opening symbol, report an error.
 // At end of input, if the stack is not empty report an error
+// Time Complexity: O(n). Since we are scanning the input only once. Space Complexity: O(n) [for stack].
 func IsValid(s string) bool {
 	stack := NewStack(1)
 	for _, r := range s {
