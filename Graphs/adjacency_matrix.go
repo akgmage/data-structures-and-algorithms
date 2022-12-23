14ddff06a2df29f3a7353a241e906f2fc321b05e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-12-23 22:04:40 +0530

    add graph interface

--- Graphs/adjacency_matrix.go
+++ Graphs/adjacency_matrix.go
@@ -10,3 +10,16 @@ const (
 DIRECTED GraphType = "DIRECTED"
 UNDIRECTED GraphType = "UNDIRECTED"
 )
+type Graph interface {
+	Init()
+	AddEdge(vertexOne int, vertexTwo int) error
+	AddEdgeWithWeight(vertexOne int, vertexTwo int, weight int) error
+	RemoveEdge(vertexOne int, vertexTwo int) error
+	HasEdge(vertexOne int, vertexTwo int) bool
+	GetGraphType() GraphType
+	GetAdjacentNodesForVertex(vertex int) map[int]bool
+	GetWeightOfEdge(vertexOne int, vertexTwo int) (int, error)
+	GetNumberOfVertices() int
+	GetNumberOfEdges() int
+	GetIndegreeForVertex(vertex int) int
+}
