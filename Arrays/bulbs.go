27f460d50acd15e5df7f118a0f1cc7eacfb623a6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-18 22:42:33 +0530

    add description

--- Arrays/bulbs.go
+++ Arrays/bulbs.go
@@ -1,4 +1,11 @@
-package main
+/*
+	N light bulbs are connected by a wire.
+	Each bulb has a switch associated with it, however due to faulty wiring, a switch also changes the state of all the bulbs to the right of current bulb.
+	Given an initial state of all bulbs, find the minimum number of switches you have to press to turn on all the bulbs.
+	You can press the same switch multiple times.
+	Note : 0 represents the bulb is off and 1 represents the bulb is on.
+*/
+ackage main
 
 import "fmt"
 
