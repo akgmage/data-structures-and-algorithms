a29141a534a7a78ed1cc980e02c89e555367f7ae
uday510 <admin@udayteja.com>
2023-01-20 20:21:01 +0530

    Added ThreeNumberSum with optimal solution.

--- HashTable/three_number_sum.java
+++ HashTable/three_number_sum.java
@@ -1,32 +1,48 @@
-// https://leetcode.com/problems/3sum/
-// O(N(log(N)) time | O(1) space
-var threeSum = function(nums) {
-        const triplets = [];
-        nums.sort((a, b) => a - b);
-        for(let i = 0; i < nums.length - 2; i++) {
-        if( (i > 0) && nums[i] == nums[i - 1]) {
-        continue;
-        }
-        let left = i + 1;
-        let right = nums.length - 1;
+import java.util.*;
 
-        while(left < right) {
-        const currentSum = nums[i] + nums[left] + nums[right];
-        if(currentSum == 0) {
-        triplets.push([nums[i], nums[left], nums[right]]);
-        while(left < right && nums[left] == nums[left + 1]) left++;
-        while(left < right && nums[right] == nums[right - 1]) right--;
-        left++;
-        right--;
-        } else if(currentSum < 0) {
-        left++;
-        } else if(currentSum > 0) {
-        right--;
-        }
-        }
+/**
+ *
+
+ Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. The function should find all triplets in the array that sum up to the target sum and return a two-dimensional array of all these triplets. The numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold.
+
+ If no three numbers sum up to the target sum, the function should return an empty array.
+ Sample Input
+
+ array = [12, 3, 1, 2, -6, 5, -8, 6]
+ targetSum = 0
+
+ Sample Output
+
+ [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+
+
+ */
+public class ThreeNumberSum {
+    // O(n^2) time | O(n) space
+    public static void main(String[] args){
+        int[] array = {12, 3, 1, 2, -6, 5, -8, 6};
+        int targetSum = 0;
+        List<Integer[]> triplets = solve(array, targetSum);
+        for (var triplet: triplets) System.out.println(Arrays.toString(triplet));
+    }
+    public static List<Integer[]> solve(int[] array, int targetSum) {
+        Arrays.sort(array);
+        List<Integer[]> triplets = new ArrayList<>();
+
+        for(int i = 0; i < array.length - 2; i++) {
+            int left = i + 1;
+            int right = array.length - 1;
+            while(left < right) {
+                int currentSum = array[i] + array[left] + array[right];
+                if(currentSum == targetSum) {
+                    Integer[] newTriplet = {array[i], array[left], array[right]};
+                    triplets.add(newTriplet);
+                    left++;
+                    right--;
+                } else if(currentSum < targetSum) left++;
+                else right--;
+            }
         }
         return triplets;
-        };
-        const array = [-1,0,1,2,-1,-4];
-
-        console.log(threeSum(array));
+    }
+}
