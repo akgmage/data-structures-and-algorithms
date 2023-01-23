a739004dd4a3b81096269e3229adda6354391703
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-01-23 23:06:51 +0530

    add description

--- Binary_Search/binary_search_recursive.go
+++ Binary_Search/binary_search_recursive.go
@@ -1,3 +1,15 @@
+/*
+	In computer science, binary search, also known as half-interval search,
+	logarithmic search, or binary chop, is a search algorithm that
+	finds the position of a target value within a sorted array.
+	Binary search compares the target value to the middle element of the array.
+	If they are not equal, the half in which the target cannot lie is eliminated
+	and the search continues on the remaining half,
+	again taking the middle element to compare to the target value,
+	and repeating this until the target value is found.
+	If the search ends with the remaining half being empty, the target is not in the array.
+	Source(https://en.wikipedia.org/wiki/Binary_search_algorithm)
+*/
 package main
 
 import "fmt"
@@ -8,9 +20,9 @@ func BinarySearch(Arr []int, key int) bool {
 	if low <= high {
 		mid := (high + low) / 2
 		if Arr[mid] > key {
-			return BinarySearch(Arr[:mid], key)
+			return BinarySearch(Arr[:mid], key) // search from start to mid 
 		} else if Arr[mid] < key {
-			return BinarySearch(Arr[mid + 1:], key)
+			return BinarySearch(Arr[mid + 1:], key) // search from mid + 1 to last
 		} else {
 			return true
 		}
