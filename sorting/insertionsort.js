ed1c360d10da95325e564282864aa29da74273da
Ashish Raj <82704654+7flash-pi@users.noreply.github.com>
2023-01-24 23:46:18 +0530

    add description

--- sorting/insertionsort.js
+++ sorting/insertionsort.js
@@ -1,13 +1,21 @@
-//insertionsort
+/* 
+Insertion sort is a sorting algorithm that places an unsorted element 
+at its suitable place in each iteration.
+*/
 const inputArr = [4,5,67,56,3,35,45];
 
 const insertionSort= (arr) => {
+
+    //loop to access each element in the array
     for (let i = 1; i < arr.length; i++) {
       let currentValue = arr[i];
       const j = 0;
-      for (let j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
+          
+    // loop to Compare key with each element on the left of it until an element smaller than is found
+    for (let j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
         arr[j + 1] = arr[j];
       }
+      // Place key at after the element just smaller than it.
       arr[j + 1] = currentValue;
     }
     return arr;
