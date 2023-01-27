8cdee3220d7c28eb4112e6e460af175a105f6151
Madhav Setia <103564105+maddy020@users.noreply.github.com>
2023-01-27 22:50:41 +0530

    Update 4Sum.cpp

--- Random_Problems/4Sum.cpp
+++ Random_Problems/4Sum.cpp
@@ -1,3 +1,17 @@
+// Problem Name-4Sum
+
+// Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+// 0 <= a, b, c, d < n
+// a, b, c, and d are distinct.
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+// You may return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,0,-1,0,-2,2], target = 0
+// Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
