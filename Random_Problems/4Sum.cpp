83a49d603eedc9f229e2cf6bf64beeccd20f9845
unknown <madhavsetia24@gmail.com>
2023-01-27 22:35:45 +0530

    4sum code added CPP

---
+++ Random_Problems/4Sum.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        set<vector<int>> st;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int start = j+1,end = n-1;
+                while(start<end){
+                    long long crSum = (long long)nums[i]+nums[j]+nums[start]+nums[end];
+                    if(crSum == target){
+                        st.insert({nums[i],nums[j],nums[start],nums[end]});
+                        end--;
+                    }else if(crSum>target) end--;
+                    else start++;
+                }
+            }
+            
+        }
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+    }
+  int main(){
+    int n;
+    cin>>n;
+    vector<int>nums;
+    int target;
+    cin>>target;
+    vector<vector<int>>res;
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    res=fourSum(nums,target);
+     for(int i = 0; i < res.size(); i++){
+        cout<<res[i];
+    }
+    return 0;
+}
+
