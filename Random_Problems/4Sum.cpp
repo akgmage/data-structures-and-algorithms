ab98ce94c2eec67591db2910088b35284db3b74f
Madhav Setia <103564105+maddy020@users.noreply.github.com>
2023-01-27 22:56:43 +0530

    Update 4Sum.cpp

--- Random_Problems/4Sum.cpp
+++ Random_Problems/4Sum.cpp
@@ -25,14 +25,15 @@ public:
                 int start = j+1,end = n-1;
                 while(start<end){
                     long long crSum = (long long)nums[i]+nums[j]+nums[start]+nums[end];
-                    if(crSum == target){
+                    if(crSum == target)
+                    {
                         st.insert({nums[i],nums[j],nums[start],nums[end]});
                         end--;
-                    }else if(crSum>target) end--;
+                    }
+                    else if(crSum>target) end--;
                     else start++;
                 }
             }
-            
         }
         vector<vector<int>> ans(st.begin(),st.end());
         return ans;
