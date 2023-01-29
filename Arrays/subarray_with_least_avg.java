bffe07e6e94a9da2bc1bdd3dff8e7306417e21f1
uday510 <admin@udayteja.com>
2023-01-29 13:13:08 +0530

    Added SubArrayWithLeastAverage using constant space Solution.

--- Arrays/subarray_with_least_avg.java
+++ Arrays/subarray_with_least_avg.java
@@ -50,35 +50,40 @@ package SlidingWindow;
 
 public class SubArrayWithLeastAverage {
     public static void main(String[] args) {
-//        int[] arr = {3, 7, 90, 20, 10, 50, 40};
-        int[] arr = {3, 7, 5, 20, -10, 0, 12};
-        int size = 2;
+//        int[] arr = {3, 7, 90, 20, -10, 0, 12};
+        int[] arr = {431, 313, 53, 251, 105, 423, 326, 297,
+                218, 89, 394, 365, 348, 474, 157, 262, 33, 187,
+                67, 79, 495, 199, 175, 228, 27, 305, 496, 331,
+                40, 98, 405, 221, 327, 488, 252, 73, 218, 152,
+                313, 274, 195, 353, 225, 292, 426, 257, 418,
+                364, 344, 349, 181};
+        int size = 12;
         int ans = solve(arr, size);
         System.out.println(ans);
     }
     public static int solve(int[] arr, int size) {
         // O(N) time | O(1) space
-        int sum = 0;
-        int ans = 0;
+        int currentSum = 0;
+        int idx = 0;
         int len = arr.length;
 
         // sliding window
         for (int i = 0; i < size; i++)
-            sum += arr[i];
+            currentSum += arr[i];
 
         int startIdx = 1;
         int endIdx = size;
-
+        int minSum = currentSum;
         // slide remaining windows
         while (endIdx < len) {
-            int currentSum = sum + arr[endIdx] - arr[startIdx - 1];
-            if ((currentSum / 3 ) < (sum / 3)) {
-                ans = startIdx;
+            currentSum = currentSum + arr[endIdx] - arr[startIdx - 1];
+            if (currentSum < minSum) {
+                minSum = currentSum;
+                idx = startIdx;
             }
             startIdx++;
             endIdx++;
-            sum = currentSum;
         }
-        return ans;
+        return idx;
     }
 }
