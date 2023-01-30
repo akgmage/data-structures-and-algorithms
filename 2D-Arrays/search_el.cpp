42e5093fe334f5ca3bd55b88f3686a1341d089e7
Ujjawal Tyagi <tyagiujjawal763@gmail.com>
2023-01-30 06:06:41 +0000

    space updated

--- 2D-Arrays/search_el.cpp
+++ 2D-Arrays/search_el.cpp
@@ -15,15 +15,15 @@ Output: false*/
 
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool searchMatrix(vector<vector<int>> &matrix,int target) {
         int i = 0;
-        int j = matrix[0].size()-1; 
+        int j = matrix[0].size() - 1; 
         
-        while(i<matrix.size() && j>=0)
+        while(i < matrix.size() && j >= 0)
         {
-            if(target==matrix[i][j])
+            if(target == matrix[i][j])
                 return true;         
-            else if(target<matrix[i][j])
+            else if(target < matrix[i][j])
                 j--;
             else 
                 i++;
--- Arrays/majority_el.cpp
+++ Arrays/majority_el.cpp
@@ -18,11 +18,11 @@ Output: 2 */
 class Solution {
 public:
     int majorityElement(vector<int>& arr) {
-        int ele=arr[0];
-        int count=0;
-        for(int i=0;i<arr.size();i++){
-            if(count==0)ele=arr[i];
-            count+=(ele==arr[i])?1:-1;
+        int ele = arr[0];
+        int count = 0;
+        for(int i = 0;i < arr.size();i++){
+            if(count == 0) ele = arr[i];
+            count += (ele == arr[i]) ? 1 : -1;
         }
         return ele;
     }
