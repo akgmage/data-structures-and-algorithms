97e7a10f576eb844f3c6c3181f96bf9c5e5cab9d
Kamal Pratik <kamalpratik144@gmail.com>
2023-02-05 03:48:21 +0530

    Added a more readable solution of Integer to Roman java solution

--- HashTable/integer_to_roman.java
+++ HashTable/integer_to_roman.java
@@ -41,7 +41,7 @@ Constraints:
 class Solution {
     public String intToRoman(int num) {
         int numsVal[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] romans = {"M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" , "IV" , "I"};
         StringBuilder sb = new StringBuilder();        
         for (int i = 0; i < numsVal.length; i++) {
             while (num >= numsVal[i]) {
