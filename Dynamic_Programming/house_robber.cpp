be7005a416ab24c7f7106f7ad15c776c0c66ba1a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-08 22:36:32 +0530

    add house robber problem

---
+++ Dynamic_Programming/house_robber.cpp
@@ -0,0 +1,54 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+*/
+
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+       /*
+        if(len == 0) return 0;
+        if(len == 1) return nums[0];
+        if(len == 2) return max(nums[0], nums[1]);
+        int dp[len];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < len; i++){
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[len-1];
+        */
+        int prev = 0, curr = 0, temp = 0;
+        for(int i = 0; i < len; i++){
+            temp = max(nums[i] + prev, curr);
+            prev = curr;
+            curr = temp;
+        }
+        return curr;
+    }
+};
