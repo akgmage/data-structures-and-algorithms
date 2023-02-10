66e51e8a3a2a89b2583659ad9930dd0978c55e28
Aditya Mukherjee <adimukh2001@gmail.com>
2023-02-10 15:08:47 +0530

    Added Code and Explanation

--- Leetcode/Median_of_Two_Sorted_Arrays.cpp
+++ Leetcode/Median_of_Two_Sorted_Arrays.cpp
@@ -6,26 +6,12 @@ Approach:
     This will have a time Complexity of O(n1 + n2), Space Complexity of O(n1 + n2)
 
     Now, lets optimise it.
-    So, the sorted form of the given array is arr = [1, 2, 3, 3, 4, 6, 7, 10, 12, 15]. To find the median of the array, we need to select the 2 mid elements and average it out.
-    If we observe the sorted array carefully, then we notice that the 2 middle elements are arr[4] = 4 and arr[5] = 6, => arr[4] <= arr[5].
-    
-    Thought process:
-    Now, since the arrays are sorted, so the binary searh may be able to solve the problem.
-    Observation 1: If we partition the sorted array arr into 2 halves, then for sure we know that there would be 5 elements on left half and 5 elements on right half.
-        Now, we can select 5 elements for right half from nums1 and nums2 combinedly and similarly the rest of the elements for the left half.
-        Example:
-            1. left => [1, 3, 4, 7, 2], right => [10, 12, 3, 6, 15]
-            2. left => [1, 3, 4, 2, 3], right => [7, 10, 12, 6, 15]
-            3. left => [1, 3, 2, 3, 6], right => [4, 7, 10, 12, 15]
-    
-    Observation 2: All the elements on left half is lesser than all the elements on the right half.
-        Now, according to the observation, I have to check that all the elements in the left <= all the elements in the right. This can be done by just comparing the maximum of left half <= minimum of right half.
-    
-    Hence, the problem boils down to a searching problem; but how to identify the binary search approach??
-        Suppose, we partition the element as example 1, then the max of left[] = 7 and min of right[] = 3, but according to the 2nd observation, it is not valid. So, in order to have a correct max in left, I need to reduce its value and consequestly, the min of right[] should be increased. That means, I have to move leftwards in nums1 to have a correct max value in left half.
-*/
+    So, the sorted form of the given array is [1, 2, 3, 3, 4, 6, 7, 10, 12, 15]. To find the median of the array, we need to select the 2 mid elements and average it out. Now suppose, on partitioning the above merged-sorted array in the mid-point, we get 5 elements on left and 5 elements on the right.
+    Now, we can get 5 elements by selecting {4 from left, 1 from right}, {3 from left, 2 from right}, {2 from left, 3 from right} and {1 from left, 4 from right}.
+    Lets analyse case-wise:
+        case 1: 4 from left, 1 from right
 
-// Leetcode link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+*/
 
 #include<bits/stdc++.h>
 
@@ -42,8 +28,9 @@ public:
         while(lo <= hi){
             int mid1 = (lo+hi)/2; // mid of nums1
             int mid2 = (n1 + n2 + 1)/2 - mid1;
+            // why?? => suppose nums1[] is partitioned at index 3 => nums1[3] = 7.
 
-            std::pair<int, int> maxleft, minright;
+            std::pair<int, int> maxleft, maxright;
             maxleft.first = mid1 == 0 ? INT_MIN : nums1[mid1-1];
             maxleft.second = mid2 == 0 ? INT_MIN : nums2[mid2-1];
 
@@ -81,7 +68,7 @@ int main(int argc, char const *argv[])
         std::cin>>arr2[i];
     }
 
-    Solution sol = Solution();
+    const Solution sol = new Solution();
     double res = sol.findMedianSortedArrays(arr1, arr2);
 
     std::cout<<res<<"\n";
