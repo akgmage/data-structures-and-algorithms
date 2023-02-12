8b41570076422aa1335e1125c12ee3e490dff87a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-12 21:19:05 +0530

    add description

--- Linked List/reverse_linked_list.cpp
+++ Linked List/reverse_linked_list.cpp
@@ -1,3 +1,32 @@
+
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+*/
+
 //iterative solution-----
 
 ListNode* reverseList(ListNode* head) {
