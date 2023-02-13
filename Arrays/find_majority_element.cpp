a76fabe5f7991c0d648c57edefe3b769da73fd70
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-13 23:09:17 +0530

    add description and change file name

--- Arrays/find_majority_element.cpp
+++ Arrays/find_majority_element.cpp
@@ -1,9 +1,35 @@
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+*/
+
 //approach----
 //here we make a map which maintain the frequency of each array element while traversing through the element.
 //and also check the frequency at the same time..if frequency(occurence) of any  element is  greater than (n/2) at anytime we will return that element.
 // suppose if no such element exist return simply -1.
 
-code----
 Find Majority Element in C++ 
 class Solution {
 public:
