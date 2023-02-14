0d45918a9bc03e810656029d9ac6e46e3205a56b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-14 18:47:04 +0530

    rename file

--- Arrays/find_no_of_good_pairs.java
+++ Arrays/find_no_of_good_pairs.java
@@ -10,7 +10,8 @@ A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
 Example 1:
 
-Input: nums = [1,2,3,1,1,3]
+Input: num
+s = [1,2,3,1,1,3]
 Output: 4
 Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 Example 2:
