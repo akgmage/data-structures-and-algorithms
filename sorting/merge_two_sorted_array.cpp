d2367e0b718c4541cb142513e5d5ff0896c5760e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-15 22:17:15 +0530

    reformat code

--- sorting/merge_two_sorted_array.cpp
+++ sorting/merge_two_sorted_array.cpp
@@ -24,25 +24,26 @@
 // Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 
 // The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1
-//                                 -   - -
+
+// Time complexity of this code is O(m+n)
+// Space complexity is O(1)
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-           int tar = m+n-1;  // size of nums1 
+        int tar = m + n - 1;  // size of nums1 
 
-           int i=m-1;        // index of last element of nums1
+        int i = m - 1;        // index of last element of nums1
 
-            int j = n-1;     // index of last element of nums2
+        int j = n - 1;     // index of last element of nums2
 
-            while(j>=0){    // iterate from last
+        while(j >= 0) {    // iterate from last
 
-                   if(i>=0 && nums1[i] > nums2[j]) nums1[tar--] = nums1[i--]; 
-
-                   else nums1[tar--] = nums2[j--];
+            if( i >= 0 && nums1[i] > nums2[j]) 
+                nums1[tar--] = nums1[i--]; 
+            else 
+                nums1[tar--] = nums2[j--];
                            
         }
-        // Time complexity of this code is O(m+n)
-        
-        // Space complexity is S(1)
     }
 };
