144436fd8dc0a934975cc94d156de6f135c860cd
Manuel Cota <108767897+MaAnCoSa@users.noreply.github.com>
2023-02-16 14:42:04 -0700

    Number of Steps to Reduce a Number to Zero in Java
    Number of Steps to Reduce a Number to Zero in Java
    Added a solution to the problem from leetcode: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
    Answer to issue #615.

---
+++ Math/num_steps_reduce_to_zero.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        // We initialize the step counter.
+        int steps = 0;
+        // While the number is still not zero...
+        while (num != 0) {
+            // ...we check if it is odd or even.
+            // If it is even...
+            if (num % 2 == 0) {
+                // ...we divide it by 2 and increase the step counter.
+                num /= 2;
+                steps++;
+            // If it is odd...
+            } else {
+                // ...we substract 1 and increase the step counter.
+                num -= 1;
+                steps++;
+            }
+        }
+        // Finally, we return the step counter.
+        return steps;
+    }
+}
