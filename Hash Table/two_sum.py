0663b3ddf0072c4a8e7a3b39f938f3005e8f7944
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-18 20:02:32 +0530

    add brute force approach of two sum in py

---
+++ Hash Table/two_sum.py
@@ -0,0 +1,18 @@
+'''
+  Write a function that takes in a non-empty array of distinct integers and an
+  integer representing a target sum. If any two numbers in the input array sum
+  up to the target sum, the function should return them in an array, in any
+  order. If no two numbers sum up to the target sum, the function should return
+  an empty array.
+  Sample Input: [2, 1, 3, -1, 11, 5, 4, 0] Target: 10
+  Output: [-1 11]
+'''
+# Brute Force Approach
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+    
