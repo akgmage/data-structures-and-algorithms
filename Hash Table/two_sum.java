67929f6f20d9792d016a9c0a8bfb61a50bb68a3d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-18 20:11:19 +0530

    reformat code

--- Hash Table/two_sum.java
+++ Hash Table/two_sum.java
@@ -1,31 +1,40 @@
+/*
+  Write a function that takes in a non-empty array of distinct integers and an
+  integer representing a target sum. If any two numbers in the input array sum
+  up to the target sum, the function should return them in an array, in any
+  order. If no two numbers sum up to the target sum, the function should return
+  an empty array.
+  Sample Input: [2, 1, 3, -1, 11, 5, 4, 0] Target: 10
+  Output: [-1 11]
+*/
 import java.util.*;
 
-/**
- *
-
- Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order. If no two numbers sum up to the target sum, the function should return an empty array.
-
- Note that the target sum has to be obtained by summing two different integers in the array; you can't add a single integer to itself in order to obtain the target sum.
-
- You can assume that there will be at most one pair of numbers summing up to the target sum.
- Sample Input
-
- array = [3, 5, -4, 8, 11, 1, -1, 6]
- targetSum = 10
-
- Sample Output
-
- [-1, 11] // the numbers could be in reverse order
-
-
- */
 public class TwoNumberSum {
+    
     public static void main(String[] args) {
         int[] array = {3, 5, -4, 8, 11, 1, -1, 6};
         int targetSum = 10;
     }
-    public static int[] twoNumberSumOptimal(int[] array, int targetSum) {
-// O(nlog(n)) time | O(1) space
+    
+    // Brute Force Solution
+    // Time complexity: O(N^2);
+    // Space Complexity: O(1);
+    public static int[] TwoSumBruteForce(int[] array, int targetSum) {
+        for(int i = 0; i < array.length - 1; i++) {
+            int firstNum = array[i];
+            for(int j = i + 1; j < array.length; j++) {
+                int secondNum = array[j];
+                if(firstNum + secondNum == targetSum)
+                    return new int[] {firstNum, secondNum};
+            }
+        } return new int[0];
+    }
+
+    // Sorting Solution
+    // Time complexity: O(N log N);
+    // Space Complexity: O(1);
+    public static int[] TwoSumBySorting(int[] array, int targetSum) {
+    // O(nlog(n)) time | O(1) space
         Arrays.sort(array);
         int left = 0;
         int right = array.length - 1;
@@ -38,24 +47,16 @@ public class TwoNumberSum {
             else right -= 1;
         } return new int[0];
     }
-    public static int[] twoNumberSumUsingSet(int[] array, int targetSum) {
-// O(n) time | O(n) space
+    
+    // Optimal Solution
+    // Time complexity: O(N);
+    // Space Complexity: O(N);
+    public static int[] TwoSumOptimal(int[] array, int targetSum) {
         Set<Integer> nums = new HashSet<>();
         for(int num: array) {
             int potentialMatch = targetSum - num;
             if(nums.contains(potentialMatch)) return new int[] {num, potentialMatch};
             nums.add(num);
-        } return new int[0];
-    }
-    public static int[] twoNumberSumBruteForce(int[] array, int targetSum) {
-// O(n^2) time | O(1) space
-        for(int i = 0; i < array.length - 1; i++) {
-            int firstNum = array[i];
-            for(int j = i + 1; j < array.length; j++) {
-                int secondNum = array[j];
-                if(firstNum + secondNum == targetSum)
-                    return new int[] {firstNum, secondNum};
-            }
         } return new int[0];
     }
 }
