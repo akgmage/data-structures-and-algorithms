aff9b697fc4bab91c9d4f5076f452a03cf7c42d9
Arpit Sharma <arpit.s@neodove.com>
2023-02-19 02:29:24 +0530

    Is prime optimized

--- Math/isPrime.js
+++ Math/isPrime.js
@@ -1,5 +1,6 @@
 // Whether the given number is a prime nujmber or not
 
+// Time complexity - O(n)
 function isPrime(input) {
     for (i = 2; i < input; i++) {
         if (input % i == 0) {
@@ -12,5 +13,19 @@ function isPrime(input) {
     return "Given number is a prime number";
 }
 
+// Time complexity - (O logn)
+function isPrimeLogn(input) {
+    for (i = 2; i < Math.sqrt(input); i++) {
+        if (input % i == 0) {
+            return "The number is not a prime number";
+            break;
+        } else {
+            continue;
+        }
+    }
+    return "Given number is a prime number";
+}
+
 // Driver code
 console.log(isPrime(6));
+console.log(isPrimeLogn(7));
