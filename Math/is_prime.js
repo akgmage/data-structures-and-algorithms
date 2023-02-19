81df52f14cbdf716c68a9d0576c59af561483783
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-19 13:01:59 +0530

    remove duplicate file

--- Math/is_prime.js
+++ Math/is_prime.js
@@ -25,3 +25,20 @@ const isPrime = (num) => {
 };
 
 console.log(isPrime(10));
+
+// Time complexity - O(logn)
+function isPrimeLogn(input) {
+  for (i = 2; i < Math.sqrt(input); i++) {
+    if (input % i == 0) {
+      return "The number is not a prime number";
+      break;
+    } else {
+      continue;
+    }
+  }
+  return "Given number is a prime number";
+}
+
+// Driver code
+console.log(isPrime(6));
+console.log(isPrimeLogn(7));
