147e8bb5e765624c611ee4040c9445215a476850
Tarun Samanta <tarunsamanta77@gmail.com>
2023-02-19 21:55:05 +0530

    added 2P

---
+++ Two Pointers/tripletSum.py
@@ -0,0 +1,36 @@
+# Python3 program to find a triplet sum in array
+def find3Numbers(A, arr_size, sum):
+
+	# Sort the elements
+	A.sort()
+
+	# Now fix the first element
+	# one by one and find the
+	# other two elements
+	for i in range(0, arr_size-2):
+		
+		# index of the first element
+		# in the remaining elements
+		l = i + 1
+		
+		# index of the last element
+		r = arr_size-1
+		while (l < r):
+		
+			if( A[i] + A[l] + A[r] == sum):
+				return True
+			elif (A[i] + A[l] + A[r] < sum):
+				l += 1
+			else: # A[i] + A[l] + A[r] > sum
+				r -= 1
+
+	# If we reach here, then
+	# no triplet was found
+	return False
+
+# Driver program to test above function
+A = [1, 2, 3, 4, 5, 6]
+sum = 9
+arr_size = len(A)
+
+print(find3Numbers(A, arr_size, sum))
