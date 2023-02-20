beecf30027dfe58a74b9cee0778613999fab46ad
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-20 23:10:50 +0530

    add comments

--- Strings/valid_pallindrome2.go
+++ Strings/valid_pallindrome2.go
@@ -16,6 +16,7 @@ package main
 
 func validate(s string, left int, right int) bool {
   for left < right {
+	// doesn't match then return false
     if s[left] != s[right] {
       return false
     } else {
@@ -27,13 +28,15 @@ func validate(s string, left int, right int) bool {
 }
 
 func validPalindrome(s string) bool {
+	// initialize two pointers at opposite end of string
     left := 0
     right := len(s) - 1
     for left < right {
+	  // if value at left and right index match then move them closer
       if s[left] == s[right] {
         left += 1
         right -= 1
-      } else {
+      } else { // if mismatch occurs then skip one and check rest
         skipLeft := validate(s, left + 1, right) // skip one from left and check remaining
         skipRight := validate(s, left, right - 1) // skip one from right and check remaining
         return skipLeft || skipRight // if either is true return true
