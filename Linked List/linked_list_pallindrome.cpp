8886e7d76b38860fe3faf1b4ff499b6eef634ebb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-21 23:36:15 +0530

    add description

--- Linked List/linked_list_pallindrome.cpp
+++ Linked List/linked_list_pallindrome.cpp
@@ -1,4 +1,30 @@
+/*
+    Given the head of a singly linked list, return true if it is a 
+    palindrome
+    or false otherwise.
 
+    
+
+    Example 1:
+
+
+    Input: head = [1,2,2,1]
+    Output: true
+    Example 2:
+
+
+    Input: head = [1,2]
+    Output: false
+    
+
+    Constraints:
+
+    The number of nodes in the list is in the range [1, 105].
+    0 <= Node.val <= 9
+    
+
+    Follow up: Could you do it in O(n) time and O(1) space?
+*/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,6 +35,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// TODO: Implement O(1) space
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
