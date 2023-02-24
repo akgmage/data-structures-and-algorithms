0d5e9e201b52622e53151deb26b122c852ccb5a6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-24 22:39:13 +0530

    add description

--- Math/count_primes.cpp
+++ Math/count_primes.cpp
@@ -1,3 +1,21 @@
+/*
+    Given an integer n, return the number of prime numbers that are strictly less than n.
+
+    Example 1:
+    Input: n = 10
+    Output: 4
+    Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+    Example 2:
+    Input: n = 0
+    Output: 0
+
+    Example 3:
+    Input: n = 1
+    Output: 0
+    Constraints:
+    0 <= n <= 5 * 106
+*/
 class Solution {
 public:
     int countPrimes(int n) {
