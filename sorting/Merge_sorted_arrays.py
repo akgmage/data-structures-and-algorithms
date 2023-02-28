8dead5fdae3f80a1ef7dbee3c53c27c7742889c6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-02-28 22:31:10 +0530

    rfmt code

--- sorting/Merge_sorted_arrays.py
+++ sorting/Merge_sorted_arrays.py
@@ -9,25 +9,25 @@ class Solution:
         #Using similar to merge sort algorithm implementation.
         #Traversing in reverse order and comparing m,n values.
         #Placing the highest element last and continuing with the algorithm. 
-        i,j,k=m-1,n-1,m+n-1
-        while(i>=0 and j>=0):
-            if(nums1[i]>nums2[j]):
-                nums1[k]=nums1[i]
-                i-=1
-                k-=1
+        i, j, k = m - 1, n - 1, m + n - 1
+        while(i >= 0 and j >= 0):
+            if(nums1[i] > nums2[j]):
+                nums1[k] = nums1[i]
+                i -= 1
+                k -= 1
             else:
-                nums1[k]=nums2[j]
-                j-=1
-                k-=1
+                nums1[k] = nums2[j]
+                j -= 1
+                k -= 1
         
         #placing left over elements from num1 to nums1
-        while(i>=0):
-            nums1[k]=nums1[i]
-            i-=1
-            k-=1
+        while(i >= 0):
+            nums1[k] = nums1[i]
+            i -= 1
+            k -= 1
         
         #placing left over elements from num2 to num1
-        while(j>=0):
-            nums1[k]=nums2[j]
-            j-=1
-            k-=1
+        while(j >= 0):
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
