9c9a76ed8253ded971acad1a60f417d50a89fa4b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-04 22:53:50 +0530

    add description

--- Backtracking/Generate_Parentheses.py
+++ Backtracking/Generate_Parentheses.py
@@ -1,3 +1,15 @@
+'''
+    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+    Example 1:
+    Input: n = 3
+    Output: ["((()))","(()())","(())()","()(())","()()()"]
+    
+    Example 2:
+    Input: n = 1
+    Output: ["()"]
+    Constraints:
+    1 <= n <= 8
+'''
 class Solution:
     #reference: GeeksforGeeks for comments
     '''To form all the sequences of balanced bracket subsequences with n pairs. So there are n opening brackets and n closing brackets. So the subsequence will be of length 2*n. There is a simple idea, the i’th character can be ‘{‘ if and only if the count of ‘{‘ till i’th is less than n and i’th character can be ‘}’ if and only if the count of ‘{‘ is greater than the count of ‘}’ till index i. If these two cases are followed then the resulting subsequence will always be balanced'''
