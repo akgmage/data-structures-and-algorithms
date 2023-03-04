f95df948d4f55135c200c88e09c9ce9dff6b5ebd
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-04 22:59:25 +0530

    add  description

--- Math/unique_digits.py
+++ Math/unique_digits.py
@@ -1,3 +1,18 @@
+'''
+    Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n.
+
+    Example 1:
+    Input: n = 2
+    Output: 91
+    Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+    
+    Example 2:
+    Input: n = 0
+    Output: 1
+    
+    Constraints:
+    0 <= n <= 8
+'''
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
         '''For n = 0, ans = 1
