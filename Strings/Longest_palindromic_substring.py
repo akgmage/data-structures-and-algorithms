232f9b656bfc05ceb269368ea18a2789380c03b4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-06 20:48:00 +0530

    add  description

--- Strings/Longest_palindromic_substring.py
+++ Strings/Longest_palindromic_substring.py
@@ -1,3 +1,19 @@
+'''
+    Given a string s, return the longest palindromic substring in s.
+
+    Example 1:
+    Input: s = "babad"
+    Output: "bab"
+    Explanation: "aba" is also a valid answer.
+    
+    Example 2:
+    Input: s = "cbbd"
+    Output: "bb"
+    
+    Constraints:
+    1 <= s.length <= 1000
+    s consist of only digits and English letters.
+'''
 class Solution:
     #Instead of checking if a value is palindrome from the end, we can consider each index to be the center
     #Then, Check if the left and right indexed values to it are different.
