a7dc1942132a370da10c5d8a084b3623d8f058d3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-08 23:18:19 +0530

    modify description

--- 2D Arrays (Matrix)/2d_binary_search.cpp
+++ 2D Arrays (Matrix)/2d_binary_search.cpp
@@ -1,17 +1,19 @@
-/*You are given an m x n integer matrix matrix with the following two properties:
+/*
+    You are given an m x n integer matrix matrix with the following two properties:
 
-Each row is sorted in non-decreasing order.
-The first integer of each row is greater than the last integer of the previous row.
-Given an integer target, return true if target is in matrix or false otherwise.
+    Each row is sorted in non-decreasing order.
+    The first integer of each row is greater than the last integer of the previous row.
+    Given an integer target, return true if target is in matrix or false otherwise.
 
-You must write a solution in O(log(m * n)) time complexity.
+    You must write a solution in O(log(m * n)) time complexity.
 
 
-1.Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
-Output: true
+    1.Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+    Output: true
 
-2.Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
-Output: false*/
+    2.Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+    Output: false
+*/
 
 class Solution {
     bool costumBinarySearch(const vector<vector<int>>& matrix, int target, int low, int high){
