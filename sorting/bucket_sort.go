450c829f8e2471d2da2d078aae9efb3a862a738c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-12 21:53:36 +0530

    add description

--- sorting/bucket_sort.go
+++ sorting/bucket_sort.go
@@ -1,3 +1,25 @@
+/*
+	Bucket sort, or bin sort, is a sorting algorithm that works by distributing 
+	the elements of an array into a number of buckets. Each bucket is then 
+	sorted individually, either using a different sorting algorithm, 
+	or by recursively applying the bucket sorting algorithm. 
+	It is a distribution sort, a generalization of pigeonhole sort that 
+	allows multiple keys per bucket, and is a cousin of 
+	radix sort in the most-to-least significant digit flavor. 
+	Bucket sort can be implemented with comparisons and therefore can also 
+	be considered a comparison sort algorithm. 
+	The computational complexity depends on the algorithm used to sort 
+	each bucket, the number of buckets to use, 
+	and whether the input is uniformly distributed.
+
+	Bucket sort works as follows:
+
+	Set up an array of initially empty "buckets".
+	Scatter: Go over the original array, putting each object in its bucket.
+	Sort each non-empty bucket.
+	Gather: Visit the buckets in order and put all elements back into the original array.
+*/
+
 package main
 
 import (
