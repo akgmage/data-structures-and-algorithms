7d51e14ab29385da75a9771c60e9a89f512c9c95
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-15 20:52:43 +0530

    add description

--- Trees/Binary Trees/branch_sum.go
+++ Trees/Binary Trees/branch_sum.go
@@ -1,6 +1,14 @@
+/*
+  Write a function that takes in a Binary Tree and returns a list of its branch
+  sums ordered from leftmost branch sum to rightmost branch sum.
+
+  A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree
+  branch is a path of nodes in a tree that starts at the root node and ends at
+  any leaf node.
+
+*/
 package main
 
-// This is the struct of the input root. Do not edit it.
 type BinaryTree struct {
 	Value int
 	Left  *BinaryTree
