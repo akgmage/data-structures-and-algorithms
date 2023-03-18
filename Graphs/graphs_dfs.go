1df515a2eb7b9e8a4bd66953fd5a74ffb2fb8dee
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-18 22:49:08 +0530

    add approach and time and space complexity

--- Graphs/graphs_dfs.go
+++ Graphs/graphs_dfs.go
@@ -10,6 +10,15 @@
     I   J   K
 	Output: ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"]
 */
+/*
+	Start at the root Node and try simply calling the depthFirstSearch method on all of its children Nodes.
+	Then, call the depthFirstSearch method on all children Nodes of each child node.
+	Keep applying this logic until the entire graph has been traversed.
+	Don't forget to add the current Node's name to the input array at every call of depthFirstSearch.
+
+	Time and Space complexity:  O(v + e) time | O(v) space - where v is the number of vertices of the input graph and e is the number of edges of the input graph
+*/
+
 package main
 
 import "fmt"
