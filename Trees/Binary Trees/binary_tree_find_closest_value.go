fa123dcb59dc0077996ddbde60997b1553892ece
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-18 22:33:04 +0530

    add find closest value in binary search tree

---
+++ Trees/Binary Trees/binary_tree_find_closest_value.go
@@ -0,0 +1,33 @@
+// Find Closest Value In BST
+package main
+
+type BST struct {
+	Value int
+
+	Left  *BST
+	Right *BST
+}
+
+func (tree *BST) FindClosestValue(target int) int {
+	// Write your code here.
+    return tree.findClosestValue(target, tree.Value)
+}
+
+func (tree *BST) findClosestValue(target, closest int) int {
+    if absDiff(target, closest) > absDiff(target, tree.Value) {
+        closest = tree.Value
+    }
+    if target < tree.Value && tree.Left != nil {
+        return tree.Left.findClosestValue(target, closest)
+    } else if target > tree.Value && tree.Right != nil {
+        return tree.Right.findClosestValue(target, closest)
+    }
+    return closest
+}
+
+func absDiff(a, b int) int {
+    if a > b {
+        return a - b
+    }
+    return b - a
+}
