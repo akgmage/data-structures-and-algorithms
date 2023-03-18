28b62697e4ac4fa7504165891bcdeaca3951fc59
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-18 22:37:58 +0530

    add approach

--- Trees/Binary Trees/binary_tree_find_branch_sum.go
+++ Trees/Binary Trees/binary_tree_find_branch_sum.go
@@ -2,7 +2,7 @@
 	Write a function that takes in a Binary Tree and returns a list of its branch
 	sums ordered from leftmost branch sum to rightmost branch sum.
 
-	
+
 	A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree
 	branch is a path of nodes in a tree that starts at the root node and ends at
 	any leaf node.
@@ -18,6 +18,13 @@
 
  	Output:[15, 16, 18, 10, 11]
 
+*/
+
+/*
+	As you recursively traverse the tree, if you reach a leaf node (a node with no "left" or "right" Binary Tree nodes),
+	add the relevant running sum that you've calculated to a list of sums (which you'll also have to pass to the recursive
+	function). If you reach a node that isn't a leaf node, keep recursively traversing its children nodes,
+	passing the correctly updated running sum to them.
 */
 package main
 
