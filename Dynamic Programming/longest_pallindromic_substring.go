3c8fca458b3afa513443be3c769e5a04b3211685
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-19 19:40:40 +0530

    add description

--- Dynamic Programming/longest_pallindromic_substring.go
+++ Dynamic Programming/longest_pallindromic_substring.go
@@ -1,3 +1,21 @@
+/*
+	Given a string s, return the longest palindromic substring in s.
+
+
+	Example 1:
+	Input: s = "babad"
+	Output: "bab"
+	Explanation: "aba" is also a valid answer.
+
+	Example 2:
+	Input: s = "cbbd"
+	Output: "bb"
+
+
+	Constraints:
+	1 <= s.length <= 1000
+	s consist of only digits and English letters.
+*/
 package main
 
 import (
