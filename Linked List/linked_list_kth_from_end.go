79589ecb4f0105918b1dd0597bc264edeaf485dd
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-20 23:07:00 +0530

    add time and space complexity

--- Linked List/linked_list_kth_from_end.go
+++ Linked List/linked_list_kth_from_end.go
@@ -8,6 +8,16 @@ Approach
 3. Now, move the fast pointer and the slow pointer each one step at a time till
    the fast pointer reaches the tail of the linked list. When the fast pointer reaches the tail,
    the node at which the slow pointer resides will be our answer.
+
+Time and space complexity
+
+	Time complexity of the solution is O(n) as we have traversed the linked list once.
+	We have traversed it in two parts.
+	First we traversed k elements and then we traversed the remaining (size minus k) elements.
+
+	Space complexity is O(1) as we have not used any extra space for the solution.
+
+
 */
 func kthFromEnd(head *ListNode, n int) *ListNode {
     first, second := head, head
