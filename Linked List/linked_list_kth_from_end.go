ef471ea872f0f4d0e56ef0630d2cb8b60d68780d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-03-20 23:05:22 +0530

    add approach

--- Linked List/linked_list_kth_from_end.go
+++ Linked List/linked_list_kth_from_end.go
@@ -1,6 +1,14 @@
 // Finding Kth node from end of a LinkedList
 package main
 
+/*
+Approach
+1. Make two pointers slow and fast and keep them both on the head of the linked list.
+2. Now, move the fast pointer k steps away from the slow pointer.
+3. Now, move the fast pointer and the slow pointer each one step at a time till
+   the fast pointer reaches the tail of the linked list. When the fast pointer reaches the tail,
+   the node at which the slow pointer resides will be our answer.
+*/
 func kthFromEnd(head *ListNode, n int) *ListNode {
     first, second := head, head
     for ; n > 0; n-- {
