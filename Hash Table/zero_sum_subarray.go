0ea227e2aa157ec7ec07ede0f1f9d1eadd9f5d9d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-02 11:52:14 +0530

    add main func with test cases

--- Hash Table/zero_sum_subarray.go
+++ Hash Table/zero_sum_subarray.go
@@ -1,24 +1,27 @@
 /*
-	You're given a list of integers nums. Write a function that returns a boolean representing 
+	You're given a list of integers nums. Write a function that returns a boolean representing
 	whether there exists a zero-sum subarray of nums
 
 	Sample Input :  = [-5, -5, 2, 3, -2]
 	Output : True
-	The subarray [-5, 2, 3] has a sum of 0 
+	The subarray [-5, 2, 3] has a sum of 0
 */
 /*
 Approach:
 
   A good way to approach this problem is to first think of a simpler version.
   How would you check if the entire array sum is zero?
-  
+
   If the entire array does not sum to zero, then you need to check if there are
   any smaller subarrays that sum to zero. For this, it can be helpful to keep
   track of all of the sums from [0, i], where i is every index in the array.
-  
+
   After recording all sums from [0, i], what would it mean if a sum is repeated?
 */
+// Time and Space complexity : O(n) time | O(n) space - where n is the length of nums
 package main
+
+import "fmt"
 
 func ZeroSumSubarray(nums []int) bool {
 	sums := map[int]bool{0: true}
@@ -29,7 +32,13 @@ func ZeroSumSubarray(nums []int) bool {
             return true
         }
         sums[currentSum] = true
-        print(currentSum , " ")
     }
     return false
+}
+
+func main() {
+	nums := []int{-5, -5, 2, 3, -2}
+	fmt.Println(ZeroSumSubarray(nums))
+	nums = []int{1, 2, 3, 4, 5}
+	fmt.Println(ZeroSumSubarray(nums))
 }
