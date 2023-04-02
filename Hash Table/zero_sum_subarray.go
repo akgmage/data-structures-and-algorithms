167f0fdd20d0fcfd4ade25dd559b58685e383ad1
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-02 11:48:43 +0530

    add approach

--- Hash Table/zero_sum_subarray.go
+++ Hash Table/zero_sum_subarray.go
@@ -6,7 +6,16 @@
 	Output : True
 	The subarray [-5, 2, 3] has a sum of 0 
 */
+/*
+Approach:
 
+  A good way to approach this problem is to first think of a simpler version.
+  How would you check if the entire array sum is zero?
+  
+  If the entire array does not sum to zero, then you need to check if there are
+  any smaller subarrays that sum to zero. For this, it can be helpful to keep
+  track of all of the sums from [0, i], where i is every index in the array.
+*/
 package main
 
 func ZeroSumSubarray(nums []int) bool {
