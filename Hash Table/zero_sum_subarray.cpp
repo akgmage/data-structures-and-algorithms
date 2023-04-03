ed8e4da7dd58aa9306f79fd26963b8153b05176b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-03 23:02:06 +0530

    add comments

--- Hash Table/zero_sum_subarray.cpp
+++ Hash Table/zero_sum_subarray.cpp
@@ -1,3 +1,25 @@
+/*
+	You're given a list of integers nums. Write a function that returns a boolean representing
+	whether there exists a zero-sum subarray of nums
+
+	Sample Input :  = [-5, -5, 2, 3, -2]
+	Output : True
+	The subarray [-5, 2, 3] has a sum of 0
+*/
+/*
+Approach:
+
+  A good way to approach this problem is to first think of a simpler version.
+  How would you check if the entire array sum is zero?
+
+  If the entire array does not sum to zero, then you need to check if there are
+  any smaller subarrays that sum to zero. For this, it can be helpful to keep
+  track of all of the sums from [0, i], where i is every index in the array.
+
+  After recording all sums from [0, i], what would it mean if a sum is repeated?
+*/
+// Time and Space complexity : O(n) time | O(n) space - where n is the length of nums
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
