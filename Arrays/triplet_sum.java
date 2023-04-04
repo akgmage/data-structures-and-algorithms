ab8fccecac331f31407025b626e2773c0950b142
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-04 22:39:20 +0530

    update three number sum for java

--- Arrays/triplet_sum.java
+++ Arrays/triplet_sum.java
@@ -4,47 +4,61 @@ determine if there are any three integers in nums whose sum equals the target.
 Return TRUE if three such integers are found in the array. Otherwise, return FALSE.
 */
 
-import java.util.*;
-class FindTriplet {
-    boolean findTriplet(int arr[], int n, int sum)
-    {
-        int l, r;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-        /* Sort the elements */
-        Arrays.sort(arr);
+public class ThreeNumberSum {
 
-        /* Now fix the first element one by one and find the
-        other two elements */
-        for (int i = 0; i < n - 2; i++) {
-
-            l = i + 1; // index of the first element
-            r = n - 1; // index of the last element
-            while (l < r) {
-                if (arr[i] + arr[l] + arr[r] == sum) {
-                    return true;
+    public static List<List<Integer>> threeNumberSum(int[] nums, int targetSum) {
+        Arrays.sort(nums); // Sort the input array in non-decreasing order
+        List<List<Integer>> triplets = new ArrayList<>();
+        
+        // Loop through the array
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            // While the left index is less than the right index
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+                
+                if (currentSum == targetSum) {
+                    // If the current triplet sums up to the target sum, add it to the result list
+                    triplets.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Move the left and right indices towards the center to find other triplets
+                    left++;
+                    right--;
+                } else if (currentSum < targetSum) {
+                    // If the current triplet sums up to less than the target sum, move the left index towards the center to find larger numbers
+                    left++;
+                } else {
+                    // If the current triplet sums up to more than the target sum, move the right index towards the center to find smaller numbers
+                    right--;
                 }
-                else if (arr[i] + arr[l] + arr[r] < sum)
-                    l++;
-
-                else
-                // arr[i] + arr[l] + arr[r] is greater than sum
-                    r--;
             }
         }
-
-        // If no triplet was found
-        return false;
+        
+        return triplets;
     }
-	
-	public static void main(String[] args)
-	{
-	    
-        FindTriplet triplet = new FindTriplet();
-		int[] arr = { 1, 2, 3, 4, 5, 6 };
-		int sum = 9;
-		int n = arr.length;
 
-		System.out.println(triplet.findTriplet(arr, n, sum));
-	}
+    public static void main(String[] args) {
+        // Example usage
+        int[] nums = { 12, 3, 1, 2, -6, 5, -8, 6 };
+        int targetSum = 0;
+        
+        List<List<Integer>> triplets = threeNumberSum(nums, targetSum);
+        
+        for (List<Integer> triplet : triplets) {
+            System.out.print("[");
+            for (int i = 0; i < triplet.size(); i++) {
+                System.out.print(triplet.get(i));
+                if (i < triplet.size() - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println("]");
+        }
+    }
 }
-
