1573b198059f86361f486a0f92aed438891ad73a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-10 13:08:25 +0530

    add description

--- sorting/quick_sort.js
+++ sorting/quick_sort.js
@@ -1,3 +1,17 @@
+/*
+    The quickSort function implements the quicksort algorithm, which takes an array arr and the indices low and high 
+    of the subarray to be sorted. If low is less than high, the array is partitioned around a pivot element using the 
+    partition function, and the subarrays on either side of the pivot are recursively sorted using the quickSort function.
+
+    The partition function takes an array arr and the indices low and high of the subarray to be partitioned. 
+    It chooses the rightmost element as the pivot, initializes the index of the smaller element to low - 1, and 
+    iterates over the subarray. If an element arr[j] is less than the pivot, it is swapped with the element arr[i+1] 
+    and the index i is incremented. Finally, the pivot element is swapped with the element arr[i+1], 
+    and the index i+1 is returned as the index of the pivot.
+
+    The time complexity of quicksort is O(n log n) on average, and O(n^2) in the worst case. The space complexity 
+    is O(log n) on average, and O(n) in the worst case.
+*/
 /**
  * This function implements the quicksort algorithm to sort an array in place.
  * @param {Array} arr - The array to be sorted.
