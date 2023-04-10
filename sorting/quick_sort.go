e4d69e632cca9f9cef2b4723e5a7392a2fef3148
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-10 13:03:11 +0530

    add description

--- sorting/quick_sort.go
+++ sorting/quick_sort.go
@@ -1,3 +1,22 @@
+// Quicksort
+/*
+	In this implementation, we use the standard algorithm for quicksort:
+
+	1. Choose a pivot element (we choose the middle element in this case)
+	2. Partition the array into two subarrays: elements less than the pivot go to the left subarray,
+	   and elements greater than the pivot go to the right subarray
+	3. Recursively sort the left and right subarrays using the same algorithm
+
+	The quicksort function takes three arguments: the array to be sorted, the left index of the subarray,
+	and the right index of the subarray. We start by choosing the pivot element (in this case, the middle element),
+	initializing two pointers to the left and right ends of the subarray, and then partitioning the array into two
+	subarrays by moving elements less than the pivot to the left subarray and elements greater than the pivot to
+	the right subarray. We repeat this process until the subarrays have length 0 or 1.
+
+	Once the subarrays are partitioned, we recursively sort the left and right subarrays using the quicksort function.
+	We use two conditional statements to check if the left or right subarray has length greater than 1 and call quicksort
+	on that subarray if it does. Finally, the sorted array is returned.
+*/
 package main
 
 import "fmt"
