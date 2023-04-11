7801705e9f29b5714501c2100d92b428cc6f6363
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-11 22:43:44 +0530

    add dfs in go

--- Graphs/graphs_dfs.go
+++ Graphs/graphs_dfs.go
@@ -1,43 +1,46 @@
-/*
-	DFS on a graph
-	Sample Input
-        A
-     /  |  \
-    B   C   D
-   / \     / \
-  E   F   G   H
-     / \   \
-    I   J   K
-	Output: ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"]
-*/
-/*
-	Start at the root Node and try simply calling the depthFirstSearch method on all of its children Nodes.
-	Then, call the depthFirstSearch method on all children Nodes of each child node.
-	Keep applying this logic until the entire graph has been traversed.
-	Don't forget to add the current Node's name to the input array at every call of depthFirstSearch.
-
-	Time and Space complexity:  O(v + e) time | O(v) space - where v is the number of vertices of the input graph and e is the number of edges of the input graph
-*/
-
 package main
 
 import "fmt"
 
+// Node represents a single node in the graph
 type Node struct {
-	Name     string
-	Children []*Node
+    value int
+    visited bool
+    neighbors []*Node
 }
 
-func (n *Node) DepthFirstSearch(array []string) []string {
-    return n.dfsHelper(array)
-}
+// DFS traverses the graph starting from the given node using Depth First Search
+func DFS(node *Node) {
+    // Mark the current node as visited
+    node.visited = true
 
-func (n *Node) dfsHelper(array []string) []string {
-    array = append(array, n.Name)
-    
-    for _, child := range n.Children {
-        fmt.Println(child.Name, " -->")
-        array = child.dfsHelper(array)
+    // Print the value of the current node
+    fmt.Printf("%d ", node.value)
+
+    // Visit each of the neighboring nodes
+    for _, neighbor := range node.neighbors {
+        // If the neighbor has not been visited yet, visit it recursively
+        if !neighbor.visited {
+            DFS(neighbor)
+        }
     }
-    return array
+}
+
+func main() {
+    // Create the nodes of the graph
+    node1 := &Node{value: 1}
+    node2 := &Node{value: 2}
+    node3 := &Node{value: 3}
+    node4 := &Node{value: 4}
+    node5 := &Node{value: 5}
+
+    // Add the neighbors for each node
+    node1.neighbors = []*Node{node2, node3}
+    node2.neighbors = []*Node{node1, node4, node5}
+    node3.neighbors = []*Node{node1, node5}
+    node4.neighbors = []*Node{node2}
+    node5.neighbors = []*Node{node2, node3}
+
+    // Start the DFS traversal from node1
+    DFS(node1)
 }
