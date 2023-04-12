d8ac22f88b333c9ea5f5bbcf9eb56d69254d95a3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-12 22:29:30 +0530

    add description

--- Arrays/coin_change_greedy.go
+++ Arrays/coin_change_greedy.go
@@ -1,3 +1,19 @@
+/*
+	The coin change problem is a classic algorithmic problem, where given a target amount and a set of coin
+	denominations, we need to find the minimum number of coins required to make up that amount.
+
+	In this implementation, we start by sorting the array of coin denominations in descending order.
+	We then iterate through the array from largest to smallest denomination, and for each denomination,
+	we repeatedly subtract the largest possible multiple of that denomination from the target amount
+	until we reach 0.
+
+	The time complexity of this algorithm is O(n log n) due to the sorting step, where n is the number
+	of coin denominations. However, the space complexity is O(1) since we are only using a constant
+	amount of extra space.
+
+	For the sample input coins := []int{1, 5, 10, 25} and target := 36, the output should be Minimum
+	number of coins required to make 36 cents: 3.
+*/
 package main
 
 import "fmt"
