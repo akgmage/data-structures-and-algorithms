0a8dae580dfbe02699c75aa168f9746edc1d3187
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-13 22:53:12 +0530

    add description

--- Dynamic Programming/coin_change.cpp
+++ Dynamic Programming/coin_change.cpp
@@ -1,3 +1,14 @@
+/*
+    The code uses a dynamic programming approach to solve the coin change problem. The dp vector is used to 
+    store the minimum number of coins needed to make each amount from 0 to amount. The minimum number 
+    of coins needed to make an amount of 0 is 0. Then, for each coin, the code iterates through each 
+    amount from the coin value to the target amount. If the current amount minus the coin value is a 
+    valid amount, the code updates the minimum number of coins needed to make that amount by taking 
+    the minimum of the current value and the value of dp[i - coin] + 1. Finally, if the minimum 
+    number of coins needed to make the target amount is still INT_MAX, it is not possible to make 
+    the amount and the code returns -1. Otherwise, the code returns the minimum number of coins 
+    needed to make the target amount.
+*/
 #include <iostream>
 #include <vector>
 #include <climits>
