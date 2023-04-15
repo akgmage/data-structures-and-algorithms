7099fb7c965b7fbbbd669c7120bdd32e793edc6c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-15 19:30:52 +0530

    add description

--- Hash Table/integer_to_roman.py
+++ Hash Table/integer_to_roman.py
@@ -1,3 +1,18 @@
+# Integer to Roman
+'''
+    The function takes an integer num as input and returns the corresponding Roman numeral as a string. 
+    The mapping of values to symbols is defined in a dictionary symbols. The keys in the dictionary are 
+    the values that can be used to represent Roman numerals, and the values are the symbols themselves.
+
+    The keys are sorted in descending order in a list called keys. This is done so that we can iterate 
+    through the keys in descending order and subtract the largest value from num until num is 0.
+
+    We initialize an empty string called roman to store the Roman numeral representation. We then iterate 
+    through the keys in keys and subtract the largest value from num until num is less than the current key. 
+    We append the corresponding symbol to roman for each value subtracted.
+
+    Finally, we return the string roman, which is the Roman numeral representation of the input integer num.
+'''
 def intToRoman(num: int) -> str:
     # define the mapping of values to symbols
     symbols = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L',
