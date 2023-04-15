417638a39f45e4b7f030a8ec2f0ddfb79308712a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-15 19:26:45 +0530

    add description

--- Hash Table/integer_to_roman.cpp
+++ Hash Table/integer_to_roman.cpp
@@ -1,3 +1,14 @@
+// Integer to Roamn
+/*
+    In this implementation, we use an unordered map to store the mapping between integers and Roman numerals. 
+    We then loop through the map from the largest number to the smallest, checking if the current number 
+    can be divided by the key of the map. If it can, we append the corresponding Roman numeral to the result 
+    string and subtract the key from the number. We continue this process until the number is 0. 
+    The final result string is then returned.
+
+    The time complexity of this implementation is O(1) since the size of the map is fixed, and the 
+    space complexity is O(1) since we only use a fixed amount of memory to store the map and the result string.
+*/
 #include <iostream>
 #include <unordered_map>
 using namespace std;
