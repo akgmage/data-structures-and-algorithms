abecf44f3afe47ffe6d9e3a153362aa9de54e9a8
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-15 19:18:24 +0530

    modify queue

--- Queue/queue.go
+++ Queue/queue.go
@@ -1,57 +1,80 @@
-// Implement Queue Data Structure
-// TODO: Make Queue Generic
+// Queue Data Structure
 
 package main
 
 import "fmt"
 
-type Queue []string
-
-// IsEmpty func checks if the queue is empty
-func (q *Queue) IsEmpty() bool{
-	return len(*q) == 0
+// Define a struct for queue that has a slice to store values and pointers to the front and rear of the queue
+type Queue struct {
+    values []int
+    front  *int
+    rear   *int
 }
 
-// EnQueue func enqueues a new value in the Queue
-func (q *Queue) EnQueue(str string) {
-	fmt.Printf("%s entered queue\n", str)
-	*q = append(*q, str)
+// Function to create a new queue and initialize its front and rear pointers
+func NewQueue() *Queue {
+    q := Queue{values: make([]int, 0), front: nil, rear: nil}
+    return &q
 }
-// DeQueue func dequeues "first in" as in FIFO, value in the Queue
-func (q *Queue) DeQueue() (string, bool) {
-	if q.IsEmpty() {
-		return "", false
-	} else {
-		// pick first element from queue
-		element := (*q)[0]
-		// slice off the picked element
-		*q = (*q)[1:]
-		return element, true
-	}
+
+// Function to check if the queue is empty
+func (q *Queue) IsEmpty() bool {
+    return q.front == nil
+}
+
+// Function to add an element to the rear of the queue
+func (q *Queue) Enqueue(val int) {
+    // If the queue is empty, set both front and rear pointers to the new element
+    if q.front == nil {
+        q.front = &val
+        q.rear = &val
+    } else {
+        // Otherwise, add the new element to the rear of the queue and update the rear pointer
+        q.values = append(q.values, val)
+        q.rear = &q.values[len(q.values)-1]
+    }
+}
+
+// Function to remove an element from the front of the queue and return its value
+func (q *Queue) Dequeue() (int, error) {
+    // If the queue is empty, return an error
+    if q.front == nil {
+        return 0, fmt.Errorf("queue is empty")
+    }
+
+    // Get the value of the element at the front of the queue
+    val := *q.front
+
+    // If there is only one element in the queue, set both front and rear pointers to nil
+    if len(q.values) == 1 {
+        q.front = nil
+        q.rear = nil
+    } else {
+        // Otherwise, remove the front element from the slice and update the front pointer
+        q.values = q.values[1:]
+        q.front = &q.values[0]
+    }
+
+    // Return the value of the dequeued element
+    return val, nil
 }
 
 func main() {
-	var queue Queue
+    // Create a new queue
+    q := NewQueue()
 
-	// DeQueue empty queue
-	ele, msg := queue.DeQueue()
-	if msg == true {
-		fmt.Printf("%s", ele)
-	} else {
-		fmt.Printf("Nothing to delete!\n")
-	}
+    // Add some elements to the queue
+    q.Enqueue(10)
+    q.Enqueue(20)
+    q.Enqueue(30)
 
-	// EnQueue 3 values in queue
-	queue.EnQueue("Hello0")
-	queue.EnQueue("Hello1")
-	queue.EnQueue("Hello2")
-
-	// DeQueue all values
-	for len(queue) > 0 {
-		ele, msg := queue.DeQueue()
-		if msg == true {
-			fmt.Printf("%s deleted from queue\n", ele)
-		}
-	}
-
+    // Dequeue elements from the queue and print their values
+    for !q.IsEmpty() {
+        val, err := q.Dequeue()
+        if err != nil {
+            fmt.Println(err)
+        } else {
+            fmt.Println(val)
+        }
+    }
 }
