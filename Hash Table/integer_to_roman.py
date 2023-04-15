49e300432b3b55648ae961873af7abcb9d128742
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-15 19:29:59 +0530

    modify int to roman

--- Hash Table/integer_to_roman.py
+++ Hash Table/integer_to_roman.py
@@ -1,51 +1,19 @@
-'''
-    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-    Symbol       Value
-    I             1
-    V             5
-    X             10
-    L             50
-    C             100
-    D             500
-    M             1000
-    For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-
-    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-    I can be placed before V (5) and X (10) to make 4 and 9. 
-    X can be placed before L (50) and C (100) to make 40 and 90. 
-    C can be placed before D (500) and M (1000) to make 400 and 900.
-
-    Given an integer, convert it to a roman numeral.
+def intToRoman(num: int) -> str:
+    # define the mapping of values to symbols
+    symbols = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L',
+               90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
     
-    Example 1:
-    Input: num = 3
-    Output: "III"
-    Explanation: 3 is represented as 3 ones.
+    # create a list of keys in descending order
+    keys = sorted(symbols.keys(), reverse=True)
     
-    Example 2:
-    Input: num = 58
-    Output: "LVIII"
-    Explanation: L = 50, V = 5, III = 3.
+    # initialize an empty string to store the roman numeral representation
+    roman = ''
     
-    Example 3:
-    Input: num = 1994
-    Output: "MCMXCIV"
-    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+    # iterate through the keys and subtract the largest value from the number
+    # until the number is 0
+    for k in keys:
+        while num >= k:
+            roman += symbols[k]
+            num -= k
     
-    Constraints:
-    1 <= num <= 3999
-'''
-class Solution:
-    def intToRoman(self, num: int) -> str:
-        #Based on given rules, we can directly consider the other possibilities
-        #Then, we can divide it directly as required and take the roman values
-        arr = [["I",1],["IV",4],["V",5],["IX",9],["X",10],["XL",40],["L",50],["XC",90],["C",100],["CD",400],["D",500],["CM",900],["M",1000]]
-
-        final_string = ""
-        for i in arr[::-1]:
-            rev = num//i[1]
-            final_string += i[0]*rev
-            num = num%i[1]
-        return final_string
+    return roman
