b4b24e5a8c84439c3d09b37e277e685c7ba7eaa0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-16 19:59:06 +0530

    modify best time to buy and sell stock in python

--- Dynamic Programming/best_time_to_buy_and_sell_stock.py
+++ Dynamic Programming/best_time_to_buy_and_sell_stock.py
@@ -1,50 +1,30 @@
+# Best Time to buy and sell stock
 '''
-    You are given an array prices where prices[i] is the price of a given stock on the ith day.
-    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
-    Example 1:
-    Input: prices = [7,1,5,3,6,4]
-    Output: 5
-    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
-    
-    Example 2:
-    Input: prices = [7,6,4,3,1]
-    Output: 0
-    Explanation: In this case, no transactions are done and the max profit = 0.
-    
-    Constraints:
-    1 <= prices.length <= 105
-    0 <= prices[i] <= 104
+    The function maxProfit takes a list of integers prices and returns the maximum profit that can be 
+    made from buying and selling the stock represented by prices. The function works by initializing 
+    the minimum price to a very large number (sys.maxsize) and the maximum profit to 0. It then loops 
+    through the prices, updating the minimum price if the current price is less than the current minimum 
+    price, and updating the maximum profit if the difference between the current price and the minimum 
+    price is greater than the current maximum profit. Finally, it returns the maximum profit.
 '''
 
 
-def maxProfit(prices):
-    minElement = 10000
-    maxDiff = 0
-    for i in range(len(prices)):
-        minElement = min(prices[i], minElement)
-        maxDiff = max(prices[i]-minElement, maxDiff)
-        
-    return maxDiff
-
-prices = [7,1,5,3,6,4]
-print(maxProfit(prices))
-
-# Optimize solution
-def max_profit(prices):    
-    min_price = prices[0]
+def maxProfit(prices: List[int]) -> int:
+    # initialize the minimum price as maximum integer value
+    min_price = sys.maxsize
+    
+    # initialize the maximum profit as 0
     max_profit = 0
     
+    # loop through the prices
     for price in prices:
+        # if the current price is less than the minimum price, update the minimum price
         if price < min_price:
             min_price = price
+        # else if the difference between the current price and the minimum price is greater than the maximum profit,
+        # update the maximum profit
         elif price - min_price > max_profit:
             max_profit = price - min_price
-            
+    
+    # return the maximum profit
     return max_profit
-
-
-print(maxProfit([7,1,5,3,6,4])) # 5
-print(maxProfit([7,6,4,3,1]))# 0
