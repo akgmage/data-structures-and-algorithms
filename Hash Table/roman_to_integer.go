fb4f10fc9d8df1b174fb0d8931ec7412b5ad740f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-16 20:20:18 +0530

    add time and space complexity

--- Hash Table/roman_to_integer.go
+++ Hash Table/roman_to_integer.go
@@ -1,3 +1,20 @@
+// Roman To Integer
+/*
+	This implementation converts a given Roman numeral string to its corresponding integer value.
+	The implementation uses a map to store the integer value of each Roman numeral, and then
+	iterates over the input string, checking if the next Roman numeral is greater than the
+	current one to determine whether to add or subtract the current value from the result.
+	The time complexity of this implementation is O(n), where n is the length of the input string,
+	since it iterates over the string only once. The space complexity is O(1), since the
+	implementation only uses a constant amount of extra space to store the map and result variable.
+
+	Time complexity: O(n), where n is the length of the input string. The program iterates through
+	each character in the string once and performs a constant number of operations for each character.
+	Therefore, the time complexity is linear in the length of the input string.
+
+	Space complexity: O(1). The program uses a constant amount of extra space to store the result
+	and the current and previous values. Therefore, the space complexity is constant, or O(1).
+*/
 package main
 
 import (
