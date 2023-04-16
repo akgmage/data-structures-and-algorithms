3204ffba7de5c91a91d9a72f4a6d2997a4e52cd4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-16 20:02:21 +0530

    modify best time to buy and sell stocks in js

--- Dynamic Programming/best_time_to_buy_and_sell_stock.js
+++ Dynamic Programming/best_time_to_buy_and_sell_stock.js
@@ -1,39 +1,32 @@
+// Best Time to buy and sell stock
 /*
-    You are given an array prices where prices[i] is the price of a given stock on the ith day.
-    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
-    Example 1:
-    Input: prices = [7,1,5,3,6,4]
-    Output: 5
-    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
-    
-    Example 2:
-    Input: prices = [7,6,4,3,1]
-    Output: 0
-    Explanation: In this case, no transactions are done and the max profit = 0.
-    
-    Constraints:
-    1 <= prices.length <= 105
-    0 <= prices[i] <= 104
+	Explanation:
+	We start by initializing the minimum price to the maximum integer value and the maximum profit to 0.
+	We loop through the prices array, and for each price:
+	If the price is less than the current minimum price, we update the minimum price.
+	Otherwise, if the difference between the price and the minimum price is greater than the current maximum profit, we update the maximum profit.
+	Finally, we return the maximum profit
+	Time Complexity: O(n), where n is the length of the prices array.
+	Space Complexity: O(1), as we are only using two variables to keep track of the minimum price and maximum profit
 */
-function maxProfit(prices) {
-    
-    let minPrice = prices[0];
-    let maxProfit = 0;
-    
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else if (prices[i] - minPrice > maxProfit) {
-            maxProfit = prices[i] - minPrice;
-        }
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = Infinity; // keep track of minimum price seen so far
+  let maxProfit = 0; // keep track of maximum profit seen so far
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i]; // update minimum price seen so far
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice; // update maximum profit seen so far
     }
-    
-    return maxProfit;
-}
+  }
 
-console.log(maxProfit([7,1,5,3,6,4]))// 5
-console.log(maxProfit([7,6,4,3,1]))// 0
+  return maxProfit;
+};
 
+const prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices)); // Output: 5
