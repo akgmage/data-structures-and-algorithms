63e21010bd9e6cce5e92d6266e137056cbe19416
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-16 19:53:47 +0530

    add description

--- Dynamic Programming/best_time_to_buy_and_sell_stock.go
+++ Dynamic Programming/best_time_to_buy_and_sell_stock.go
@@ -1,3 +1,14 @@
+// Best Time to buy and sell stock
+/*
+	Explanation:
+	We start by initializing the minimum price to the maximum integer value and the maximum profit to 0.
+	We loop through the prices array, and for each price:
+	If the price is less than the current minimum price, we update the minimum price.
+	Otherwise, if the difference between the price and the minimum price is greater than the current maximum profit, we update the maximum profit.
+	Finally, we return the maximum profit
+	Time Complexity: O(n), where n is the length of the prices array.
+	Space Complexity: O(1), as we are only using two variables to keep track of the minimum price and maximum profit
+*/
 package main
 
 import (
