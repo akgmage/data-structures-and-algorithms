5b1573288accace5c66b18b57736bf575fe3dc64
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-17 22:56:01 +0530

    add description

--- Hash Table/group_anagrams.py
+++ Hash Table/group_anagrams.py
@@ -1,3 +1,23 @@
+# Group Anagrams
+'''
+    The function groupAnagrams takes a list of strings as input and returns a list of lists, where each inner list 
+    contains a group of anagrams from the input list.
+
+    The function starts by creating an empty dictionary groups to store the groups of anagrams. The keys of the dictionary 
+    will be the unique signatures of the anagrams, and the values will be lists of the actual anagrams.
+
+    Then, the function iterates through each string s in the input list strs. For each string, it sorts the characters 
+    of the string to get its unique signature sig. If the signature is not already in the dictionary, a new key-value pair 
+    is added to the dictionary with the signature as the key and an empty list as the value. Finally, the current 
+    string s is appended to the list of its corresponding signature in the dictionary.
+
+    Once all the strings have been processed, the function returns the values of the dictionary as a list of lists, 
+    where each inner list contains a group of anagrams from the input list.
+
+    For example, if the input list is ["eat", "tea", "tan", "ate", "nat", "bat"], 
+    the function will return [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']], which 
+    represents the three groups of anagrams: ['eat', 'tea', 'ate'], ['tan', 'nat'], and ['bat'].
+'''
 def groupAnagrams(strs):
     # create a dictionary to store the groups of anagrams
     groups = {}
