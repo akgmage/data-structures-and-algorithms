95e6986a37902b37c7a2d57c9b524d5253361984
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-18 22:17:30 +0530

    add main func

--- Linked List/floyds_cycle_detection.go
+++ Linked List/floyds_cycle_detection.go
@@ -1,5 +1,7 @@
 // Definition for singly-linked list.
 package main
+
+import "fmt"
 type Node struct {
     value int
     next  *Node
@@ -26,3 +28,20 @@ func DetectCycle(head *Node) bool {
     return false
 }
 
+func main() {
+    // Create a linked list with a cycle
+    head := &Node{value: 1}
+    node2 := &Node{value: 2}
+    node3 := &Node{value: 3}
+    node4 := &Node{value: 4}
+    node5 := &Node{value: 5}
+    head.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    node5.next = node2 // Create a cycle
+
+    // Detect cycle in the linked list
+    hasCycle := DetectCycle(head)
+    fmt.Println(hasCycle) // Output: true
+}
