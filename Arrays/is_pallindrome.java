11d712600336441e41975c04077dfc91e078f60b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-23 13:07:27 +0530

    add description

--- Arrays/is_pallindrome.java
+++ Arrays/is_pallindrome.java
@@ -1,3 +1,23 @@
+/*
+    Write a function that takes in a non-empty string and that returns a boolean
+    representing whether the string is a palindrome.
+    Sample Input: abba
+    Output: True
+    Sample Input: aberba
+    Output: False
+    
+    Explanation:
+    This function takes in a non-empty string and returns a boolean representing whether the string 
+    is a palindrome. It first removes all non-alphanumeric characters from the string and converts 
+    it to lowercase using the replaceAll() and toLowerCase() string methods, respectively.
+
+    It then initializes two pointers, one at the beginning and one at the end of the cleaned string. 
+    It iterates through the string from both ends, comparing characters at each step. 
+    If the characters do not match, it returns false.
+
+    If the loop completes without returning false, the string is a palindrome and the function 
+    returns true.
+*/
 /**
  * This function takes in a non-empty string and returns a boolean representing
  * whether the string is a palindrome.
