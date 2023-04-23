bbe3ec7fcc3bf10a0c9c149ad5285429bd51d755
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-23 13:09:39 +0530

    add description

--- Arrays/is_pallindrome.py
+++ Arrays/is_pallindrome.py
@@ -1,3 +1,19 @@
+'''
+    Write a function that takes in a non-empty string and that returns a boolean
+    representing whether the string is a palindrome.
+    Sample Input: abba
+    Output: True
+    Sample Input: aberba
+    Output: False
+
+    Explanation:
+
+    In this implementation, we use Python's filter() function to remove all non-alphanumeric characters 
+    from the string and lower() method to convert the string to lowercase. Then we check if the reversed 
+    string is equal to the original string using the slice notation [::-1]. If the two strings are equal, 
+    we return True, indicating that the input string is a palindrome; otherwise, we return False.
+'''
+
 def is_palindrome(s: str) -> bool:
     """
     This function takes in a non-empty string and returns a boolean indicating
