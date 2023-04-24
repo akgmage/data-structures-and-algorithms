760b4db0c5d0e71b6d756865f2f7463a0b2c6b8a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-24 18:42:27 +0530

    modify solution

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -11,23 +11,22 @@
 // Sample Input : [2, 1, 9, 3, 5, 4, 0]
 // Output : [0 1 2 3 4 5 9]
 
-const inputArr = [4, 5, 67, 56, 3, 35, 45];
-
-const bubbleSort = (arr) => {
-  let len = arr.length;
-
-  //loop to access each array element
-  for (let i = 0; i < len - 1; i++)
-    //loop to compare array elements
-    for (let j = i + 1; j < len; j++)
-      //compare two adjacent elements
-      if (arr[i] > arr[j]) {
-        //swapping elements if elements are not in the intended order
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+function bubbleSort(arr) {
+  var n = arr.length;
+  // Traverse through all array elements
+  for (var i = 0; i < n - 1; i++) {
+    // Last i elements are already sorted
+    for (var j = 0; j < n - i - 1; j++) {
+      // Swap adjacent elements if they are in the wrong order
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
-  return arr;
-};
-bubbleSort(inputArr);
-console.log(inputArr);
+    }
+  }
+}
+
+var arr = [64, 25, 12, 22, 11];
+bubbleSort(arr);
+console.log("Sorted array: " + arr);
