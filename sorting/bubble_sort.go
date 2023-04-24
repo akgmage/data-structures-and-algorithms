e925b7e39cc58df46874939dbb84891bc2b7ff91
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-24 18:42:10 +0530

    modify solution

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -15,22 +15,28 @@ package main
 
 import "fmt"
 
-func main() {
-	arr := [6]int{2, 1, 3, 5, 4, 0}
-	// arr := [6]int{0, 1, 2, 3, 4, 5} uncomment for best case
+func bubbleSort(arr []int) {
+    n := len(arr)
 	flag := true
-	for i := 0; i < 6; i++ {
-		for j := 0; j < 6 - i - 1; j++ {
-			if(arr[j] > arr[j + 1]) {
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-				flag = false // hack if the array is already sorted, no need for redundant passes
-			}
-		}
+    // Traverse through all array elements
+    for i := 0; i < n-1; i++ {
+        // Last i elements are already sorted
+        for j := 0; j < n-i-1; j++ {
+            // Swap adjacent elements if they are in the wrong order
+            if arr[j] > arr[j+1] {
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+				flag = false
+            }
+        }
 		if flag {
 			fmt.Println("Already sorted so no further redundant passes best case O(n)")
 			break
 		}
-	} 
-	fmt.Println(arr)
+    }
+}
 
+func main() {
+    arr := []int{64, 25, 12, 22, 11}
+    bubbleSort(arr)
+    fmt.Println("Sorted array:", arr)
 }
