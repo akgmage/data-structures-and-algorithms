acf501f7f8a1333ea9857679ac14cb15367f823e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-25 23:22:51 +0530

    add description

--- Binary Search/binary_search.cpp
+++ Binary Search/binary_search.cpp
@@ -1,3 +1,16 @@
+/*
+    The binarySearch function takes in a sorted vector of integers arr and a target value target to search for.
+    The function initializes the left and right pointers to the first and last indices of the array respectively.
+    The function uses a while loop to keep searching until the left and right pointers meet.
+    Inside the loop, the function calculates the middle index by adding the left and right indices and dividing by 2.
+    If the middle element is equal to the target, the function returns the middle index.
+    If the target is greater than the middle element, the function discards the left half of the array and moves the left pointer to the middle + 1 index.
+    If the target is smaller than the middle element, the function discards the right half of the array and moves the right pointer to the middle - 1 index.
+    If the target is not found in the array, the function returns -1.
+    The driver code initializes a sorted array and a target value, and calls the binarySearch function to search for the target value.
+    The function returns the index of the target value, or -1 if it is not found.
+    The driver code prints the result of the search to the console.
+*/
 #include <iostream>
 #include <vector>
 
