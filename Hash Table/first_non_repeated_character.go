32f899969e4f09492b3bf8aed08ec68f28a3cbc7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-26 08:27:51 +0530

    add first non repeated character in go

---
+++ Hash Table/first_non_repeated_character.go
@@ -0,0 +1,23 @@
+package main
+
+// This function takes a string as input and returns the index of the first non-repeating character in the string.
+func FirstNonRepeatingCharacter(str string) int {
+	// Create a map to store the frequency of each character in the string.
+	// The key of the map is a rune, which is an integer representation of a Unicode character.
+	// The value of the map is an integer representing the frequency of the corresponding character.
+	charFreq := make(map[rune]int)
+	// Loop through each character in the string and update its frequency in the map.
+	for _, char := range str {
+		charFreq[char] += 1
+	}
+	// Loop through each character in the string again.
+	for idx, char := range str {
+		// If the frequency of the current character is 1, it means it is the first non-repeating character in the string.
+		if charFreq[char] == 1 {
+			// Return the index of the non-repeating character.
+			return idx
+		}
+	}
+	// If no non-repeating character is found in the string, return -1.
+	return -1
+}
