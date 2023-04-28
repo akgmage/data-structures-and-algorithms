9b5ff11d97a77cf8df92eea28ef9a32d2ceecc05
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-28 20:03:16 +0530

    add description

--- Arrays/is_monotonic.cpp
+++ Arrays/is_monotonic.cpp
@@ -1,3 +1,22 @@
+/*
+	An array is said to be monotonic in nature if it is either continuously increasing or continuously decreasing.
+	Mathematically, An array A is continuously increasing if for all i <= j, A[i] <= A[j].
+
+	The IsMonotonic function takes an array of integers and returns a boolean value indicating whether the array
+	is monotonic or not. A monotonic array is one in which the elements are either non-increasing or non-decreasing.
+
+	The function works by initializing two boolean variables, isNonDecreasing and isNonIncreasing, to true.
+	It then iterates over the array from the first element to the second-to-last element, comparing each element to the next one.
+
+	If the current element is less than the next element, it sets isNonDecreasing to false, indicating that the
+	array is not non-decreasing. If the current element is greater than the next element, it sets isNonIncreasing to false,
+	indicating that the array is not non-increasing.
+
+	At the end of the loop, the function returns true if either isNonDecreasing or isNonIncreasing is still true,
+	indicating that the array is monotonic. Otherwise, it returns false.
+
+	O(n) time | O(1) space - where n is the length of the array
+*/
 #include <iostream>
 #include <vector>
 
