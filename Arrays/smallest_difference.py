f6cc5610f5833b8d8612fbf8bf95151a2346dc5c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-29 09:29:49 +0530

    add description

--- Arrays/smallest_difference.py
+++ Arrays/smallest_difference.py
@@ -1,3 +1,25 @@
+'''
+    Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array)
+	whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from
+	the first array in the first position.
+
+	Note that the absolute difference of two integers is the distance between them on the real number line.
+	For example, the absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+
+  	You can assume that there will only be one pair of numbers with the smallest difference.
+
+	Sample Input Array1 = [-1, 5, 10, 20, 28, 3]
+	Sample Input Array2 = [26, 134, 135, 15, 17]
+
+	Sample Output = [28, 26]
+    
+    The smallest_difference function takes two arrays of integers as input and returns a list of two integers, where the first integer 
+    is from the first array and the second is from the second array, and the absolute difference between them is the smallest among 
+    all such pairs. It achieves this by sorting the two arrays and then iterating through them in parallel, keeping track of the 
+    current smallest difference and the pair of integers that produced it. The function returns the pair with the smallest difference.
+
+    O(nlog(n) + mlog(m)) time | O(1) space - where n is the length of the first input array and m is the length of the second input array
+'''
 from typing import List
 import math
 
