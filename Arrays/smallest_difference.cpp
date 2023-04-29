152fb96e396522275f27113b6a12c85e7c3d38e3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-29 09:22:34 +0530

    add description

--- Arrays/smallest_difference.cpp
+++ Arrays/smallest_difference.cpp
@@ -1,3 +1,29 @@
+/*
+	Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array)
+	whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from
+	the first array in the first position.
+
+	Note that the absolute difference of two integers is the distance between them on the real number line.
+	For example, the absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+
+  	You can assume that there will only be one pair of numbers with the smallest difference.
+
+	Sample Input Array1 = [-1, 5, 10, 20, 28, 3]
+	Sample Input Array2 = [26, 134, 135, 15, 17]
+
+	Sample Output = [28, 26]
+
+
+    The code finds the pair of elements from two arrays, array1 and array2, whose absolute difference is the smallest 
+    among all possible pairs. It does this by first sorting both arrays and then using two pointers (idx1 and idx2) 
+    to iterate through the arrays. At each iteration, it compares the current element of array1 and array2, 
+    and updates smallest (the smallest absolute difference so far) and result (the pair of elements that give 
+    the smallest absolute difference) if the absolute difference between the current elements is smaller than 
+    the current smallest. The function returns the result at the end.
+
+    Time complexity: O(nlog(n) + mlog(m)), where n and m are the lengths of array1 and array2, respectively
+    Space complexity: O(1)
+*/
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -5,11 +31,9 @@
 
 using namespace std;
 
-// SmallestDifference takes in two slices of integers and returns a pair of integers, one from each slice, such that their
+// SmallestDifference takes in two Vector of integers and returns a pair of integers, one from each Vector, such that their
 // absolute difference is as close to zero as possible. If multiple pairs have the same absolute difference, SmallestDifference
 // returns the pair whose elements come first in the respective slices.
-// Time complexity: O(nlog(n) + mlog(m)), where n and m are the lengths of array1 and array2, respectively
-// Space complexity: O(1)
 vector<int> SmallestDifference(vector<int> array1, vector<int> array2) {
     // Initialize variables to track the smallest absolute difference seen so far and the current absolute difference
     int current = INT_MAX, smallest = INT_MAX;
