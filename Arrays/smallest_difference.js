7d9a354685a25fee72c8abb668ce8dd6b0edffb0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-29 09:25:31 +0530

    add description

--- Arrays/smallest_difference.js
+++ Arrays/smallest_difference.js
@@ -1,3 +1,27 @@
+/*
+    Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array)
+	whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from
+	the first array in the first position.
+
+	Note that the absolute difference of two integers is the distance between them on the real number line.
+	For example, the absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+
+  	You can assume that there will only be one pair of numbers with the smallest difference.
+
+	Sample Input Array1 = [-1, 5, 10, 20, 28, 3]
+	Sample Input Array2 = [26, 134, 135, 15, 17]
+
+	Sample Output = [28, 26]
+    The smallestDifference function takes in two arrays of integers and finds the pair of values (one from each array) 
+    that have the smallest difference. It does this by sorting the two arrays in ascending order, and then iterating 
+    through both arrays using two index variables. At each step, the function calculates the difference between the 
+    values pointed to by the index variables, updates the current difference, and moves the index of the array with 
+    the smaller value to the right. If the values are equal, the function returns the pair as an array. 
+    The function keeps track of the smallest difference seen so far, and updates the result array accordingly. 
+    Finally, the function returns the result array containing the pair with the smallest difference.
+
+    O(nlog(n) + mlog(m)) time | O(1) space - where n is the length of the first input array and m is the length of the second input array
+*/
 /**
  * Given two arrays of integers, finds a pair of values (one value from each array)
  * that have the smallest difference. Returns the pair as an array.
