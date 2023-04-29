9dba0463ecc904a61abb7ae33b3068848d0a01c9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-29 09:15:27 +0530

    add questioon with sample io

--- Arrays/smallest_difference.go
+++ Arrays/smallest_difference.go
@@ -1,4 +1,18 @@
 /*
+	Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array)
+	whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from
+	the first array in the first position.
+
+	Note that the absolute difference of two integers is the distance between them on the real number line.
+	For example, the absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+
+  	You can assume that there will only be one pair of numbers with the smallest difference.
+
+	Sample Input Array1 = [-1, 5, 10, 20, 28, 3]
+	Sample Input Array2 = [26, 134, 135, 15, 17]
+
+	Sample Output = [28, 26]
+
 
 	This code implements the Smallest Difference problem which takes two arrays of integers as input and returns a pair of integers,
 	one from each array, with the smallest absolute difference between them.
