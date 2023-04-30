47b791ddef9b578c0bc3046d993182996cf9a8e4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-04-30 13:48:52 +0530

    add question

--- 2D Arrays (Matrix)/spiral_traverse.go
+++ 2D Arrays (Matrix)/spiral_traverse.go
@@ -1,3 +1,12 @@
+/*
+	Write a function that takes in an n x m two-dimensional array (that can be
+	square-shaped when n == m) and returns a one-dimensional array of all the
+	array's elements in spiral order.
+
+	Spiral order starts at the top left corner of the two-dimensional array, goes
+	to the right, and proceeds in a spiral pattern all the way until every element
+	has been visited.
+*/
 package main
 
 import "fmt"
