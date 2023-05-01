b639ac4b60eced6bf84044ad3721dbb9cba1552f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-05-01 23:22:48 +0530

    add main func

--- Hash Table/first_duplicate_value.go
+++ Hash Table/first_duplicate_value.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func FirstDuplicateValue(array []int) int {
 	seenSoFar := make(map[int]bool)
 	for _, num := range array {
@@ -9,4 +11,10 @@ func FirstDuplicateValue(array []int) int {
 		seenSoFar[num] = true
 	}
 	return -1
+}
+
+func main() {
+	array := []int{2, 1, 5, 2, 3, 3, 4}
+	firstDuplicate := FirstDuplicateValue(array)
+	fmt.Println(firstDuplicate)
 }
