7d05318fb01aecb5e7f7b359b1d329f96668bbb0
Sagar <sagarwadhwa888@gmail.com>
2023-05-03 23:00:34 +0530

    Added Reverse Linked list code

--- Linked List/addTwoNumbers.cpp
+++ Linked List/addTwoNumbers.cpp
@@ -6,7 +6,7 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ * };       
  */
 class Solution
 {
