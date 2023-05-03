17685bf5afb0cd62f27ffc42a67742a0192b8e66
Harsit-Agarwalla <harsitagarwalla187@gmail.com>
2023-05-03 11:09:35 +0530

    Update Find maximum in Sliding Window solution

--- Sliding Window/find_max.go
+++ Sliding Window/find_max.go
@@ -1,3 +1,23 @@
+// Statement:
+
+//// Given an array and an integer K, find the maximum for each and every contiguous subarray of size K.
+
+// Example:
+
+//// Input: N = 5 arr[] = {5, 4, 1, 7, 3}, K = 2
+//// Output: 5 4 7 7
+//// Explanation: 	Maximum of 5, 4 is 5
+////           	Maximum of 4, 1 is 4
+////           	Maximum of 1, 7 is 7
+////           	Maximum of 7, 3 is 7
+
+// Complexity:
+
+//// Time  Complexity: O(N*K)
+//// Space Complexity: O(1)
+
+// Code: 
+
 package main
 import "fmt"
 
@@ -5,10 +25,11 @@ func printKMax(arr []int, N int, K int) {
     
 	var j, max int
 
+	// Finding the maximum elemnt in each window
 	for i := 0; i <= N-K; i++ {
 		max = arr[i]
 
-		for j = 1; j < K; j++ {
+		for j = 1; j < K; j++ {		
 			if arr[i+j] > max {
 				max = arr[i+j]
 			}
@@ -19,15 +40,15 @@ func printKMax(arr []int, N int, K int) {
 
 func main() {
 
-	var N, K int
-	fmt.Scan(&N)
+	var N, K int  					// Variable declaration 
+	fmt.Scan(&N) 					// Initialise length of array 
 	
-	arr := make([]int, N)
-	for i := 0; i < N; i++ {
+	arr := make([]int, N)			// Array Declaration
+	for i := 0; i < N; i++ {			// Array Initialisation
 	    fmt.Scan(&arr[i])
 	}
 	
-	fmt.Scan(&K)
+	fmt.Scan(&K)					// Window length initialisation
 	
-	printKMax(arr, N, K)
+	printKMax(arr, N, K)			// Function call to print maximum element in the window
 }
