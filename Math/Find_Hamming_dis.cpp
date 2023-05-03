e59113c98126ebec5479de266bcd05bdda799f35
Thriver <97860789+SuyashSingh01@users.noreply.github.com>
2023-05-03 14:17:26 +0530

    Update Find_Hamming_dis.cpp

--- Math/Find_Hamming_dis.cpp
+++ Math/Find_Hamming_dis.cpp
@@ -1,5 +1,26 @@
-#include <vector>
+/*
+Example:
+Input: nums = [4,14,2]
+Output: 6
+
+Explanation: 
+In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+showing the four bits relevant in this case).
+The answer will be:
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+
+Detailed Explanation:
+The totalHammingDistance function takes a reference to a vector of integers nums, and returns the total Hamming distance between all possible pairs of elements in the vector.
+
+In the function, we initialize the answer ans to 0, and the size of the vector n is obtained using the size() method of the vector. We then loop through all 32 bits (since we are dealing with 32-bit integers), and for each bit position i, we count the number of elements in nums with the i-th bit set by looping through all elements and checking if the bit is set using bitwise AND operator &.
+
+The count c is then multiplied by (n-c), which gives the number of pairs with different bits at the i-th position. This count is added to the answer ans. Finally, the function returns the total Hamming distance.
+The main function is just an example usage, where we create a vector nums and call the totalHammingDistance method of a Solution object to get the total Hamming distance between all possible pairs of elements in nums.
+*/
+// code:
+
 #include<iostream>
+#include <vector>
 
 using namespace std;
 
@@ -28,3 +49,4 @@ int main() {
     int result = s.totalHammingDistance(nums);
     return 0;
 }
+
