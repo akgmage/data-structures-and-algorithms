b4a870ebb5c9b3570cfa4d700ef67974890aa858
shivanand-patil <shivanandpatil963@gmail.com>
2023-05-05 15:20:29 +0530

    comments, space and time complexity added

--- Arrays/merge_intervals.cpp
+++ Arrays/merge_intervals.cpp
@@ -12,19 +12,23 @@ Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>v;
-        sort(intervals.begin(), intervals.end());
-        vector<int>v1=intervals[0];
-        for(int i=1; i<intervals.size(); i++){
-            if(v1[1]<intervals[i][0]){
-                v.push_back(v1);
-                v1=intervals[i];
+        vector<vector<int>>v;  // Create an empty vector of vectors to store the merged intervals
+        sort(intervals.begin(), intervals.end());  // Sort the intervals based on their start times
+        vector<int>v1=intervals[0];  // Initialize the first merged interval to be the first interval in the sorted list
+        for(int i=1; i<intervals.size(); i++){  // Iterate through the sorted intervals starting from the second interval
+            if(v1[1]<intervals[i][0]){  
+            // If the end time of the current merged interval is less than the start time of the next interval, then they don't overlap
+                v.push_back(v1);  // Add the current merged interval to the output vector
+                v1=intervals[i];  // Update the current merged interval to be the next interval
             }
-            else{
-                v1[1]=max(v1[1], intervals[i][1]);
+            else{  // If the current merged interval and the next interval overlap
+                v1[1]=max(v1[1], intervals[i][1]);  // Update the end time of the current merged interval to be the maximum of the two end times
             }
         }
-        v.push_back(v1);
-        return v;
+        v.push_back(v1);  // Add the last merged interval to the output vector
+        return v;  // Return the vector of merged intervals
     }
 };
+
+// TIME COMPLEXITY : O(n log n)
+// SPACE COMPLEXITY : O(n)
