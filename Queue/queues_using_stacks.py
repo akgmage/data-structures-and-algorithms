df58ecd6486168af6287359d03f19d52e7e6a208
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-05-05 16:00:31 +0530

    Update queues_using_stacks.py

--- Queue/queues_using_stacks.py
+++ Queue/queues_using_stacks.py
@@ -18,41 +18,54 @@ The program provides the user with a menu to select the operation they want to p
 
 class Queue:
     def __init__(self):
+        # Initialize two empty stacks
         self.stack1 = []
         self.stack2 = []
 
     def enqueue(self, val):
+        # Add an element to the end of the queue by appending it to stack1
         self.stack1.append(val)
 
     def dequeue(self):
+        # If stack2 is empty, reverse the order of the elements by popping from stack1 and pushing onto stack2
         if not self.stack2:
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
+        # If stack2 is still empty, the queue is empty, so return None
         if not self.stack2:
             return None
+        # Otherwise, pop the top element from stack2 and return it
         return self.stack2.pop()
 
+# Create an instance of the Queue class
 queue = Queue()
 
+# Enter into an infinite loop to prompt the user for operations
 while True:
     print("Select operation -\n"
           "1. Enqueue\n"
           "2. Dequeue\n"
           "3. Quit")
 
+    # Prompt the user for their choice of operation
     choice = int(input("Enter choice: "))
 
     if choice == 1:
+        # If the user selects option 1, prompt them for a value to enqueue and enqueue it
         val = int(input("Enter value to enqueue: "))
         queue.enqueue(val)
         print("Enqueued value:", val)
     elif choice == 2:
+        # If the user selects option 2, dequeue a value and print it, or indicate that the queue is empty
         val = queue.dequeue()
         if val:
             print("Dequeued value:", val)
         else:
             print("Queue is empty.")
     elif choice == 3:
+        # If the user selects option 3, quit the loop
         break
     else:
+        # If the user selects an invalid option, prompt them to try again
         print("Invalid choice. Please try again.")
+
