c2073cb85533e83c6356a949edbeeb57ecc7f947
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2023-05-06 11:21:42 +0530

    Increased readibilty of the code

--- Arrays/insert_interval.py
+++ Arrays/insert_interval.py
@@ -1,16 +1,16 @@
-##57. Insert Interval
-#You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
-#Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
-#Return intervals after the insertion.
-#Example 1:
-#Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
-#Output: [[1,5],[6,9]]
-#Example 2:
-#Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
-#Output: [[1,2],[3,10],[12,16]]
-#Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+# 57. Insert Interval
+# You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+# Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+# Return intervals after the insertion.
+# Example 1:
+# Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+# Output: [[1,5],[6,9]]
+# Example 2:
+# Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+# Output: [[1,2],[3,10],[12,16]]
+# Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
 
-##CODE Explaination
+# CODE Explaination
 # The function insert takes two arguments, intervals which is a list of non-overlapping intervals and newInterval which is another interval that needs to be inserted into intervals.
 # The variable n holds newInterval. The variable j holds intervals. The variables m and m1 are initialized with the start and end points of newInterval, respectively. The variable i is used to iterate through the intervals in j.
 # The while loop checks if newInterval overlaps with any interval in intervals. If there is an overlap, we update m and/or m1 to include all overlapping intervals.
@@ -45,35 +45,46 @@
 
 # Finally, we sort j in ascending order based on the start point of each interval. The sorted list is [[1,5], [6,9]].
 # We return j which is the updated list of non-overlapping intervals after inserting newInterval. Therefore, the output of this function for the input intervals = [[1,3], [6,9]], newInterval = [2,5] is [[1,5],[6,9]].
+# Code
 from typing import List
 
+
 def insert(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    # Assign variables for easier readability
     n = newInterval
     j = intervals
     i = 0
-    m = n[0]
-    m1 = n[1]
+    m = n[0]  # Initialize starting point of the interval to be inserted
+    m1 = n[1]  # Initialize ending point of the interval to be inserted
+
+    # Iterate over all existing intervals
     while i < len(j):
-        if n[0] in range(j[i][0],j[i][1]+1) or n[1] in range(j[i][0],j[i][1]+1) or (n[1]>=j[i][1] and n[0]<=j[i][0]):
-            if j[i][0]<m:
-                m=j[i][0]
-            if j[i][1]>m1:
-                m1=j[i][1]
+        # If any part of the new interval overlaps with the current interval
+        if n[0] in range(j[i][0], j[i][1]+1) or n[1] in range(j[i][0], j[i][1]+1) or (n[1] >= j[i][1] and n[0] <= j[i][0]):
+            # Expand the new interval to include the current interval
+            if j[i][0] < m:
+                m = j[i][0]
+            if j[i][1] > m1:
+                m1 = j[i][1]
+            # Remove the current interval from the list and adjust the index accordingly
             j.pop(i)
-            if i<=0:
-                i=0
+            if i <= 0:
+                i = 0
             else:
-                i-=1
+                i -= 1
         else:
-            i+=1
-    s1=[m,m1]
+            i += 1  # Move to the next interval if there is no overlap
+
+    # Create the final interval by combining all intervals that overlap with the new interval
+    s1 = [m, m1]
     j.append(s1)
-    j.sort()
+    j.sort()  # Sort the final list of intervals by their starting points
     return j
 
+
 # Example usage
-intervals = [[1,3], [6,9]]
-newInterval = [2,5]
+intervals = [[1, 3], [6, 9]]
+newInterval = [2, 5]
 result = insert(intervals, newInterval)
 print(result)
 # Output: [[1,5],[6,9]]
