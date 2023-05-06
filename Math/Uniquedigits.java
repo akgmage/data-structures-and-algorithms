f084a8ed0025c21a54819945705e258c38629b68
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-05-06 17:47:08 +0530

    Create Uniquedigits.java

---
+++ Math/Uniquedigits.java
@@ -0,0 +1,37 @@
+/*Name : Abhinav kumar
+Github username : Abhinavcode13
+Repository name : data-structures-and-algorithms
+Problem : Count Numbers with Unique Digits in Java
+Issue Number : #501
+Problem statement : 
+
+Explanation of the below Java code :
+
+We handle the base case of n = 0 by returning 1. We initialize the answer ans to 10 since there are 10 unique digits between 0 and 9. We also initialize the variables unique_digits and available_digits to 9 since we can't use 0 as the first digit.
+
+We then enter a loop that runs n-1 times (since we have already considered the case of i = 1). In each iteration of the loop, we compute unique_digits as the product of the current value of unique_digits and available_digits. We then add unique_digits to the answer ans and decrement available_digits. This is because we can't use the digits that have already been used for the previous digits.
+
+Finally, we return the value of ans.
+
+
+*/
+
+--------------------------------------------------------------------------------------------------------//Java code begins here---------------------------------------------------------------------------------------------------------------------------------
+
+
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int ans = 10;
+        int unique_digits = 9;
+        int available_digits = 9;
+        while (n-- > 1 && available_digits > 0) {
+            unique_digits *= available_digits;
+            ans += unique_digits;
+            available_digits--;
+        }
+        return ans;
+    }
+}
