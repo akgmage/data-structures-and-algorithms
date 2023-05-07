dbbbe7bdd39312360e9026090f01e39729557bad
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-05-07 11:13:44 +0530

    cleanup and add good documentation

--- Arrays/array_of_products.go
+++ Arrays/array_of_products.go
@@ -1,60 +1,61 @@
-/**
-	* Given an array of integers A, find and return the product array of the same size where the ith element of the product array will be equal to the product of all the elements divided by the ith element of the array.
-	*
-	* Note: It is always possible to form the product array with integer (32 bit) values. Solve it without using the division operator.
-	*
-	*
-	* Input Format
-	*
-	* The only argument given is the integer array A.
-	* Output Format
-	*
-	* Return the product array.
-	* Constraints
-	*
-	* 2 <= length of the array <= 1000
-	* 1 <= A[i] <= 10
-	* For Example
-	*
-	* Input 1:
-	*     A = [1, 2, 3, 4, 5]
-	* Output 1:
-	*     [120, 60, 40, 30, 24]
-	*
-	* Input 2:
-	*     A = [5, 1, 10, 1]
-	* Output 2:
-	*     [10, 50, 5, 50]
+/*
+	Given an array of integers A, find and return the product array of the same size where the ith 
+	element of the product array will be equal to the product of all the elements divided by the ith 
+	element of the array.
+
+	Note: It is always possible to form the product array with integer (32 bit) values. 
+	Solve it without using the division operator.
+
+	Input: [1, 2, 3, 4, 5]
+	Output : [120, 60, 40, 30, 24]
+	
+	Explanation:
+	This code implements a function called `ArrayOfProducts`, which takes an input array of integers and returns a 
+	new array of the same size, where each element in the new array is the product of all the elements in the input 
+	array except for the element at that index.
+
+	To accomplish this, the function first creates a new result array with the same length as the input array. 
+	Then, it initializes a `leftRunningProduct` variable to 1, and loops through the input array from left to right. 
+	For each element in the input array, the function sets the corresponding element in the result array to the 
+	current value of `leftRunningProduct`, and then updates `leftRunningProduct` to be the product of `leftRunningProduct` 
+	and the current element in the input array.
+
+	Next, the function initializes a `rightRunningProduct` variable to 1, and loops through the input array from right to left. 
+	For each element in the input array, the function multiplies the corresponding element in the result array by `rightRunningProduct`, 
+	and then updates `rightRunningProduct` to be the product of `rightRunningProduct` and the current element in the input array.
+
+	Finally, the function returns the result array.
+
+	This algorithm has a time complexity of O(n), where n is the length of the input array, since it loops through the input array 
+	three times. It has a space complexity of O(n), since it creates a new result array of size n, and two additional integer variables.
  */
 
 package main
 
-import "fmt"
+func ArrayOfProducts(array []int) []int {
+	// create two arrays to store the products of all elements to the left and right of each element in the input array
+	leftProduct := make([]int, len(array))
+	rightProduct := make([]int, len(array))
 
-
-
-func ArrayOfProducts(Arr[]int) {
-	Result := make([]int, len(Arr))
-	product := 1
-	// fill left prefix product in result array
-	for i := 0; i < len(Arr); i++ {
-		Result[i] = product
-		product = product * Arr[i]
+	// compute the product of all elements to the left of each element in the input array
+	leftRunningProduct := 1
+	for i := 0; i < len(array); i++ {
+		leftProduct[i] = leftRunningProduct
+		leftRunningProduct *= array[i]
 	}
 
-	// fill right prefix product in result array
-	product = 1
-	for i := len(Arr) - 1; i >= 0; i-- {
-		Result[i] = Result[i] * product
-		product = product * Arr[i]
+	// compute the product of all elements to the right of each element in the input array
+	rightRunningProduct := 1
+	for i := len(array) - 1; i >= 0; i-- {
+		rightProduct[i] = rightRunningProduct
+		rightRunningProduct *= array[i]
 	}
 
-	for i := 0; i < len(Arr); i++ {
-		fmt.Print(Result[i]," ")
+	// compute the product of all elements to the left and right of each element in the input array
+	for i := 0; i < len(array); i++ {
+		rightProduct[i] = leftProduct[i] * rightProduct[i]
 	}
+
+	// return the array of products
+	return rightProduct
 }
-
- func main() {
-	Arr := []int{5, 1, 10, 1}
-	ArrayOfProducts(Arr)
- }
