3b6e940572340485435cd3521c053ec470091730
Shivanand Patil <70444072+shivanand-patil@users.noreply.github.com>
2023-05-07 23:48:58 +0530

    Update valid_palindrome.java

--- Arrays/valid_palindrome.java
+++ Arrays/valid_palindrome.java
@@ -3,6 +3,20 @@ Two Pointers: Check whether a given string is a Valid Palindrome in Java
 */
 
 
+/* EXPLAINATION: 
+The function isPalindrome takes a String input and returns a boolean indicating whether the input string is a palindrome or not.
+
+The function initializes two integer variables, startIdx and endIdx, to the first and last indices in the string, respectively. It then enters a loop that continues while startIdx is less than endIdx.
+
+Inside the loop, the function gets the characters at the start and end indices using the charAt method. It then checks if the lowercase versions of these characters match using the toLowerCase method. If they do not match, the function immediately returns false, indicating that the string is not a palindrome.
+
+If the loop completes without returning false, the function returns true, indicating that the input string is a palindrome.
+
+The time complexity of the function is O(n), where n is the length of the input string. This is because the function loops through half the length of the string in the worst case.
+
+The space complexity of the function is O(1), as it uses a constant amount of additional memory to keep track of the start and end indices.
+*/
+
 
 
 public static boolean isPalindrome(String str)
@@ -30,18 +44,3 @@ public static boolean isPalindrome(String str)
     // if the loop completes without returning false, the string is a palindrome so return true
     return true;
 }
-
-
-/* EXPLAINATION: 
-The function isPalindrome takes a String input and returns a boolean indicating whether the input string is a palindrome or not.
-
-The function initializes two integer variables, startIdx and endIdx, to the first and last indices in the string, respectively. It then enters a loop that continues while startIdx is less than endIdx.
-
-Inside the loop, the function gets the characters at the start and end indices using the charAt method. It then checks if the lowercase versions of these characters match using the toLowerCase method. If they do not match, the function immediately returns false, indicating that the string is not a palindrome.
-
-If the loop completes without returning false, the function returns true, indicating that the input string is a palindrome.
-
-The time complexity of the function is O(n), where n is the length of the input string. This is because the function loops through half the length of the string in the worst case.
-
-The space complexity of the function is O(1), as it uses a constant amount of additional memory to keep track of the start and end indices.
-*/
