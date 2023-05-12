a77b2c38187d8380287df948a73db9ebc90aef8d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-05-13 00:11:46 +0530

    add approach 2 using reverse inorder traversal

--- Trees/Binary Search Trees/kth_largest.go
+++ Trees/Binary Search Trees/kth_largest.go
@@ -25,3 +25,40 @@ func inOrderTraverse(tree *BST, sortedValues *[]int) {
         inOrderTraverse(tree.Right, sortedValues)
     }
 }    
+
+
+// Approach 2: Using Reverse InOrder Traversal               
+// Helper struct to store traversal information
+type treeInfo struct {
+	numberOfNodesVisited   int
+	latestVisitedNodeValue int
+}
+
+// FindKthLargestValueInBst finds the Kth largest value in the BST
+func FindKthLargestValueInBst2(tree *BST, k int) int {
+	// Create treeInfo to track traversal progress
+	treeInfo := treeInfo{0, -1}
+	reverseInOrderTraverse(tree, k, &treeInfo)
+	return treeInfo.latestVisitedNodeValue
+}
+
+// reverseInOrderTraverse performs reverse in-order traversal of the BST
+func reverseInOrderTraverse(tree *BST, k int, treeInfo *treeInfo) {
+	// Base case: if current node is nil or Kth largest value found, return
+	if tree == nil || treeInfo.numberOfNodesVisited == k {
+		return
+	}
+
+	// Traverse the right subtree
+	reverseInOrderTraverse(tree.Right, k, treeInfo)
+
+	// Check if Kth largest value has been found
+	if treeInfo.numberOfNodesVisited < k {
+		// Increment the count of visited nodes and update the latest visited node value
+		treeInfo.numberOfNodesVisited++
+		treeInfo.latestVisitedNodeValue = tree.Value
+
+		// Traverse the left subtree
+		reverseInOrderTraverse(tree.Left, k, treeInfo)
+	}
+}
