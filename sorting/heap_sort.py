d19c49813138efdfed2a2c952bfe32709089d113
Niraj <modiniraj1034@gmail.com>
2023-05-12 07:54:59 +0530

    #516 Number of Substrings with only 1's

--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -1,50 +1,50 @@
-"""
-Sorts a list by converting the list to a max heap which by definition contains the
-largest element at the root node which is the first element of the list. Then the 
-function exchanges the first element with the last element of the list and heapifies
-the root node to bring the largest element of the remaining list back into the first
-position. The function continues this operation n-1 (n = length of the list) times
-at which point the list is sorted.
+  
+Heap sort is a sorting algorithm that works by transforming an unsorted list into a heap data structure, 
+which is a binary tree where each parent node is greater than or equal to its children if any.
+then it repeatedly extracts the maximum element from the heap and puts it into its correct sorted position until the whole list is sorted.
 
-Time complexity: O(n*log(n))
-"""
+Sample_input : [5,16,8,14,20,1,26]
+Sample_output : [1,5,8,14,16,20,26]
+  
+def heap_sort(arr):
+    # Build the initial heap
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    
+    # Extract the maximum element repeatedly
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0] # Swap
+        heapify(arr, i, 0)
+    
+    return arr
 
-def main():
-    an_array = [-5, 4, 0, -1, 2, 4, 6, 1, 3, -10, 1, 2]
-    heap_sort(an_array)
-    print(an_array)
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    
+    # Check if left child is larger than root
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    
+    # Check if right child is larger than root
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+    
+    # Swap if necessary and heapify the affected subtree
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+ere's how the algorithm works:
 
-def heap_sort(arr: list[int]) -> None:
-    heap_size = len(arr)
-    build_max_heap(arr, heap_size)
-    source_idx = 0
-    for dest_idx in range(len(arr) - 1, 0, -1):
-        arr[source_idx], arr[dest_idx] = arr[dest_idx], arr[source_idx]
-        heap_size -= 1
-        max_heapify(arr, 0, heap_size)
+The heap_sort function takes an unsorted list arr as input.
+It starts by building the initial heap by calling the heapify function on each parent node in the tree. 
+It does this by iterating over the parent nodes in reverse order starting from the last parent node,
+and calling heapify on each of them. This builds a heap where each parent node is greater than or equal to its children.
+It then repeatedly extracts the maximum element from the heap and puts it into its correct sorted position. 
+It does this by swapping the maximum element which is always at the root of the heap with the last element in the heap, 
+and then calling heapify on the root node to restore the heap property.
+Finally, it returns the sorted list.
 
-def build_max_heap(arr: list[int], heap_size: int) -> None:
-    """Modifies the input array into a max heap. A tree binary tree structure in every node 
-    satisfies the property: parent node > left node and parent node > right node"""
-    for idx in range((heap_size // 2) - 1, -1, -1):
-        max_heapify(arr, idx, heap_size)
 
-def max_heapify(arr: list[int], idx: int, heap_size: int) -> None:
-    """
-    A recursive function that recursively modifies sub roots
-    of the tree indicated by the idx into a max heap
-    """
-    left_idx = idx + 1
-    right_idx = idx + 2
-    if left_idx < heap_size and arr[idx] < arr[left_idx]:
-        largest = left_idx
-    else:
-        largest = idx
-    if right_idx < heap_size and arr[largest] < arr[right_idx]:
-        largest = right_idx
-    if largest != idx:
-        arr[idx], arr[largest] = arr[largest], arr[idx]
-        return max_heapify(arr, largest, heap_size)
-
-if __name__ == "__main__":
-    main()
