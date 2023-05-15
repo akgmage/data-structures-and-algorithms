3dff12a56736009e74a5aadb5d5178e9264d22cb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-05-15 23:24:30 +0530

    add explanation

--- Trees/Binary Search Trees/reconstruct_bst.go
+++ Trees/Binary Search Trees/reconstruct_bst.go
@@ -1,3 +1,59 @@
+/*
+	Explanation:
+
+	Approach 1:
+
+	The ReconstructBst function takes a slice preOrderTraversalValues which represents the pre-order traversal of a binary search tree.
+	It reconstructs the BST using a recursive approach. Here's how the algorithm works:
+
+	The base case is defined when the preOrderTraversalValues slice is empty, in which case it returns nil indicating an empty tree.
+
+	The first element in the preOrderTraversalValues slice represents the current node value of the BST.
+
+	The algorithm finds the index (rightSubTreeRootIdx) where the right subtree starts by iterating over the remaining elements in
+	the preOrderTraversalValues slice and finding the first value greater than or equal to the current value.
+
+	It recursively calls ReconstructBst on the sub-array representing the left subtree (preOrderTraversalValues[1:rightSubTreeRootIdx])
+	to reconstruct the left subtree.
+
+	It recursively calls ReconstructBst on the sub-array representing the right subtree (preOrderTraversalValues[rightSubTreeRootIdx:])
+	to reconstruct the right subtree.
+
+	Finally, it creates a new BST node with the current value, the reconstructed left subtree, and the reconstructed right subtree,
+	and returns the node.
+
+	The algorithm builds the BST in a top-down manner by dividing the pre-order traversal values into left and right subtrees.
+	It constructs the left subtree first and then the right subtree.
+
+	The time complexity of the algorithm is O(n^2) in the worst case, where n is the number of nodes in the BST.
+
+
+	******************************************************************************************
+
+	Approach 2:
+
+	The ReconstructBst function takes a slice preOrderTraversalValues which represents the pre-order traversal of a binary search tree.
+	It reconstructs the BST using a range-based approach. Here's how the algorithm works:
+
+	The ReconstructBst function initializes a treeInfo struct to keep track of the current root index.
+
+	The ReconstructBst function calls the reconstructBSTFromRange helper function, passing the minimum and maximum integer values
+	as the initial range, the pre-order traversal values, and the treeInfo struct.
+
+	The reconstructBSTFromRange function first checks if the current root index has reached the end of the pre-order traversal values.
+	If so, it returns nil indicating an empty subtree.
+
+	It retrieves the value of the current root from the pre-order traversal values.
+
+	It checks if the root value is outside the valid range defined by the lower and upper bounds. If so, it returns
+
+	The time complexity of the ReconstructBst function is O(n), where n is the number of nodes in the reconstructed BST.
+	This is because the function processes each node exactly once.
+
+	The space complexity of the ReconstructBst function is O(n), where n is the number of nodes in the reconstructed BST.
+	This is because the function creates BST nodes and recursively calls itself to construct the left and right subtrees.
+	The space complexity is determined by the height of the BST, which can be at most n in the worst case for a skewed BST.
+*/
 package main
 
 import "math"
