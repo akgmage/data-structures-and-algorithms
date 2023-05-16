93d51fc75a3a14ae303f8ef1f88cfebad9793b48
mohitsingh1011 <mohitssu001@gmail.com>
2023-05-16 14:38:58 +0530

    Update bucket_sort.java

--- sorting/bucket_sort.java
+++ sorting/bucket_sort.java
@@ -1,5 +1,31 @@
-package util;
+/*
 
+Here is a step by step process :
+Set up an array of initially empty "buckets".
+Scatter: Go over the original array, putting each object in its bucket.
+Sort each non-empty bucket.
+Gather: Visit the buckets in order and put all elements back into the original array.
+
+Bucket Sort time complexity
+Best Case Time Complexity: O(n+k)
+Average Case Time Complexity: O(n)
+Worst Case Time Complexity: O(n^2^)
+Best Case Time Complexity:
+If the array elements are uniformly distributed, bucket size will almost be the same for all the buckets. Hence, this will be the best case which will take up the least amount of time.
+Sorting time complexity will reduce even further if all the elements inside each bucket are already sorted.
+To create n buckets and scatter each element from the array, time complexity = O(n). If we use Insertion sort to sort each bucket, time complexity = O(k). Hence, best case time complexity for bucket sort = O(n+k), where n = number of elements, and k = number of buckets
+Worst Case Time Complexity
+If the array elements are not uniformly distributed, i.e., elements are concentrated within specific ranges.
+This will result in one or more buckets having more elements than other buckets, making bucket sort like any other sorting technique, where every element is compared to the other. Time complexity increases even further if the elements in the array are present in the reverse order. If insertion sort is used, the worst-case time complexity can go up to O(n^2^).
+
+
+Bucket Sort Space Complexity
+Space Complexity : O(n+k)
+Space Complexity for bucket sort is O(n+k), where n = number of elements in the array, and k = number of buckets formed Space taken by each bucket is O(k), and inside each bucket, we have n elements scattered. Hence, the space complexity becomes O(n+k).
+
+*/
+
+package util;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
