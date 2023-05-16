b28d604d0d606ab5f9cfd24af4871ea4958928d4
mohitsingh1011 <132889381+mohitsingh1011@users.noreply.github.com>
2023-05-16 22:09:27 +0530

    Revert "Update bucket_sort.java"

--- sorting/bucket_sort.java
+++ sorting/bucket_sort.java
@@ -1,93 +1,42 @@
-/*
-
-Here is a step by step process :
-Set up an array of initially empty "buckets".
-Scatter: Go over the original array, putting each object in its bucket.
-Sort each non-empty bucket.
-Gather: Visit the buckets in order and put all elements back into the original array.
-
-Bucket Sort time complexity
-Best Case Time Complexity: O(n+k)
-Average Case Time Complexity: O(n)
-Worst Case Time Complexity: O(n^2^)
-Best Case Time Complexity:
-If the array elements are uniformly distributed, bucket size will almost be the same for all the buckets. Hence, this will be the best case which will take up the least amount of time.
-Sorting time complexity will reduce even further if all the elements inside each bucket are already sorted.
-To create n buckets and scatter each element from the array, time complexity = O(n). If we use Insertion sort to sort each bucket, time complexity = O(k). Hence, best case time complexity for bucket sort = O(n+k), where n = number of elements, and k = number of buckets
-Worst Case Time Complexity
-If the array elements are not uniformly distributed, i.e., elements are concentrated within specific ranges.
-This will result in one or more buckets having more elements than other buckets, making bucket sort like any other sorting technique, where every element is compared to the other. Time complexity increases even further if the elements in the array are present in the reverse order. If insertion sort is used, the worst-case time complexity can go up to O(n^2^).
-
-
-Bucket Sort Space Complexity
-Space Complexity : O(n+k)
-Space Complexity for bucket sort is O(n+k), where n = number of elements in the array, and k = number of buckets formed Space taken by each bucket is O(k), and inside each bucket, we have n elements scattered. Hence, the space complexity becomes O(n+k).
-
-*/
-
-package util;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 
-public class BucketSort {
+public class Bucket_Sort {
+    public static void bucketSort(double[] arr) {
+        int n = arr.length;
+        ArrayList<Double>[] buckets = new ArrayList[n];
 
-static void bucketSort(int[] arr, int noOfBuckets) {
+        // Create empty buckets
+        for (int i = 0; i < n; i++) {
+            buckets[i] = new ArrayList<>();
+        }
 
-boolean isNegativePresent = false;
-int offset = Integer.MAX_VALUE;
-for (int i : arr) {
-  if (i < offset) offset = i;
-  if (i < 0) isNegativePresent = true;
-}
+        // Add elements to corresponding buckets
+        for (int i = 0; i < n; i++) {
+            int bucketIndex = (int) (n * arr[i]);
+            buckets[bucketIndex].add(arr[i]);
+        }
 
-int globalMax = Integer.MIN_VALUE;
-int globalMin = Integer.MAX_VALUE;
-for (int i = 0; i < arr.length; i++) {
-  arr[i] -= offset;
-  globalMin = Math.min(arr[i], globalMin);
-  globalMax = Math.max(arr[i], globalMax);
-}
+        // Sort individual buckets
+        for (int i = 0; i < n; i++) {
+            Collections.sort(buckets[i]);
+        }
 
-int range = globalMax - globalMin;
-int bucketRange = (int) Math.ceil((double) range / noOfBuckets);
+        // Concatenate all buckets
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < buckets[i].size(); j++) {
+                arr[index++] = buckets[i].get(j);
+            }
+        }
+    }
 
-// Create bucket array
-List<Integer>[] buckets = new List[noOfBuckets];
-
-// Associate a list with each index in the bucket array
-for (int i = 0; i < noOfBuckets; i++) {
-  buckets[i] = new LinkedList<>();
-}
-
-// Assign numbers from array to the proper bucket
-// by using hashing function
-for (int num : arr) {
-  buckets[hash(num, bucketRange, noOfBuckets)].add(num);
-}
-
-// sort buckets
-for (List<Integer> bucket : buckets) Collections.sort(bucket);
-
-int idx = 0;
-// Merge buckets to get sorted array
-for (List<Integer> bucket : buckets) {
-  for (int num : bucket) {
-    arr[idx++] = num;
-  }
-}
-
-if (isNegativePresent) {
-  for (int i = 0; i < arr.length; i++) {
-    arr[i] += offset;
-  }
-}
-}
-
-private static int hash(int num, int hashValue, int numberOfBuckets) {
-int bucketNumber = num / hashValue;
-if (bucketNumber == numberOfBuckets)
-bucketNumber--;
-return bucketNumber;
-}
+    public static void main(String[] args) {
+        double[] arr = {0.8, 0.3, 0.2, 0.9, 0.1, 0.6, 0.4, 0.7, 0.5, 0.0};
+        bucketSort(arr);
+        System.out.println("Sorted array:");
+        for (double d : arr) {
+            System.out.print(d + " ");
+        }
+    }
 }
