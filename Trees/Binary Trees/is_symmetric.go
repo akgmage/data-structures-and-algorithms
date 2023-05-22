e35574ae6076b3e9bc5ceebb411f0b109d3eb9c2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-05-22 22:30:50 +0530

    add iterative approach using stack

--- Trees/Binary Trees/is_symmetric.go
+++ Trees/Binary Trees/is_symmetric.go
@@ -8,8 +8,9 @@ type BinaryTree struct {
 	Right *BinaryTree
 }
 
+// Approach 1: Recursive Approach
 // SymmetricalTree checks if a binary tree is symmetrical.
-func SymmetricalTree(tree *BinaryTree) bool {
+func SymmetricalTreerecursive(tree *BinaryTree) bool {
 	// Call the helper function to check if the left and right subtrees are mirrored.
 	return treesAreMirrored(tree.Left, tree.Right)
 }
@@ -25,4 +26,31 @@ func treesAreMirrored(left, right *BinaryTree) bool {
 	// If either left or right tree is nil or their values are not equal, they are not mirrored.
 	// Also, if both left and right trees are nil, they are considered mirrored.
 	return left == right
+}
+
+// Approach 2: Iterative Approach using Stack
+func SymmetricalTreeIterative(tree *BinaryTree) bool {
+	stackLeft := []*BinaryTree{tree.Left}   // Initialize stackLeft with the left child of the root node
+	stackRight := []*BinaryTree{tree.Right} // Initialize stackRight with the right child of the root node
+
+	// Perform mirror traversal of the left and right subtrees
+	for len(stackLeft) > 0 {
+		var left, right *BinaryTree
+		left, stackLeft = stackLeft[len(stackLeft)-1], stackLeft[:len(stackLeft)-1]       // Pop the top node from stackLeft
+		right, stackRight = stackRight[len(stackRight)-1], stackRight[:len(stackRight)-1] // Pop the top node from stackRight
+
+		if left == nil && right == nil {
+			continue // Both left and right subtrees are symmetric, continue to the next iteration
+		}
+
+		if left == nil || right == nil || left.Value != right.Value {
+			return false // Asymmetry detected, tree is not symmetric
+		}
+
+		// Push the children of left and right onto the respective stacks in reverse order
+		stackLeft = append(stackLeft, left.Left, left.Right)
+		stackRight = append(stackRight, right.Right, right.Left)
+	}
+
+	return true // Tree is symmetric
 }
