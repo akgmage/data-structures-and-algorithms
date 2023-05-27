7b711ff07203bc0ea2eeb8aec3770193a3c8cc89
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-05-27 23:43:37 +0530

    add time and space complexity

--- Trees/Binary Trees/height_balanced_binary_tree.go
+++ Trees/Binary Trees/height_balanced_binary_tree.go
@@ -6,38 +6,50 @@
 
 	The provided code is for checking whether a binary tree is height-balanced or not. Here's how it works:
 
-	- The code defines a `BinaryTree` struct representing a node in a binary tree. Each node has a value and 
+	- The code defines a `BinaryTree` struct representing a node in a binary tree. Each node has a value and
 	pointers to its left and right child nodes.
 
-	- The code also defines a `TreeeInfo` struct to store information about a binary tree. It includes a boolean 
-	field `isBalanced` indicating whether the tree is balanced or not, and an integer field `height` representing 
+	- The code also defines a `TreeeInfo` struct to store information about a binary tree. It includes a boolean
+	field `isBalanced` indicating whether the tree is balanced or not, and an integer field `height` representing
 	the height of the tree.
 
-	- The `HeightBalancedBinaryTree` function is the main function that checks if a binary tree is height-balanced. 
+	- The `HeightBalancedBinaryTree` function is the main function that checks if a binary tree is height-balanced.
 	It takes the root of the tree as input and returns a boolean value indicating the balance status.
 
-	- The `getTreeInfo` function is a helper function that recursively calculates the information of a binary tree. 
-	It takes a binary tree node as input and returns the `TreeeInfo` struct containing the balance status and height 
+	- The `getTreeInfo` function is a helper function that recursively calculates the information of a binary tree.
+	It takes a binary tree node as input and returns the `TreeeInfo` struct containing the balance status and height
 	of the tree.
 
 	- In the `getTreeInfo` function, there are two base cases:
 	- If the current tree node is `nil`, it is considered balanced with height -1.
-	- If the current tree node is not `nil`, the function recursively calculates the tree information of its 
+	- If the current tree node is not `nil`, the function recursively calculates the tree information of its
 	left and right subtrees.
 
-	- After getting the information of the left and right subtrees, the code checks if both subtrees are balanced 
-	(`isBalanced` field is `true`) and their height difference is at most 1. If so, the current tree is considered 
+	- After getting the information of the left and right subtrees, the code checks if both subtrees are balanced
+	(`isBalanced` field is `true`) and their height difference is at most 1. If so, the current tree is considered
 	balanced.
 
-	- The height of the current tree is calculated by taking the maximum height of the left and right subtrees and 
+	- The height of the current tree is calculated by taking the maximum height of the left and right subtrees and
 	adding 1.
 
-	- Finally, the `max` function is used to get the maximum of two integers, and the `abs` function is used to get 
+	- Finally, the `max` function is used to get the maximum of two integers, and the `abs` function is used to get
 	the absolute value of an integer.
 
-	To determine whether a binary tree is height-balanced, you can call the `HeightBalancedBinaryTree` function with 
+	To determine whether a binary tree is height-balanced, you can call the `HeightBalancedBinaryTree` function with
 	the root of the tree. It will return `true` if the tree is balanced and `false` otherwise.
-  
+
+	The time complexity of the `HeightBalancedBinaryTree` function is O(N), where N is the number of nodes in the
+	binary tree. This is because the function needs to traverse each node of the tree once to calculate the tree
+	information.
+
+	The space complexity of the `HeightBalancedBinaryTree` function is O(H), where H is the height of the binary tree.
+	This is because the recursive calls to the `getTreeInfo` function will utilize the call stack, and the maximum
+	depth of the recursive calls is equal to the height of the tree. Additionally, the space complexity of the
+	`getTreeInfo` function itself is O(1) as it uses a constant amount of space for the `TreeeInfo` struct.
+
+	Overall, the space complexity is determined by the height of the binary tree, and the time complexity is
+	determined by the number of nodes in the binary tree.
+
 */
 package main
 
