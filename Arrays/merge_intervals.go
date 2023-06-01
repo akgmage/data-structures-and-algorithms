ecba8fde0d5246d2c86cb0ed1b29209951bf93b1
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-01 22:56:59 +0530

    add question

--- Arrays/merge_intervals.go
+++ Arrays/merge_intervals.go
@@ -1,4 +1,7 @@
 /*
+   Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+   and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
    Explanation:
 
    The function takes an array of intervals as input.
