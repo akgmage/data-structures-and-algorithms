c610ac84e7fe56f2285de4d61686d8127aeb8345
Naman <namanpokhriyal03@gmail.com>
2023-06-02 14:13:21 +0530

    Added comments

--- Two Pointer/Three_number_sum.java
+++ Two Pointer/Three_number_sum.java
@@ -2,41 +2,58 @@ import java.util.*;
 
 class Three_number_sum {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> triplets = new HashSet<>();
-        List<List<Integer>> pairs = new ArrayList<>();
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int num1 = nums[i], num2 = nums[j];
-                int index = binarySearch(j + 1, nums.length-1, -(num1 + num2), nums);
-                if(index >= 0 && index < nums.length && (nums[index] + num1 + num2 == 0)) {
-                    List<Integer> triplet = List.of(num1, num2, nums[index]);
-                    if(!triplets.contains(triplet)) {
-                        pairs.add(triplet);
-                        triplets.add(triplet);
+        Set<List<Integer>> triplets = new HashSet<>();  // Set to store unique triplets
+        List<List<Integer>> pairs = new ArrayList<>();  // List to store the resulting triplets
+        Arrays.sort(nums);  // Sort the input array in ascending order
+        
+        for (int i = 0; i < nums.length; i++) {  // Iterate through the array
+            for (int j = i + 1; j < nums.length; j++) {  // Iterate through the remaining elements after i
+                int num1 = nums[i], num2 = nums[j];  // Current pair of numbers
+                
+                // Perform binary search to find the third number that complements the sum to 0
+                int index = binarySearch(j + 1, nums.length - 1, -(num1 + num2), nums);
+                
+                // Check if the third number exists and forms a triplet with num1 and num2 that sums up to 0
+                if (index >= 0 && index < nums.length && (nums[index] + num1 + num2 == 0)) {
+                    List<Integer> triplet = List.of(num1, num2, nums[index]);  // Create the triplet
+                    if (!triplets.contains(triplet)) {  // Check if the triplet is unique
+                        pairs.add(triplet);  // Add the triplet to the resulting list
+                        triplets.add(triplet);  // Add the triplet to the set to avoid duplicates
                     }
                 }
             }
         }
-        return pairs;
+        
+        return pairs;  // Return the resulting list of triplets
     }
 
     private int binarySearch(int start, int end, int target, int[] arr) {
-        if(start > end) return start;
-        int index =  (end - start) / 2 + start;
-        if(arr[index] == target) return index;
-        else if (arr[index] < target) return binarySearch(index + 1, end, target, arr);
-        else return binarySearch(start, index - 1, target, arr);
+        if (start > end) return start;  // Base case: target not found, return the insertion position
+        int index = (end - start) / 2 + start;  // Calculate the middle index
+        if (arr[index] == target) return index;  // Base case: target found, return the index
+        else if (arr[index] < target) return binarySearch(index + 1, end, target, arr);  // Search the right half
+        else return binarySearch(start, index - 1, target, arr);  // Search the left half
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 3, -3, 5, 2, 6};
-        int target = 6;
 
-        List<List<Integer>> ans = threeSum(nums, target);
+        Three_number_sum solution = new Three_number_sum();  // Create an instance of the class
+        List<List<Integer>> ans = solution.threeSum(nums);  // Call the threeSum method
 
-        System.out.println(ans);
+        System.out.println(ans);  // Print the resulting triplets
     }
 }
 
-// This program is exclusively for sum target of 0
+// Explanation
+// The given code finds unique triplets in an array that sum up to zero using sorting and binary search. Here's a shorter explanation:
+
+// 1. Sort the input array in ascending order.
+// 2. Iterate through pairs of numbers in the array.
+// 3. Perform binary search to find the third number that complements the sum to zero.
+// 4. Add unique triplets to the result list.
+// 5. Return the resulting list of triplets.
+
+// Time Complexity: O(n^2 * log(n))
+
+// Space Complexity: O(n)
--- Two Pointer/two_sum.java
+++ Two Pointer/two_sum.java
@@ -2,34 +2,35 @@ import java.util.*;
 
 class two_sum {
     public int[] twoSum(int[] nums, int target) {
-        int i=0, j=1, gap = 1;
-        while(i<nums.length){
-            if(nums[i]+nums[j]== target){
-                return new int[]{i,j};
+        int i=0, j=1, gap = 1;  // Initialize variables i, j, and gap
+        
+        while(i<nums.length){  // Start a loop that continues until i reaches the end of the array
+            if(nums[i]+nums[j]== target){  // Check if the sum of nums[i] and nums[j] is equal to the target
+                return new int[]{i,j};  // If the sum is equal to the target, return the indices [i, j] as the result
             }
-            if(j>=nums.length-1){
-                gap++;
-                j = gap;
-                i=0;
+            if(j>=nums.length-1){  // Check if j has reached the end of the array
+                gap++;  // Increment the gap variable
+                j = gap;  // Reset j to the value of gap
+                i=0;  // Reset i to 0 to start checking the next set of pairs
             }
             else{
-                i++;
-                j++;
+                i++;  // Move i one position up
+                j++;  // Move j one position up
             }
         }
-        return new int[]{-1,-1};
+        return new int[]{-1,-1};  // If no pair is found, return [-1, -1] to indicate that a solution doesn't exist
     }
-}
 
     public static void main(String[] args) {
         int[] nums = {1, 3, -3, 5, 4, 6};
         int target = 6;
 
-        int[] pair = twoSum(nums, target);
+        int[] pair = twoSum(nums, target);  // Call the twoSum method to find the pair of numbers
 
-        System.out.println("The Two Numbers positions are " + pair[0] + " " + pair[1] + " That sum up to "+ target);
+        System.out.println("The Two Numbers positions are " + pair[0] + " " + pair[1] + " That sum up to "+ target);  // Print the result
     }
 }
+
 
 //---------------Explanation---------------------
 
@@ -40,3 +41,6 @@ class two_sum {
 //      3. Increase a variable `gap` for each time j reaches the end. We also move i one position up each time j reaches the end.
 //      4. If we find a pair, We return it.
 //      5. If `i` reaches the end, that means a solution doesnt exist. So we return [-1,-1].
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
