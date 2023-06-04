d7812345544652d7040cd4f457eedf3bf633d400
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-04 22:01:41 +0530

    add question and sample io

--- Dynamic Programming/num_ways_to_make_change.go
+++ Dynamic Programming/num_ways_to_make_change.go
@@ -1,3 +1,11 @@
+/*
+	Given an array of distinct positive integers representing coin denominations and a single non-negative integer n
+	representing a target amount of money, write a function that returns the number of ways to make change for
+	that target amount using the given coin denominations.
+
+	Sample Input: n = 6 denominations : [1, 5]
+	Output: 2 (1 * 1 + 1 * 5 and  6 * 1)
+*/
 package main
 
 func NumberOfWaysToMakeChange(n int, denoms []int) int {
