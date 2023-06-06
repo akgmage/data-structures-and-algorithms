6d22bdcc9fbff31a5d3c41c0650be5f5d7cfb3ef
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-06 22:25:51 +0530

    add time and space complexity

--- Arrays/ceaser_cipher.go
+++ Arrays/ceaser_cipher.go
@@ -11,28 +11,36 @@
   Output: dec
 
   Explanation:
-  
-	The CaesarCipherEncryptor function takes a string and a key (integer) as input and returns a new string 
-	obtained by shifting each character of the input string by the key number of positions to the right in 
+
+	The CaesarCipherEncryptor function takes a string and a key (integer) as input and returns a new string
+	obtained by shifting each character of the input string by the key number of positions to the right in
 	the alphabet, wrapping around if necessary.
 
-	The function first calculates the shift amount and offset value. The shift amount is calculated by taking 
-	the key modulo 26, which ensures that the shift amount is always within the range of 0 to 25. The offset 
+	The function first calculates the shift amount and offset value. The shift amount is calculated by taking
+	the key modulo 26, which ensures that the shift amount is always within the range of 0 to 25. The offset
 	value is calculated by taking 26, which is the number of letters in the alphabet.
 
-	The function then converts the input string to a rune slice (for ease of manipulation). A rune is a single 
+	The function then converts the input string to a rune slice (for ease of manipulation). A rune is a single
 	character in a programming language. The rune type is used to represent characters in Go.
 
-	The function then iterates over each character in the rune slice. For each character, the function checks 
+	The function then iterates over each character in the rune slice. For each character, the function checks
 
-	if the character is a lowercase letter and if shifting it will still be within the lowercase range. 
-	
-	If the character is a lowercase letter and shifting it will still be within the lowercase range, the 
+	if the character is a lowercase letter and if shifting it will still be within the lowercase range.
+
+	If the character is a lowercase letter and shifting it will still be within the lowercase range, the
 	function simply adds the shift amount to the character. If the character is outside of the lowercase range after shifting, the function wraps it around by adding the shift amount - offset to the character.
 
 	The function then updates the character in the rune slice.
 
 	The function then converts the resulting rune slice back to a string and returns it.
+
+	The time complexity of the CaesarCipherEncryptor function is O(n), where n is the length of the input string.
+	This is because the function iterates over each character in the input string, performing a constant amount
+	of work for each character.
+
+	The space complexity of the CaesarCipherEncryptor function is O(n), where n is the length of the input string.
+	This is because the function creates a new rune slice to store the encrypted string. The rune slice is the
+	same size as the input string, so the space complexity is O(n).
 */
 package main
 
