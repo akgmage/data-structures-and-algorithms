35d3d92d7a1f15585fd84a03a30316cceff5b0c6
Anup Khismatrao <khismatrao2003@gmail.com>
2023-06-07 12:18:08 +0530

    Update

--- 2D Arrays (Matrix)/setMatrixZero.java
+++ 2D Arrays (Matrix)/setMatrixZero.java
@@ -14,22 +14,37 @@ E.g- Input: | 1 1 1 |
 	        | 0 0 0 |
 	        | 1 0 1 |
 	
--- Naive Approach for Set Matrix Zeroes
+METHOD 1] Brute Force Approach for Set Matrix Zeroes
 Step 1. Create an array answer of size (n X m) and initialize every element as 1.
 Step 2. Traverse the matrix array row-wise and set the current row as 0 in answer array if the current row contains an element equals to 0.
 Step 3. Traverse the matrix array column-wise and set the current column as 0 in answer array if the current column contains an element equals to 0.
 Step 4. Now traverse the answer array, if the current element is 0, then set this element as 0 in a matrix array.
 Step 5. Return matrix array
 
-Complexity Analysis
+Complexity Analysis:
 Time Complexity = O(n * m)
 Space Complexity = O(n * m) 
 where n is the number of rows in the matrix and m is the number of columns in the matrix.
 
+METHOD 2] Optimal Approach for Set Matrix Zeroes
+If we assume that -1, do not occur in the matrix array, then
+
+Step 1. Traverse the matrix array row-wise and set all the elements of current row which are not 0 as -1, if the current row contains an element equals to 0.
+Step 2. Traverse the matrix array column-wise and set all the elements of the current column which are not 0 as -1, if the current column contains an element equals to 0.
+Step 3. Again traverse the matrix and set all the elements that are -1 to 0.
+Step 4. Return matrix array.
+	
+Complexity Analysis:
+Time Complexity = O(n * m)
+Space Complexity = O(1) 
+where n is the number of rows in the matrix and m is the number of columns in the matrix.
 */
-/*
+
+
 public class setMatrixZero {
-    private static void setZeroes(int[][] matrix, int n, int m) {
+
+    // Method 1] Brute Force Approach
+    private static void setZeroes_bruteForce(int[][] matrix, int n, int m) {
         int answer[][] = new int[n][m]; // Step 1
         // Set all elements of answer array as 1
         for (int i = 0; i < n; i++) {
@@ -37,7 +52,7 @@ public class setMatrixZero {
                 answer[i][j] = 1; // making each element as 1
             }
         }
-        // Traverse row wise -- Step 2
+        // Traverse row wise --> Step 2
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
@@ -49,7 +64,7 @@ public class setMatrixZero {
                 }
             }
         }
-        // Traverse column wise -- Step 3
+        // Traverse column wise --> Step 3
         for (int j = 0; j < m; j++) {
             for (int i = 0; i < n; i++) {
                 if (matrix[i][j] == 0) {
@@ -60,7 +75,7 @@ public class setMatrixZero {
                 }
             }
         }
-        // Update the elements in matrix array -- Step 4
+        // Update the elements in matrix array --> Step 4
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (answer[i][j] == 0) {
@@ -69,51 +84,9 @@ public class setMatrixZero {
             }
         }
     }
-    public static void main(String[] args) {
-        // Example 1
-        int[][] matrix = new int[][] {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
-        int n = matrix.length;
-        int m = matrix[0].length;
-        setZeroes(matrix, n, m);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-        // Example 2
-        matrix = new int[][] {{0, 0, 6, 0}, {1, 4, 9, 0}, {1, 8, 1, 8}};
-        n = matrix.length;
-        m = matrix[0].length;
-        
-        setZeroes(matrix, n, m);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-}
-*/
 
-/* 
--- Optimal Approach for Set Matrix Zeroes
-If we assume that -1, do not occur in the matrix array, then
-
-Step 1. Traverse the matrix array row-wise and set all the elements of current row which are not 0 as -1, if the current row contains an element equals to 0.
-Step 2. Traverse the matrix array column-wise and set all the elements of the current column which are not 0 as -1, if the current column contains an element equals to 0.
-Step 3. Again traverse the matrix and set all the elements that are -1 to 0.
-Step 4. Return matrix array.
-	
-Complexity Analysis
-Time Complexity = O(n * m)
-Space Complexity = O(1) 
-where n is the number of rows in the matrix and m is the number of columns in the matrix.
-*/
-
-public class setMatrixZero {
-    private static void setZeroes(int[][] matrix, int n, int m) {
+    // Method 2] Optimal Approach
+    private static void setZeroes_optimalMethod(int[][] matrix, int n, int m) {
         // Traverse row wise
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -150,26 +123,34 @@ public class setMatrixZero {
             }
         }
     }
+    
     public static void main(String[] args) {
-        // Example 1
-        int[][] matrix = new int[][] {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
-        int n = matrix.length;
-        int m = matrix[0].length;
-        setZeroes(matrix, n, m);
+        // Example using Method 1 - Brute Force
+        int[][] matrix1 = new int[][] {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};    //  Defining Matrix
+        int n = matrix1.length;
+        int m = matrix1[0].length;
+
+        setZeroes_bruteForce(matrix1, n, m);
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix1[i][j] + " ");  // Printing Matrix
             }
             System.out.println();
         }
-        // Example 2
-        matrix = new int[][] {{0, 0, 6, 0}, {1, 4, 9, 0}, {1, 8, 1, 8}};
-        n = matrix.length;
-        m = matrix[0].length;
-        setZeroes(matrix, n, m);
-        for (int i = 0; i < n; i++) {
+
+        System.out.println("-----------------");
+
+        // Example using Method 2 - Optimal 
+        int[][] matrix2 = new int[][] {{0, 0, 6, 0}, {1, 4, 9, 0}, {1, 8, 1, 8}};   //  Defining Matrix
+        n = matrix2.length;
+        m = matrix2[0].length;
+
+        setZeroes_optimalMethod(matrix2, n, m);
+
+        for (int i = 0; i < n; i++) {   
             for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix2[i][j] + " ");  // Printing Matrix
             }
             System.out.println();
         }
