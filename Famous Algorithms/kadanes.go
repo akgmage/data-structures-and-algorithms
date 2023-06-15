dac8ea8b37c57639b423cdfc400136827ec2ed71
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-15 19:30:09 +0530

    add question

--- Famous Algorithms/kadanes.go
+++ Famous Algorithms/kadanes.go
@@ -1,3 +1,10 @@
+/*
+  Write a function that takes in a non-empty array of integers and returns the
+  maximum sum that can be obtained by summing up all of the integers in a
+  non-empty subarray of the input array. A subarray must only contain adjacent
+  numbers (numbers next to each other in the input array).
+
+*/
 package main
 
 // KadanesAlgorithm computes the maximum sum subarray using Kadane's algorithm
