1f898d0b8e8d973a9b6ae699743ee097069e5d84
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-15 19:31:51 +0530

    add explanation and time space complexity

--- Famous Algorithms/kadanes.go
+++ Famous Algorithms/kadanes.go
@@ -1,8 +1,28 @@
 /*
-  Write a function that takes in a non-empty array of integers and returns the
-  maximum sum that can be obtained by summing up all of the integers in a
-  non-empty subarray of the input array. A subarray must only contain adjacent
-  numbers (numbers next to each other in the input array).
+	Write a function that takes in a non-empty array of integers and returns the
+	maximum sum that can be obtained by summing up all of the integers in a
+	non-empty subarray of the input array. A subarray must only contain adjacent
+	numbers (numbers next to each other in the input array).
+
+	Sample Input: [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+	Output: 19
+
+	Explanation:
+
+	The function takes an array of integers as input, and initializes two variables: `maxEndingHere` and `maxSoFar`. 
+	`maxEndingHere` keeps track of the maximum subarray sum that ends at the current element, and `maxSoFar` keeps track 
+	of the maximum subarray sum seen so far.
+
+	The function then iterates through the array starting at the second element, and for each element, it calculates the 
+	maximum subarray sum that ends at that element by adding the current element to the maximum subarray sum that ends at 
+	the previous element (`maxEndingHere + num`). If this sum is greater than the current element itself (`num`), then `maxEndingHere` is updated to the sum. Otherwise, `maxEndingHere` is updated to `num`.
+
+	After each iteration, `maxSoFar` is updated to the maximum value seen so far, which is either `maxEndingHere` or `maxSoFar`.
+
+	The function finally returns `maxSoFar`, which represents the maximum subarray sum of the input array. The `max` function 
+	is a helper function used to compare two integers and return the maximum of the two.
+
+	O(n) time | O(1) space - where n is the length of the input array
 
 */
 package main
