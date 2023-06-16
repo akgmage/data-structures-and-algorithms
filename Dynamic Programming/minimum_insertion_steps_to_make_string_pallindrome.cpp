f1c3878abe33de2fad843b309eeb849048855b24
gargvarunjec <gargvarunjec@gmail.com>
2023-06-16 20:26:48 +0530

    added sample input and output

--- Dynamic Programming/minimum_insertion_steps_to_make_string_pallindrome.cpp
+++ Dynamic Programming/minimum_insertion_steps_to_make_string_pallindrome.cpp
@@ -1,5 +1,22 @@
 /*
-      Program author: Varun Garg
+    Given a string, find the minimum number of insertions needed to make it a palindrome.
+
+    Sample Input: "abcde"
+    Sample Output: 4
+    Explanation: The minimum insertions required are 'edcb' -> "abcdecb", resulting in a palindrome.
+
+    Approach:
+    We can solve this problem using dynamic programming.
+    Let's define a 2D table, dp, where dp[i][j] represents the minimum number of insertions needed to make the substring from index i to j a palindrome.
+    If the characters at indices i and j are equal, then dp[i][j] = dp[i+1][j-1].
+    Otherwise, we have two options:
+        1. Insert the character at index i at the end, i.e., dp[i][j] = dp[i][j-1] + 1.
+        2. Insert the character at index j at the beginning, i.e., dp[i][j] = dp[i+1][j] + 1.
+    We take the minimum of these two options as the minimum number of insertions required for the substring from index i to j.
+    Finally, the minimum number of insertions needed for the entire string is dp[0][n-1], where n is the length of the string.
+
+    Time complexity: O(n^2)
+    Space complexity: O(n^2)
 */
 
 #include <bits/stdc++.h>
