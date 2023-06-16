97509d62fa55461ebbec4467c6c6b2c68acaf8f3
harshitcompcode <84669711+harshitcompcode@users.noreply.github.com>
2023-06-17 00:01:16 +0530

    Update Snakes_and_Ladders.java
    Comments added

--- Graphs/Snakes_and_Ladders.java
+++ Graphs/Snakes_and_Ladders.java
@@ -1,92 +1,115 @@
-Here's the Java implementation of the solution along with an explanation:
-
-import java.util.ArrayDeque;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
-
-public class SnakesAndLadders {
-    
-    private static class Position {
-        int square;
-        int moves;
-        
-        public Position(int square, int moves) {
-            this.square = square;
-            this.moves = moves;
-        }
-    }
-    
-    private static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    
-    private static int[][] getBoardPosition(int square, int n) {
-        int row = n - 1 - (square - 1) / n;
-        int col = (row % 2 == n % 2) ? (square - 1) % n : n - 1 - (square - 1) % n;
-        return new int[]{row, col};
-    }
-    
-    public static int snakesAndLadders(int[][] board) {
-        int n = board.length;
-        int destination = n * n;
-        
-        Queue<Position> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-        
-        queue.add(new Position(1, 0)); // Start position
-        visited.add(1); // Mark the start position as visited
-        
-        while (!queue.isEmpty()) {
-            Position position = queue.poll();
-            int square = position.square;
-            int moves = position.moves;
-            
-            if (square == destination) {
-                return moves;
-            }
-            
-            for (int roll = 1; roll <= 6; roll++) {
-                int nextSquare = square + roll;
-                
-                if (nextSquare > destination) {
-                    break;
-                }
-                
-                int[] pos = getBoardPosition(nextSquare, n);
-                int row = pos[0];
-                int col = pos[1];
-                
-                if (!visited.contains(nextSquare)) {
-                    visited.add(nextSquare);
-                    
-                    if (board[row][col] != -1) {
-                        queue.add(new Position(board[row][col], moves + 1));
-                    } else {
-                        queue.add(new Position(nextSquare, moves + 1));
-                    }
-                }
-            }
-        }
-        
-        return -1;
-    }
-    
-    public static void main(String[] args) {
-        int[][] board = {
-            {-1, 4},
-            {-1, 3}
-        };
-        
-        System.out.println(snakesAndLadders(board)); // Output: 1
-    }
-}
-
 Explanation:
 
-The Position class is used to store the square number and the number of moves taken to reach that square.
-The getBoardPosition function is used to convert a square number to its corresponding (row, column) position on the board.
-The snakesAndLadders function implements the breadth-first search (BFS) algorithm to find the shortest path from square 1 to square n^2.
-The function uses a queue (ArrayDeque) to store the positions to be explored during BFS and a set (HashSet) to keep track of visited positions.
-The function starts by enqueuing the starting position (1, 0) into the queue and marking it as visited.
-While the queue is not empty, it dequeues a position and checks if it is the destination square. If it is, the number of moves is returned.
-Otherwise, the function generates the next possible destinations based on the roll of a 6-sided die and checks if they are valid and not visited.
-If a destination has a snake or ladder, its destination is enqueued
+We start by importing the necessary classes and defining the class SnakesAndLadders.
+
+We declare the constant BOARD_SIZE to represent the number of cells on the board.
+
+We declare two maps: snakes to store the snake positions, and ladders to store the ladder positions.
+
+The SnakesAndLadders class has a constructor that initializes the snakes and ladders maps.
+
+The addSnake method is used to add a snake to the game by providing the start and end positions.
+
+The addLadder method is used to add a ladder to the game by providing the start and end positions.
+
+The playGame method simulates the game. It starts with the player's position at 0 and the number of dice rolls at 0.
+
+Inside the while loop, a dice is rolled using the rollDice method, and the player's position is updated.
+
+After each move, we check if the player has landed on a ladder or a snake. If so, we update the player's position accordingly.
+
+The rollDice method generates a random number between 1 and 6, simulating a dice roll.
+
+In the main method, we create an instance of SnakesAndLadders and add snakes and ladders to the game.
+
+Finally, we call the playGame method to start the game and print the number of dice rolls required to reach or exceed the BOARD_SIZE.
+
+Below is an implementation of the Snakes and Ladders game in Java, along with comments:
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SnakesAndLadders {
+    private static final int BOARD_SIZE = 100;  // Number of cells in the board
+    private Map<Integer, Integer> snakes;        // Map to store snake positions
+    private Map<Integer, Integer> ladders;       // Map to store ladder positions
+
+    public SnakesAndLadders() {
+        // Initialize the snake and ladder positions
+        snakes = new HashMap<>();
+        ladders = new HashMap<>();
+    }
+
+    public void addSnake(int start, int end) {
+        snakes.put(start, end);
+    }
+
+    public void addLadder(int start, int end) {
+        ladders.put(start, end);
+    }
+
+    public int playGame() {
+        int currentPlayerPosition = 0;  // Player's current position
+        int diceRolls = 0;              // Number of dice rolls
+
+        while (currentPlayerPosition < BOARD_SIZE) {
+            // Roll a dice
+            int dice = rollDice();
+
+            // Move the player
+            currentPlayerPosition += dice;
+
+            // Check if the player has landed on a ladder
+            if (ladders.containsKey(currentPlayerPosition)) {
+                // Climb the ladder
+                currentPlayerPosition = ladders.get(currentPlayerPosition);
+            }
+
+            // Check if the player has landed on a snake
+            if (snakes.containsKey(currentPlayerPosition)) {
+                // Go down the snake
+                currentPlayerPosition = snakes.get(currentPlayerPosition);
+            }
+
+            diceRolls++;  // Increment the number of dice rolls
+        }
+
+        return diceRolls;
+    }
+
+    private int rollDice() {
+        // Generates a random number between 1 and 6 (inclusive)
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    public static void main(String[] args) {
+        SnakesAndLadders game = new SnakesAndLadders();
+
+        // Add snakes to the game
+        game.addSnake(16, 6);
+        game.addSnake(47, 26);
+        game.addSnake(49, 11);
+        game.addSnake(56, 53);
+        game.addSnake(62, 19);
+        game.addSnake(64, 60);
+        game.addSnake(87, 24);
+        game.addSnake(93, 73);
+        game.addSnake(95, 75);
+        game.addSnake(98, 78);
+
+        // Add ladders to the game
+        game.addLadder(1, 38);
+        game.addLadder(4, 14);
+        game.addLadder(9, 31);
+        game.addLadder(21, 42);
+        game.addLadder(28, 84);
+        game.addLadder(36, 44);
+        game.addLadder(51, 67);
+        game.addLadder(71, 91);
+        game.addLadder(80, 100);
+
+        // Play the game
+        int diceRolls = game.playGame();
+        System.out.println("Number of dice rolls: " + diceRolls);
+    }
+}
