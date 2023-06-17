6e29a397c37e89fc5ab99ea7d19c031a46501bd1
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-17 19:11:38 +0530

    add question

--- Graphs/union_find.go
+++ Graphs/union_find.go
@@ -1,4 +1,21 @@
 /*
+	Write a UnionFind class that implements the union-find (also called a disjoint set) data structure.
+	This class should support three methods:
+
+
+	The union-find data structure is similar to a traditional set data structure in that it contains a collection
+	of unique values. However, these values are spread out amongst a variety of distinct disjoint sets, meaning that no set
+	can have duplicate values, and no two sets can contain the same value.
+
+	createSet(value) : : Adds a given value in a new set containing only that value.
+
+	union(valueOne, valueTwo) : : Takes in two values and determines which sets they are in. If they are in different sets, the sets are combined
+    into a single set. If either value is not in a set or they are in the same set, the function should have no effect.
+
+	find(value): : Returns the "representative" value of the set for which a value belongs to. This can be any value in the set, but it should
+    always be the same value, regardless of which value in the set find  is passed. If the value is not in a set, the function
+    should return null / none
+
 	Explanation:
 	The code snippet you provided implements the Union-Find data structure, which is used for efficient operations on disjoint
 	sets. Here's a detailed explanation of each part:
