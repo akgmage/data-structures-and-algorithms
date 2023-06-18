11f0f3a2c491234ac33361cbd6cef21710b5b1f1
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-06-18 21:05:00 +0530

    Update and rename Ford_Fulkerson.cpp to fordFulkerson.cpp

--- Graphs/fordFulkerson.cpp
+++ Graphs/fordFulkerson.cpp
@@ -14,8 +14,9 @@ Explanation of the below C++ code :
 The Ford-Fulkerson algorithm is a widely used algorithm to solve the maximum flow problem in a flow network. The maximum flow problem involves determining the maximum amount of flow that can be sent from a source vertex to a sink vertex in a directed weighted graph, subject to capacity constraints on the edges.
 The algorithm works by iteratively finding an augmenting path, which is a path from the source to the sink in the residual graph, i.e., the graph obtained by subtracting the current flow from the capacity of each edge. The algorithm then increases the flow along this path by the maximum possible amount, which is the minimum capacity of the edges along the path.
 
-*/
+Time Complexity : O(|V| * E^2) ,where E is the number of edges and V is the number of vertices.
 
+Space Complexity :O(V) , as we created queue.*/
 -------------------------------------------------------------------------//C++ code begins here------------------------------------------------------------------------
 
 
@@ -138,3 +139,6 @@ int main()
 
 	return 0;
 }
+
+/*Sample Output:
+The maximum possible flow is 23. */
