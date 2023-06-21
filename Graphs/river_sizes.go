b33db213bf75a29b94dcc39ee9facc3c0e16294f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-21 22:42:51 +0530

    add explanation

--- Graphs/river_sizes.go
+++ Graphs/river_sizes.go
@@ -1,3 +1,45 @@
+/*
+
+	Explanation:
+
+	1. The function `RiverSizes` initializes an empty slice `sizes` to store the sizes of rivers found in the matrix. It also creates a 2D `visited` matrix of the same size as the input matrix to keep track of visited nodes.
+
+	2. The function then iterates over each cell in the matrix using nested loops.
+
+	3. If a cell has already been visited (marked as `true` in the `visited` matrix), the code continues to the next iteration to avoid processing it again.
+
+	4. If a cell has not been visited, the code calls the `traverseNode` function to explore the river connected to that cell and updates the `sizes` slice with the size of the river.
+
+	5. The `traverseNode` function takes the starting position (i, j) of a river, the matrix, the `visited` matrix, and the `sizes` slice as input.
+
+	6. It initializes a variable `currentRiverSize` to 0 to keep track of the size of the river being explored.
+
+	7. It maintains a queue (`nodesToExplore`) to store the nodes that need to be visited. It starts with the initial node (i, j).
+
+	8. The function enters a loop that continues until there are no more nodes to explore in the queue.
+
+	9. In each iteration, it dequeues the first node from the `nodesToExplore` queue and updates the current position (i, j) accordingly.
+
+	10. If the current node has already been visited, the code continues to the next iteration.
+
+	11. If the current node contains a value of 0 (indicating land), the code continues to the next iteration, as it's not part of the river.
+
+	12. If the current node contains a value of 1 (indicating a river), it increments the `currentRiverSize` by 1.
+
+	13. It then retrieves the unvisited neighboring nodes of the current node using the `getUnvisitedNeighbors` function and adds them to the `nodesToExplore` queue.
+
+	14. Once the loop finishes, if the `currentRiverSize` is greater than 0, it appends it to the `sizes` slice.
+
+	15. Finally, the `sizes` slice containing the sizes of all rivers is returned.
+
+	16. The `getUnvisitedNeighbors` function takes the current position (i, j), the matrix, and the `visited` matrix as input.
+
+	17. It checks the four neighboring cells (up, down, left, right) of the current cell and adds the unvisited neighbors to the `unvisitedNeighbors` slice.
+
+	18. The function returns the `unvisitedNeighbors` slice.
+
+	The code essentially performs a depth-first search (DFS) traversal on the matrix to find connected regions of 1s (rivers) and keeps track of their sizes. The `visited` matrix helps avoid revisiting nodes that have already been processed, ensuring each river is counted only once.
+*/
 package main
 
 func RiverSizes(matrix [][]int) []int {
