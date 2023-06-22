2bc4081792b577ac2fa89018127d7111d111cc94
harshitcompcode <84669711+harshitcompcode@users.noreply.github.com>
2023-06-22 22:22:59 +0530

    Update Snakes_and_Ladders.java
    I applied the recommended changes

--- Graphs/Snakes_and_Ladders.java
+++ Graphs/Snakes_and_Ladders.java
@@ -1,18 +1,33 @@
+/*You are given an n x n integer matrix board where the cells are labeled from 1 to n2 in a Boustrophedon style starting from the bottom left of the board (i.e. board[n - 1][0]) and alternating direction each row.
+
+You start on square 1 of the board. In each move, starting from square curr, do the following:
+
+Choose a destination square next with a label in the range [curr + 1, min(curr + 6, n2)].
+This choice simulates the result of a standard 6-sided die roll: i.e., there are always at most 6 destinations, regardless of the size of the board.
+If next has a snake or ladder, you must move to the destination of that snake or ladder. Otherwise, you move to next.
+The game ends when you reach the square n2.
+A board square on row r and column c has a snake or ladder if board[r][c] != -1. The destination of that snake or ladder is board[r][c]. Squares 1 and n2 do not have a snake or ladder.
+
+Note that you only take a snake or ladder at most once per move. If the destination to a snake or ladder is the start of another snake or ladder, you do not follow the subsequent snake or ladder.
+
+For example, suppose the board is [[-1,4],[-1,3]], and on the first move, your destination square is 2. You follow the ladder to square 3, but do not follow the subsequent ladder to 4.
+Return the least number of moves required to reach the square n2. If it is not possible to reach the square, return -1.
+
 Explanation:
 
-// We start by importing the necessary classes and defining the class SnakesAndLadders.
-// We declare the constant BOARD_SIZE to represent the number of cells on the board.
-// We declare two maps: snakes to store the snake positions, and ladders to store the ladder positions.
-// The SnakesAndLadders class has a constructor that initializes the snakes and ladders maps.
-// The addSnake method is used to add a snake to the game by providing the start and end positions.
-// The addLadder method is used to add a ladder to the game by providing the start and end positions.
-// The playGame method simulates the game. It starts with the player's position at 0 and the number of dice rolls at 0.
-// Inside the while loop, a dice is rolled using the rollDice method, and the player's position is updated.
-// After each move, we check if the player has landed on a ladder or a snake. If so, we update the player's position accordingly.
-// The rollDice method generates a random number between 1 and 6, simulating a dice roll.
-// In the main method, we create an instance of SnakesAndLadders and add snakes and ladders to the game.
-// Finally, we call the playGame method to start the game and print the number of dice rolls required to reach or exceed the BOARD_SIZE.
-// Below is an implementation of the Snakes and Ladders game in Java, along with comments:
+We start by importing the necessary classes and defining the class SnakesAndLadders.
+We declare the constant BOARD_SIZE to represent the number of cells on the board.
+We declare two maps: snakes to store the snake positions, and ladders to store the ladder positions.
+The SnakesAndLadders class has a constructor that initializes the snakes and ladders maps.
+The addSnake method is used to add a snake to the game by providing the start and end positions.
+The addLadder method is used to add a ladder to the game by providing the start and end positions.
+The playGame method simulates the game. It starts with the player's position at 0 and the number of dice rolls at 0.
+Inside the while loop, a dice is rolled using the rollDice method, and the player's position is updated.
+After each move, we check if the player has landed on a ladder or a snake. If so, we update the player's position accordingly.
+The rollDice method generates a random number between 1 and 6, simulating a dice roll.
+In the main method, we create an instance of SnakesAndLadders and add snakes and ladders to the game.
+Finally, we call the playGame method to start the game and print the number of dice rolls required to reach or exceed the BOARD_SIZE.
+Below is an implementation of the Snakes and Ladders game in Java, along with comments:*/
 
 import java.util.HashMap;
 import java.util.Map;
