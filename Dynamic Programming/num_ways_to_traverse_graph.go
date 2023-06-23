f1f383426f1e68f0b0c645ae413026746b4ad9cc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-23 23:14:43 +0530

    add question

--- Dynamic Programming/num_ways_to_traverse_graph.go
+++ Dynamic Programming/num_ways_to_traverse_graph.go
@@ -1,3 +1,24 @@
+/*
+
+	You're given two positive integers representing the width and height of a grid-shaped, rectangular graph.
+	Write a function that returns the number of ways to reach the bottom right corner of the graph when starting
+	at the top left corner. Each move you take must either go down or right. In other words, you can never move up
+	or left in the graph.
+
+	For example, given the graph illustrated below, with width = 2 and height = 3 , there are three ways to
+	reach the bottom right corner when starting at the top left corner:
+	 _ _
+	|_|_|
+	|_|_|
+	|_|_|
+	Down Down Right
+	Right Down Down
+	Down Right Down
+
+	Sample Input: Width : 4 height: 3
+	Output: 10
+
+*/
 package main
 
 func NumberOfWaysToTraverseGraph(width int, height int) int {
