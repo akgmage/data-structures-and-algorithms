7b6495d547a7a6c0996288fab9e75462f2a33682
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-06-24 22:52:35 +0530

    Update longest_string.cpp

--- Strings/longest_string.cpp
+++ Strings/longest_string.cpp
@@ -1,24 +1,39 @@
+/*The code implements the dynamic programming approach to find the longest palindrome substring in a given string. It uses a two-dimensional table dp to store the results of subproblems, where dp[i][j] represents whether the substring from index i to index j is a palindrome.
+
+The algorithm first checks for all substrings of length 1 and marks them as palindromes. Then it checks for substrings of length 2 and updates the start and maxLen variables if a palindrome of length 2 is found.
+
+After that, it checks for substrings of length greater than 2 by iterating over all possible lengths and positions. If a palindrome is found at a particular position, it updates the start and maxLen variables accordingly.
+
+*/
+
 #include <string>
+#include <vector>
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
+    /**
+     * Finds the longest palindrome substring within a given string.
+     *
+     * @param s The input string.
+     * @return The longest palindrome substring.
+     */
+    std::string longestPalindrome(std::string s) {
         int n = s.length();
         if (n < 2) {
             return s;
         }
-        
+
         int start = 0;  // start index of the longest palindrome
         int maxLen = 1; // length of the longest palindrome
-        
+
         // Initialize a table to store the results of subproblems
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        
+        std::vector<std::vector<bool>> dp(n, std::vector<bool>(n, false));
+
         // All substrings of length 1 are palindromes
         for (int i = 0; i < n; i++) {
             dp[i][i] = true;
         }
-        
+
         // Check for substrings of length 2
         for (int i = 0; i < n - 1; i++) {
             if (s[i] == s[i + 1]) {
@@ -27,7 +42,7 @@ public:
                 maxLen = 2;
             }
         }
-        
+
         // Check for substrings of length greater than 2
         for (int len = 3; len <= n; len++) {
             for (int i = 0; i < n - len + 1; i++) {
@@ -39,8 +54,9 @@ public:
                 }
             }
         }
-        
+
         // Return the longest palindrome substring
         return s.substr(start, maxLen);
     }
 };
+
