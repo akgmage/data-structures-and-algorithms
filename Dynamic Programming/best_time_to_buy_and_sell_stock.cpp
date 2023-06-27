8727d08c73e867e0e6818996a2df6ab7a3e98c0b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-27 22:30:46 +0530

    add sample io

--- Dynamic Programming/best_time_to_buy_and_sell_stock.cpp
+++ Dynamic Programming/best_time_to_buy_and_sell_stock.cpp
@@ -6,6 +6,9 @@
 	If the price is less than the current minimum price, we update the minimum price.
 	Otherwise, if the difference between the price and the minimum price is greater than the current maximum profit, we update the maximum profit.
 	Finally, we return the maximum profit
+
+    Sample Input [7, 1, 5, 3, 6, 4]
+    Output: 5 buy at 1 sell at 6
     
 	Time Complexity: O(n), where n is the length of the prices array.
 	Space Complexity: O(1), as we are only using two variables to keep track of the minimum price and maximum profit
