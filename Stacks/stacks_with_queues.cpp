77c4ce06c03eabbd160c8c209abca37a1eab4345
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-06-28 18:41:01 +0530

    Update stacks_with_queues.cpp

--- Stacks/stacks_with_queues.cpp
+++ Stacks/stacks_with_queues.cpp
@@ -64,19 +64,3 @@ public:
     }
 };
 
-
-/*
-This implementation maintains two queues, queue1 and queue2, where queue1 always holds the elements in the stack. When pushing a new element, it is added to queue2, and then all the elements from queue1 are moved to queue2, making the newly added element the front/top of the stack. Finally, the names of the two queues are swapped to maintain the order.
-
-The pop() function removes and returns the front/top element of queue1, while the top() function returns the front/top element without removing it. Both functions check if queue1 is empty and throw an exception if the stack is empty.
-
-The empty() function checks if queue1 is empty and returns true if it is, indicating an empty stack.
-
-Time complexity
-
-push(x): O(n)
-pop(): O(1)
-top(): O(1)
-empty(): O(1)
-*/
-  
