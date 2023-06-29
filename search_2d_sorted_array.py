da319af3315fa8958c01b88e405945dacb850fa8
maneesha <97738136+Mani1881@users.noreply.github.com>
2023-06-29 08:16:53 +0530

    Update search_2d_sorted_array.py
    >>provided optimal solution

--- search_2d_sorted_array.py
+++ search_2d_sorted_array.py
@@ -20,40 +20,56 @@ Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 Output: false
 
 //Explanation
-The searchMatrix function takes two parameters: matrix (a 2D array) and target (the value to be searched).
-It initializes the variable m to store the length of the matrix (number of rows).
-It then iterates through each row of the matrix using a for loop.
-Inside the outer loop, there's another nested loop that iterates through each element of the current row.
-It checks if the current element is equal to the target value. 
-If a match is found, it immediately returns True, indicating that the target is present in the matrix.
-Else the function returns False, indicating that the target is not present in the matrix.
-In the main section, an instance of the Solution class is created.
-A sample matrix is defined with some values.
-The target value is set to 5.
-The searchMatrix function is called with the matrix and target as arguments, and the result is stored in the variable found.
-Finally, it prints whether the target was found or not.
+The method takes two parameters: matrix, which represents the sorted matrix, and target, which is the value we want to find in the matrix.
+
+The code initializes variables nRows and nCols to store the number of rows and columns in the matrix, respectively.
+
+The starting position for the search is set to the bottom-left corner of the matrix (row = nRows - 1, col = 0).
+
+The code enters a while loop that continues as long as the current row index (row) is within the bounds of the matrix (0 to nRows - 1
+and the current column index (col) is within the bounds of the matrix (0 to nCols - 1).
+
+Inside the loop, the code retrieves the value at the current position in the matrix (val = matrix[row][col]).
+
+If the current value (val) == value, the method returns True, indicating that the target is found in the matrix.
+
+If the current value (val) < target value, it means the target can only be found in the rows above the current row. 
+Therefore, the column index (col) is incremented to move to the next column.
+
+If the current value (val) >target value, it means the target can only be found in the columns to the left of the current column. 
+Therefore, the row index (row) is decremented to move to the previous row.
+
+If the loop completes without finding the target value, the method returns False.
 
 '''
-class Solution:
+class Solution(object):
     def searchMatrix(self, matrix, target):
-        m = len(matrix)
-        for i in range(m):
-            for j in range(len(matrix[i])):
-                if matrix[i][j] == target:
-                    return True
+        nRows = len(matrix)
+        nCols = len(matrix[0])
+        row = nRows - 1
+        col = 0
+        while 0 <= row < nRows and 0 <= col < nCols:
+            val = matrix[row][col]
+            if val == target:
+                return True
+            elif val < target:
+                col += 1
+            else:
+                row -= 1
         return False
 
-if __name__ == "__main__":
-    solution = Solution()
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-    target = 5
 
-    found = solution.searchMatrix(matrix, target)
-    print("Target found:", found)
+# Create an instance of the Solution class
+solution = Solution()
+
+# Define the matrix and target value
+matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+target = 3
+
+# Call the searchMatrix method and print the result
+result = solution.searchMatrix(matrix, target)
+print(result)
+
 
 
 
