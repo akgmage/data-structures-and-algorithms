41022242f274ba822dc12ae5fa6283e771557a45
Arsh <arsh.98k@gmail.com>
2023-07-02 00:40:21 +0530

    update trie.java

--- Trees/trie.java
+++ Trees/trie.java
@@ -1,3 +1,15 @@
+/*
+Description
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+*/
+
 class Trie {
     Node root;
 
