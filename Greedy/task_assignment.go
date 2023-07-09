512ead9c39de5935515f01950638f6a425f2f99b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-07-09 23:18:00 +0530

    add question

--- Greedy/task_assignment.go
+++ Greedy/task_assignment.go
@@ -1,4 +1,19 @@
 /*
+	
+  	You're given an integer k  representing a number of workers and an array of positive integers representing durations of 
+	tasks that must be completed by the workers. Specifically, each worker must complete two unique tasks and can only work 
+	on one task at a time. The number of tasks will always be equal to 2k  such that each worker always has exactly two tasks
+	to complete. All tasks are independent of one another and can be completed in any order. Workers will complete their 
+	assigned tasks in parallel, and the time taken to complete all tasks will be equal to the time taken to complete
+	the longest pair of tasks (see the sample output for an explanation).
+
+  
+	Write a function that returns the optimal assignment of tasks to each worker such that the tasks are completed as fast 
+	as possible. Your function should return a list of pairs, where each pair stores the indices of the tasks that should 
+	be completed by one worker. The pairs should be in the following format: [task1, task2] , where the order of task1 and 
+	task2  doesn't matter. Your function can return the pairs in any order. If multiple optimal assignments exist, any 
+	correct answer will be accepted.
+	
 	Explanation:
 	The code snippet is an implementation of the "Task Assignment" algorithm. It pairs tasks from a list based on their durations. The goal is to assign tasks to workers in a way that minimizes the total execution time.
 
