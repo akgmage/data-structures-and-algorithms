cd3643d3326fc85cf00b49b3dcb9e1f7602f037d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-07-19 23:10:06 +0530

    add sample io

--- Graphs/topological_sort.go
+++ Graphs/topological_sort.go
@@ -1,4 +1,4 @@
-/*	
+/*
   You're given a list of arbitrary jobs that need to be completed; these jobs
   are represented by distinct integers. You're also given a list of dependencies. A
   dependency is represented as a pair of jobs where the first job is a
@@ -8,6 +8,11 @@
   Write a function that takes in a list of jobs and a list of dependencies and
   returns a list containing a valid order in which the given jobs can be
   completed. If no such order exists, the function should return an empty array.
+
+  Sample INput:
+  jobs: [1, 2, 3, 4]
+  deps:  = [[1, 2], [1, 3], [3, 2], [4, 2], [4, 3]]
+  Output: [1, 4, 3, 2] or [4, 1, 3, 2]
 */
 package main
 
