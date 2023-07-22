f79e5733f5a4831bf16fc68a9d5a15f5d43a6b39
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-07-22 23:00:16 +0530

    add sample io and question

--- 2D Arrays (Matrix)/zigzag_traversal.go
+++ 2D Arrays (Matrix)/zigzag_traversal.go
@@ -1,4 +1,16 @@
 /*
+
+	Write a function that takes in an n x m two-dimensional array (that can be square-shaped when n == m)
+	and returns a one-dimensional array of all the array's elements in zigzag order.
+
+	Sample Input:= [
+		[1,  3,  4, 10],
+		[2,  5,  9, 11],
+		[6,  8, 12, 15],
+		[7, 13, 14, 16],
+	]
+	Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
 	Explanation:
 	The given code snippet implements the ZigzagTraverse algorithm, which traverses a 2D array in a zigzag pattern and
 	returns the elements in a 1D array.
