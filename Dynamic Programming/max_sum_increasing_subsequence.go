804d8840859e95de16b02857d07be20d21b8f824
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-07-25 22:27:42 +0530

    add comments

--- Dynamic Programming/max_sum_increasing_subsequence.go
+++ Dynamic Programming/max_sum_increasing_subsequence.go
@@ -2,43 +2,67 @@ package main
 
 import "math"
 
+// MaxSumIncreasingSubsequence finds the maximum sum increasing subsequence in the given array of integers.
 func MaxSumIncreasingSubsequence(array []int) (int, []int) {
-    sums := make([]int, len(array))
-    sequences := make([]int, len(array))
-    for i := range sequences {
-        sequences[i] = math.MinInt32
-        sums[i] = array[i]
-    }
-    maxSumIndex := 0
-    for i, currNum := range array {
-        for j := 0; j < i; j++ {
-            otherNum := array[j]
-            if otherNum < currNum && currNum + sums[j] >= sums[i] {
-                sums[i] = currNum + sums[j]
-                sequences[i] = j
-            }
-        }
-        if sums[i] > sums[maxSumIndex] {
-            maxSumIndex = i
-        }
-    }
-    sum := sums[maxSumIndex]
-    sequence := buildSequence(array, sequences, maxSumIndex)
-    return sum, sequence
+	// Initialize two arrays to store maximum sums and the previous elements contributing to the sums.
+	sums := make([]int, len(array))
+	sequences := make([]int, len(array))
+
+	// Initialize each element in 'sums' array to its corresponding element in the input array.
+	// Also, initialize each element in 'sequences' array to a sentinel value 'math.MinInt32'.
+	for i := range sequences {
+		sequences[i] = math.MinInt32
+		sums[i] = array[i]
+	}
+
+	// Variable to keep track of the index with the maximum sum.
+	maxSumIndex := 0
+
+	// Iterate through the input array and calculate the maximum sum increasing subsequences.
+	for i, currNum := range array {
+		for j := 0; j < i; j++ {
+			otherNum := array[j]
+			if otherNum < currNum && currNum+sums[j] >= sums[i] {
+				// If the current element can extend the increasing subsequence with a better sum,
+				// update the 'sums' and 'sequences' arrays accordingly.
+				sums[i] = currNum + sums[j]
+				sequences[i] = j
+			}
+		}
+
+		// Update the index with the maximum sum if the current sum is greater.
+		if sums[i] > sums[maxSumIndex] {
+			maxSumIndex = i
+		}
+	}
+
+	// Get the maximum sum from the 'sums' array and the increasing subsequence from the 'sequences' array.
+	sum := sums[maxSumIndex]
+	sequence := buildSequence(array, sequences, maxSumIndex)
+	return sum, sequence
 }
 
+// buildSequence reconstructs the increasing subsequence from the 'sequences' array.
 func buildSequence(array []int, sequences []int, index int) []int {
-    sequence := []int{}
-    for index != math.MinInt32 {
-        sequence = append(sequence, array[index])
-        index = sequences[index]
-    }
-    reverse(sequence)
-    return sequence
+	sequence := []int{}
+
+	// Traverse the 'sequences' array starting from the 'index' until reaching the sentinel value 'math.MinInt32'.
+	for index != math.MinInt32 {
+		// Add the element at the current index to the 'sequence' array.
+		sequence = append(sequence, array[index])
+
+		// Move to the previous index using 'sequences' array.
+		index = sequences[index]
+	}
+
+	// Reverse the 'sequence' array since it was built backward.
+	reverse(sequence)
+	return sequence
 }
 
+// reverse is a helper function used to reverse the elements in the 'sequence' array.
 func reverse(numbers []int) {
-    for i, j := 0, len(numbers) - 1; i < j; i, j = i+1, j-1 {
-        numbers[i], numbers[j] = numbers[j], numbers[i]
-    }
+	for i, j := 0, len(numbers)-1; i < j; i, j = i+1, j-1 {
+		numbers[i], numbers[j] = numbers[j], numbers[i]
+	}
 }
