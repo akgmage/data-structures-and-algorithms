cd20fb9f3aa5e50f96ae2cc3a58e799b8d692fad
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-07-27 21:47:15 +0530

    add optimal sol

--- Dynamic Programming/min_number_of_jumps.go
+++ Dynamic Programming/min_number_of_jumps.go
@@ -37,3 +37,40 @@ func min(a, b int) int {
 	}
 	return b
 }
+
+
+func MinNumberOfJumpsOptimal(array []int) int {
+	// If the array has only one element, no jumps are needed.
+	if len(array) == 1 {
+		return 0
+	}
+
+	// Initialize variables to keep track of jumps, maximum reachable position, and remaining steps in a jump.
+	jumps := 0
+	maxreach, steps := array[0], array[0]
+
+	// Iterate through the array to calculate the minimum number of jumps.
+	// We stop at the second-to-last element as we don't need an additional jump from there.
+	for i := 1; i < len(array)-1; i++ {
+		// Update the maximum reachable position if the current position plus the value at that index is greater.
+		if i+array[i] > maxreach {
+			maxreach = i + array[i]
+		}
+
+		// Decrement the remaining steps in the current jump.
+		steps--
+
+		// If the current jump is completed (steps becomes 0), calculate the new jump.
+		if steps == 0 {
+			// Increment jumps to count the completed jump.
+			jumps++
+
+			// Update steps to the number of steps required to reach the farthest position.
+			steps = maxreach - i
+		}
+	}
+
+	// The minimum number of jumps to reach the last element is the total number of jumps taken plus one
+	// because the loop doesn't consider the last element (as we don't need an additional jump from there).
+	return jumps + 1
+}
