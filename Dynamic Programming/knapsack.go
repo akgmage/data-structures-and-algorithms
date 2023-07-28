ad5533bef909508d584437134d320744d79346cc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-07-28 22:14:19 +0530

    add question

--- Dynamic Programming/knapsack.go
+++ Dynamic Programming/knapsack.go
@@ -1,3 +1,15 @@
+/*
+  You're given an array of arrays where each subarray holds two integer values and represents an item;
+  the first integer is the item's value, and the second integer is the item's weight.
+  You're also given an integer representing the maximum capacity of a knapsack that you have.
+
+  Your goal is to fit items in your knapsack without having the sum of their weights exceed the knapsack's
+  capacity, all the while maximizing their combined value. Note that you only have one of each item at your disposal.
+
+  Write a function that returns the maximized combined value of the items that you should pick as well as an array of
+  the indices of each item picked.
+
+*/
 package main
 
 func KnapsackProblem(items [][]int, capacity int) []interface{} {
