8acd1f0fed0fc181cd68179e8f0a6fa77f4ad054
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-09-05 21:06:17 +0530

    update js verion

--- Dynamic Programming/knight_probability_chessboard.js
+++ Dynamic Programming/knight_probability_chessboard.js
@@ -1,39 +1,75 @@
-/*Time Complexity: The time complexity of the code is O(n^2 * k) because, in the calculateProbability function, we have a loop that iterates over each cell on the chessboard (n x n) and for each cell, we make recursive calls for k moves. So, the total number of iterations would be proportional to n^2 * k.
-Space Complexity: The space complexity of the code is O(n^2 * k) because we use a 3D array dp to store the calculated probabilities for each cell and the number of moves. The size of this array is n x n x k, which requires space proportional to n^2 * k.
-*/
+/*
+On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1).
 
-var knightProbability = function(n, k, row, column) {
-    const moves = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]];
-    const dp = Array.from({ length: n }, () => Array.from({ length: n }, () => Array(k + 1).fill(0.0)));
-    // 3D array to store calculated probabilities
-    
-    const calculateProbability = (n, k, row, column) => {
-        // Base cases
-        if (row < 0 || row >= n || column < 0 || column >= n) {
-            return 0.0; // Knight is out of the chessboard
+A chess knight has eight possible moves it can make, as illustrated below. Each move is two cells in a cardinal direction, then one cell in an orthogonal direction.
+
+
+Each time the knight is to move, it chooses one of eight possible moves uniformly at random (even if the piece would go off the chessboard) and moves there.
+
+The knight continues moving until it has made exactly k moves or has moved off the chessboard.
+
+Return the probability that the knight remains on the board after it has stopped moving.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 2, row = 0, column = 0
+Output: 0.06250
+Explanation: There are two moves (to (1,2), (2,1)) that will keep the knight on the board.
+From each of those positions, there are also two moves that will keep the knight on the board.
+The total probability the knight stays on the board is 0.0625.
+Example 2:
+
+Input: n = 1, k = 0, row = 0, column = 0
+Output: 1.00000
+*/
+var knightProbability = function (n, k, row, column) {
+  // Create a 3D grid to store the probabilities
+  let dp = new Array(n)
+    .fill(0)
+    .map(() => new Array(n).fill(0).map(() => new Array(k + 1).fill(0)));
+
+  // Define the eight possible knight moves
+  const moves = [
+    [-2, -1],
+    [-2, 1],
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
+  ];
+
+  // Set the initial probability of the knight being on the starting cell to 1
+  dp[row][column][0] = 1.0;
+
+  // Calculate the probabilities for each move
+  for (let s = 1; s <= k; s++) {
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        for (const move of moves) {
+          const x = i + move[0];
+          const y = j + move[1];
+
+          // Check if the move is within the chessboard
+          if (x >= 0 && x < n && y >= 0 && y < n) {
+            // Accumulate the probability for the current cell
+            dp[i][j][s] += dp[x][y][s - 1] / 8.0;
+          }
         }
-        
-        if (k === 0) {
-            return 1.0; // Knight has made all the moves, so it remains on the board
-        }
-        
-        if (dp[row][column][k] > 0.0) {
-            return dp[row][column][k]; // Return already calculated probability
-        }
-        
-        let probability = 0.0;
-        
-        for (const knightMove of moves) {
-            const nextRow = row + knightMove[0];
-            const nextColumn = column + knightMove[1];
-            
-            probability += calculateProbability(n, k - 1, nextRow, nextColumn) / 8.0; // Recursively calculate probability for next moves
-        }
-        
-        dp[row][column][k] = probability; // Store the calculated probability
-        
-        return probability;
-    };
-    
-    return calculateProbability(n, k, row, column);
+      }
+    }
+  }
+
+  // Calculate the total probability of the knight remaining on the board
+  let probability = 0.0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      probability += dp[i][j][k];
+    }
+  }
+
+  return probability;
 };
