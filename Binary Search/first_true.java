17902982e246b919b706041eb87231c6a327b88c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-09-22 22:39:54 +0530

    add question

--- Binary Search/first_true.java
+++ Binary Search/first_true.java
@@ -1,3 +1,13 @@
+/*
+    An array of boolean values is divided into two sections; the left section consists of all false and the 
+    right section consists of all true. Find the First True in a Sorted Boolean Array of the 
+    right section, i.e. the index of the first true element. If there is no true element, return -1.
+    
+    Input: arr = [false, false, true, true, true]
+    Output: 2
+
+    Explanation: first true's index is 2.
+*/
 public import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
