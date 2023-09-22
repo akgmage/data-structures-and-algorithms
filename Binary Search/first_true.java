dba7fa7685a9f5d175c0e7237441cbdd419ede8e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-09-22 22:38:56 +0530

    add comments

--- Binary Search/first_true.java
+++ Binary Search/first_true.java
@@ -5,18 +5,27 @@ import java.util.stream.Collectors;
 
 class Solution {
     public static int findBoundary(List<Boolean> arr) {
-        int low = 0;
-        int high = arr.size() - 1;
-        int bv = -1;
+        int low = 0;                // Initialize the low pointer to the beginning of the list.
+        int high = arr.size() - 1;  // Initialize the high pointer to the end of the list.
+        int bv = -1;                // Initialize bv (boundary value) to -1.
+
         while (low <= high) {
-            int mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;  // Calculate the middle index.
+
             if (arr.get(mid) == false) {
-                low = mid + 1;
+                // If the element at the middle index is 'false',
+                // it means that the last 'true' value should be on the right side.
+                low = mid + 1;  // Move the low pointer to the right of mid.
             } else {
-                bv = mid;
-                high = mid - 1;
+                // If the element at the middle index is 'true',
+                // update bv to the current middle index and continue searching on the left side.
+                bv = mid;       // Update bv to the current middle index.
+                high = mid - 1; // Move the high pointer to the left of mid.
             }
         }
+
+        // The loop ends when low > high, indicating that the search is complete.
+        // bv contains the index of the last 'true' value encountered.
         return bv;
     }
 }
