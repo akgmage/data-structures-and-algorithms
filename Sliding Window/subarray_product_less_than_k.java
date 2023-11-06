d3a9cbee1dfd995a2ec233ddc5e5ec3579a35b62
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-11-06 23:02:43 +0530

    add comments

--- Sliding Window/subarray_product_less_than_k.java
+++ Sliding Window/subarray_product_less_than_k.java
@@ -5,16 +5,29 @@
  */
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int startWindow = 0, product = 1, count = 0;
+        // Initialize pointers and a count variable
+        int startWindow = 0; // The left end of the sliding window
+        int product = 1;    // Initialize product to 1 to accumulate the product
+        int count = 0;      // Count of subarrays with a product less than k
 
-        for(int endWindow = 0; endWindow < nums.length; endWindow++) {
+        // Iterate through the array using a sliding window approach
+        for (int endWindow = 0; endWindow < nums.length; endWindow++) {
+            // Multiply the current element to the product
             product *= nums[endWindow];
-            while(startWindow <= endWindow && product >= k) {
+
+            // Shrink the window by moving the start pointer as long as the product is greater than or equal to k
+            while (startWindow <= endWindow && product >= k) {
+                // Divide the product by the element at the start of the window
                 product /= nums[startWindow];
+                // Move the start of the window to the right
                 startWindow++;
             }
+
+            // Update the count with the number of valid subarrays within the current window
             count += endWindow - startWindow + 1;
         }
+
+        // Return the count, which represents the number of subarrays with a product less than k
         return count;
     }
 }
